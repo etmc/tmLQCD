0ca76e18357e05910d79ff2ac42e0d757e8bfae2
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2004-02-13 16:10:10 +0000

    Bug fixed. Version 1.0, without enhancements.

--- bicgstabell.c
+++ bicgstabell.c
@@ -1,4 +1,17 @@
 /* $Id$ */
+/*************************************************************
+ *
+ * This is an implementation of bicgstab(l)
+ * corresponding to the paper of G. L.G. Sleijpen and
+ * D.R. Fokkema
+ * Transactions on Numerical Analysis
+ * Volume1, pp. 11-32, 1993
+ *
+ * Author: Carsten Urbach
+ *         urbach@physik.fu-berlin.de
+ *
+ *************************************************************/
+
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -18,13 +31,13 @@
 #define _SO(x)
 #endif
 
-int bicgstabell(const int P, const int Q, const int max_iter, 
+int bicgstabell(const int x0, const int b, const int max_iter, 
 		double eps_sq, const int _l, const double q_off) {
 
   double err;
   int i, j, k, l;
   double rho0, rho1, beta, alpha, omega, gamma0;
-  int r[5], u[5], r0, u0, x;
+  int r[5], u[5], r0_tilde, u0, x;
   double tau[5][5], gamma[25], gammap[25], gammapp[25], sigma[25];
 
 
@@ -35,7 +48,7 @@ int bicgstabell(const int P, const int Q, const int max_iter,
     k = -4;
   }
 
-  r0 = DUM_SOLVER;
+  r0_tilde = DUM_SOLVER;
   u0 = DUM_SOLVER+1;
   x = DUM_SOLVER+2;
   for(i = 0; i <= l; i++){
@@ -44,34 +57,34 @@ int bicgstabell(const int P, const int Q, const int max_iter,
   }
 
 
-  assign(x, P, VOLUME/2);
+/*   assign(x, x0, VOLUME/2);  */
+  x = x0; 
+  gamma5(u[0], b); 
   if(g_use_clover_flag == 1){
-    M_psi(r0, P, q_off); 
+    M_psi(r0_tilde, x, q_off); 
   }
   else {
-    Mtm_plus_psi(r0, P);
+    Mtm_plus_psi(r0_tilde, x);
   }
-  diff(r[0], Q, r0, VOLUME/2);
+  diff(r[0], u[0], r0_tilde, VOLUME/2);
   zero_spinor_field(u[0]);
-  assign(r0, r[0], VOLUME/2);
+  assign(r0_tilde, r[0], VOLUME/2);
 
   rho0 = 1.;
   alpha = 0.;
   omega = 1.;
-  err = square_norm(r0, VOLUME/2);
+  err = square_norm(r0_tilde, VOLUME/2);
 
   while( k < max_iter && err > eps_sq) {
     k+=l;
-    if(g_proc_id == 0){
-      printf(" %d, %e\n", k, err);
-    }
 
     /* The BiCG part */
 
     rho0 *= -omega;
     for(j = 0; j < l; j++) {
-      rho1 = scalar_prod_r(r[j], r0, VOLUME/2);
-      beta = alpha*(rho1/rho0);
+      rho1 = scalar_prod_r(r[j], r0_tilde, VOLUME/2);
+      beta = (rho1/rho0);
+      beta *= alpha; 
       rho0 = rho1;
       for(i = 0; i <= j; i++) {
 	/* u_i = r_i - \beta u_i */
@@ -83,8 +96,9 @@ int bicgstabell(const int P, const int Q, const int max_iter,
       else {
 	Mtm_plus_psi(u[j+1], u[j]);
       }
-      gamma0 = scalar_prod_r(u[j+1], r0, VOLUME/2);
+      gamma0 = scalar_prod_r(u[j+1], r0_tilde, VOLUME/2);
       alpha = rho0/gamma0;
+      /* r_i = r_i - \alpha u_{i+1} */
       for(i = 0; i <= j; i++) {
 	assign_add_mul(r[i], -alpha, u[i+1], VOLUME/2);
       }
@@ -94,6 +108,7 @@ int bicgstabell(const int P, const int Q, const int max_iter,
       else {
 	Mtm_plus_psi(r[j+1], r[j]);
       }
+      /* x = x + \alpha u_0 */
       assign_add_mul(x, alpha, u[0], VOLUME/2);
     }
 
@@ -101,17 +116,17 @@ int bicgstabell(const int P, const int Q, const int max_iter,
 
     for(j = 1; j <= l; j++){
       for(i = 1; i < j; i++){
-	tau[i][j] = 1./sigma[i]*scalar_prod_r(r[j], r[i], VOLUME/2);
+	tau[i][j] = scalar_prod_r(r[j], r[i], VOLUME/2)/sigma[i];
 	assign_add_mul(r[j], -tau[i][j], r[i], VOLUME/2);
       }
       sigma[j] = scalar_prod_r(r[j], r[j], VOLUME/2);
-      gammap[j] = 1./sigma[j]*scalar_prod_r(r[0], r[j], VOLUME/2);
+      gammap[j] = scalar_prod_r(r[0], r[j], VOLUME/2)/sigma[j];
     }
     gamma[l] = gammap[l];
     omega = gamma[l];
     for(j = l-1; j > 0; j--) {
       gamma[j] = gammap[j];
-      for(i = j+1; i < l; i++) {
+      for(i = j+1; i <= l; i++) {
 	gamma[j] -= (tau[j][i]*gamma[i]);
       }
     }
@@ -132,8 +147,9 @@ int bicgstabell(const int P, const int Q, const int max_iter,
       assign_add_mul(u[0], -gamma[j], u[j], VOLUME/2);
     }
     err = square_norm(r[0], VOLUME/2);
+    _SO(if(g_proc_id == 0){printf(" Iterated %d %d, %e\n", l, k, err);fflush( stdout );});
   }
-  assign(P, x, VOLUME/2);
+/*   assign(x0, x, VOLUME/2);  */
   if(k == max_iter) return(-1);
   return(k);
 }
