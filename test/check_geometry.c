2ef93ab58edfa2512556a9ddccc11431ee2d03a1
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2004-04-23 11:08:13 +0000

    checking also the parallel version.

--- test/check_geometry.c
+++ test/check_geometry.c
@@ -15,21 +15,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#ifdef MPI
+#include <mpi.h>
+#endif
 #include "global.h"
 #include "geometry_eo.h"
+#ifdef MPI
+#include "mpi_init.h"
+#endif
 
-int main(void)
+int main(int argc,char *argv[])
 {
   int ix;
   int * itest;
   int x0,x1,x2,x3;
   int iy0,iy1,iy2,iy3;
   int iz0,iz1,iz2,iz3;   
+#ifdef MPI
+  int r1=0, r2=0;
+#endif
 
-  itest = calloc(VOLUME, sizeof(int));
+  mpi_init(argc, argv);
+
+  itest = calloc(VOLUME+RAND, sizeof(int));
   printf("\n");
   printf("Test of the geometry programs \n");
-  printf("So far only for serial version\n");
   printf("------------------------------\n");
 
   printf("\n");
@@ -37,19 +47,23 @@ int main(void)
    
   geometry();
 
-  for (ix=0;ix<VOLUME;ix++){
+  for (ix=0;ix<VOLUMEPLUSRAND;ix++){
     itest[ix]=0;
   }
-   
-  for (x0=0;x0<T;x0++){
-    for (x1=0;x1<L;x1++){
-      for (x2=0;x2<L;x2++){
-	for (x3=0;x3<L;x3++){
+  
+
+  for (x0 = 0; x0 < T; x0++){
+    for (x1 = 0; x1 < LX; x1++){
+      for (x2 = 0; x2 < LY; x2++){
+	for (x3 = 0; x3 < LZ; x3++){
 	  ix=g_ipt[x0][x1][x2][x3];
 
 	  if ((ix<0)||(ix>=VOLUME)){
 	    printf("The index ipt is out of range\n");
 	    printf("Program aborted\n");
+#ifdef MPI
+	    MPI_Finalize();
+#endif
 	    exit(0);
 	  }
                
@@ -59,53 +73,102 @@ int main(void)
     }
   }
 
-  for (ix=0;ix<VOLUME;ix++){
+  for (ix = 0; ix < VOLUME; ix++){
     if (itest[ix]!=1){
       printf("The index ipt is not one-to-one\n");
       printf("Program aborted\n");
+#ifdef MPI
+      MPI_Finalize();
+#endif
       exit(0);
     }
   }
 
-  for (x0=0;x0<T;x0++){
-    for (x1=0;x1<L;x1++){
-      for (x2=0;x2<L;x2++){
-	for (x3=0;x3<L;x3++){
+  for (x0 = 0; x0 < T; x0++){
+    for (x1 = 0; x1 < LX; x1++){
+      for (x2 = 0; x2 < LY; x2++){
+	for (x3 = 0; x3 < LZ; x3++){
 	  ix=g_ipt[x0][x1][x2][x3];
 
 	  iy0=g_iup[ix][0];
+#if (defined PARALLELT || defined PARALLELXT )
+	  if(x0!=T-1) {
+	    iz0=g_ipt[(x0+1)%T][x1][x2][x3];
+	  }
+	  else {
+	    iz0 = iy0;
+	    itest[iy0]++;
+	  }
+#else
 	  iz0=g_ipt[(x0+1)%T][x1][x2][x3];
-                       
+#endif     
+                  
 	  iy1=g_iup[ix][1];
-	  iz1=g_ipt[x0][(x1+1)%L][x2][x3];
+#if (defined PARALLELXT)
+	  if(x1 !=0) {
+	    iz1=g_ipt[x0][(x1+1)%LX][x2][x3];
+	  }
+	  else {
+	    iz1 = iy1;
+	    itest[iy1]++;
+	  }
+#else
+	  iz1=g_ipt[x0][(x1+1)%LX][x2][x3];
+#endif
 
 	  iy2=g_iup[ix][2];
-	  iz2=g_ipt[x0][x1][(x2+1)%L][x3];
+	  iz2=g_ipt[x0][x1][(x2+1)%LY][x3];
 
 	  iy3=g_iup[ix][3];
-	  iz3=g_ipt[x0][x1][x2][(x3+1)%L];               
+	  iz3=g_ipt[x0][x1][x2][(x3+1)%LZ];               
                
 	  if ((iy0!=iz0)||(iy1!=iz1)||(iy2!=iz2)||(iy3!=iz3)){
 	    printf("The index iup is incorrect\n");
+	    printf("%d %d %d %d\n", (iy0!=iz0), (iy1!=iz1), (iy2!=iz2), (iy3!=iz3));
 	    printf("Program aborted\n");
+#ifdef MPI
+	    MPI_Finalize();
+#endif
 	    exit(0);
 	  }
 
 	  iy0=g_idn[ix][0];
+#if (defined PARALLELT || defined PARALLELXT )
+	  if(x0 !=0) {
+	    iz0=g_ipt[(x0+T-1)%T][x1][x2][x3];
+	  }
+	  else {
+	    iz0 = iy0;
+	    itest[iy0]++;
+	  }
+#else
 	  iz0=g_ipt[(x0+T-1)%T][x1][x2][x3];
-                       
+#endif
 	  iy1=g_idn[ix][1];
-	  iz1=g_ipt[x0][(x1+L-1)%L][x2][x3];
-
+#if (defined PARALLELXT)
+	  if(x1 !=0) {
+	    iz1=g_ipt[x0][(x1+LX-1)%LX][x2][x3];
+	  }
+	  else {
+	    iz1 = iy1;
+	    itest[iy1]++;
+	  }
+#else
+	  iz1=g_ipt[x0][(x1+LX-1)%LX][x2][x3];
+#endif
 	  iy2=g_idn[ix][2];
-	  iz2=g_ipt[x0][x1][(x2+L-1)%L][x3];
+	  iz2=g_ipt[x0][x1][(x2+LY-1)%LY][x3];
 
 	  iy3=g_idn[ix][3];
-	  iz3=g_ipt[x0][x1][x2][(x3+L-1)%L];               
+	  iz3=g_ipt[x0][x1][x2][(x3+LZ-1)%LZ];               
                
 	  if ((iy0!=iz0)||(iy1!=iz1)||(iy2!=iz2)||(iy3!=iz3)){
 	    printf("The index idn is incorrect\n");
+	    printf("%d %d %d %d\n", (iy0!=iz0), (iy1!=iz1), (iy2!=iz2), (iy3!=iz3));
 	    printf("Program aborted\n");
+#ifdef MPI
+	    MPI_Finalize();
+#endif
 	    exit(0);
 	  }
 	}
@@ -113,7 +176,22 @@ int main(void)
     }
   }
 
+  for (ix = VOLUME; ix < VOLUMEPLUSRAND; ix++){
+    if (itest[ix]!=1){
+      printf("The boundary is not correctly used\n");
+      printf("Program aborted\n");
+#ifdef MPI
+      MPI_Finalize();
+#endif
+      exit(0);
+    }
+  }
+
   printf("The lattice is correctly mapped by the index arrays\n");
   printf("\n");
+
+#ifdef MPI
+  MPI_Finalize();
+#endif
   return(0);
 }
