9ede463c4272b929c1f8bef20d98cd68ba929c98
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2005-04-04 12:13:51 +0000

    ...

--- linalg/lapack.h
+++ linalg/lapack.h
@@ -20,6 +20,8 @@
 #define dlamch DLAMCH
 #define ilaenv ILAENV
 #define zlapcy CLAPCY
+#define zgetrf CGETRF
+#define zgetrs CGETRS
 extern void _FT(dsyev)();
 extern void _FT(zheev)();
 extern void _FT(dgetrs)();
@@ -31,6 +33,8 @@ extern void _FT(zlacpy)();
 extern void _FT(dlaset)();
 extern double _FT(dlamch)();
 extern int _FT(ilaenv)();
+extern void _FT(zgetrf)();
+extern void _FT(zgetrs)();
 #else
 
 extern void _FT(dsyev) (char* jobz, char* uplo, int* n, double a[],
@@ -43,9 +47,20 @@ extern void _FT(zheev) (char* jobz, char* uplo, int* n, complex a[],
 extern void _FT(dgetrs) (char* trans, int* n, int* nrhs, double a[],
         int* lda, int ipiv[], double b[], int* ldb, int* info,
         int len_trans);
-
 extern void _FT(dgetrf) (int* m, int* n, double a[], int* lda, int ipiv[],
         int* info);
+
+extern void _FT(zgetrs) (char* trans, int* n, int* nrhs, complex a[],
+        int* lda, int ipiv[], complex b[], int* ldb, int* info,
+        int len_trans);
+extern void _FT(zgetrf) (int* m, int* n, complex a[], int* lda, int ipiv[],
+        int* info);
+
+extern void _FT(zhetrs) (char* uplo, int* n, int* nrhs, complex a[],
+			 int* lda, int ipiv[], complex b[], int* ldb, int* info,
+			 int len_uplo);
+extern void _FT(zhetrf) (char* uplo, int* n, complex a[], int* lda, int ipiv[],
+			 complex work[], int * lwork, int* info, int len_uplo);
 
 extern void _FT(dlarnv) (int *IDIST, int *ISEED, int *N, double *X);
 extern void _FT(zlarnv) (int *IDIST, int *ISEED, int *N, complex *X);
