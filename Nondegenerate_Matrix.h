01721d57026220dece5749cae9449a8b14c63ea3
Thomas Chiarappa <Thomas.Chiarappa@mib.infn.it>
2005-11-11 15:05:14 +0000

    *** empty log message ***

--- Nondegenerate_Matrix.h
+++ Nondegenerate_Matrix.h
@@ -8,10 +8,12 @@ void QNon_degenerate(spinor * const l_strange, spinor * const l_charm,
 void QdaggerNon_degenerate(spinor * const l_strange, spinor * const l_charm,
                            spinor * const k_strange, spinor * const k_charm);
 
-void QNon_degenerate_eo(spinor * const l_strange, spinor * const l_charm,
-                        spinor * const k_strange, spinor * const k_charm);
+void Q_Qdagger_ND(spinor * const l_strange, spinor * const l_charm,
+                  spinor * const k_strange, spinor * const k_charm);
 
-void QNon_degenerate_eo_dagger(spinor * const l_strange, spinor * const l_charm,
-			       spinor * const k_strange, spinor * const k_charm);
+void Q_Qdagger_ND_BI(bispinor * const bisp_l, bispinor * const bisp_k);
+
+void Q_test_epsilon(spinor * const l_strange, spinor * const l_charm,
+                    spinor * const k_strange, spinor * const k_charm);
 
 #endif
