0b3079fec6a1fd391da9cd949d120597539b2d95
Thomas Chiarappa <Thomas.Chiarappa@mib.infn.it>
2005-11-11 14:59:51 +0000

    *** empty log message ***

--- linalg_eo.h
+++ linalg_eo.h
@@ -26,4 +26,17 @@
 #include "linalg/assign_add_mul_add_mul.h"
 #include "linalg/assign_mul_bra_add_mul_ket_add.h"
 
+#include "linalg/comp_decomp.h"
+#include "linalg/square_norm_bi.h"
+#include "linalg/assign_bi.h"
+#include "linalg/scalar_prod_bi.h"
+#include "linalg/diff_bi.h"
+#include "linalg/assign_diff_mul_bi.h"
+#include "linalg/assign_add_mul_add_mul_bi.h"
+#include "linalg/assign_mul_bra_add_mul_ket_add_bi.h"
+#include "linalg/mul_r_bi.h"
+#include "linalg/scalar_prod_r_bi.h"
+#include "linalg/assign_add_mul_r_bi.h"
+#include "linalg/assign_mul_add_r_bi.h"
+
 #endif
