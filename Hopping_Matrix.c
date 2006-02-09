3b49056da15d3abcafd4add4690692b65af8e1be
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2006-02-09 23:14:21 +0000

    tuning for BGL

--- Hopping_Matrix.c
+++ Hopping_Matrix.c
@@ -14,6 +14,7 @@
  *
  ******************************************/
 
+/* #define BGL */
 
 #ifdef HAVE_CONFIG_H
 # include<config.h>
@@ -416,6 +417,340 @@ void Hopping_Matrix(const int ieo, spinor * const l, spinor * const k){
   }
 }
 
+#elif (defined BGL && defined XLC)
+
+#include "bgl.h"
+
+#undef ALIGN
+#define ALIGN __attribute__ ((aligned (16)))
+
+void Hopping_Matrix(const int ieo, spinor * const l, spinor * const k){
+  int icx,icy,icz,ioff,ioff2;
+  int ix,iy,iz;
+  su3 *up ALIGN;
+  su3 *um ALIGN;
+  spinor *sp ALIGN;
+  spinor *sm ALIGN;
+  spinor *rn ALIGN;
+  double _Complex reg00, reg01, reg02, reg03, reg04, reg05, reg06, reg07;
+  double _Complex reg10, reg20;
+  double _Complex rs00, rs01, rs02, rs10, rs11, rs12, rs20, rs21, rs22, 
+    rs30, rs31, rs32;
+
+#pragma disjoint(*sp, *sm, *rn, *up, *um, *l, *k)
+
+  __alignx(16,l);
+  __alignx(16,k);
+
+#ifdef MPI
+  xchange_field(k, ieo);
+#endif
+
+  if(ieo == 0){
+    ioff = 0;
+  } 
+  else{
+    ioff = (VOLUME+RAND)/2;
+  }
+  ioff2 = (VOLUME+RAND)/2-ioff;
+
+  ix=g_eo2lexic[ioff];
+  iy=g_iup[ix][0]; 
+  icy=g_lexic2eosub[iy];
+
+  sp=k+icy;
+  up=&g_gauge_field[ix][0];
+   
+  /**************** loop over all lattice sites ******************/
+  for(icx = ioff; icx < (VOLUME/2+ioff); icx++){
+    ix=g_eo2lexic[icx];
+    /*********************** direction +0 ************************/
+    iy=g_idn[ix][0]; 
+    icy=g_lexic2eosub[iy];
+#ifndef _GAUGE_COPY
+    um=&g_gauge_field[iy][0]; 
+#else
+    um=&g_gauge_field_back[ix][0]; 
+#endif
+    _prefetch_su3(um); 
+    sm=k+icy;
+    _prefetch_spinor(sm); 
+
+    _bgl_load((*sp).s0);
+    _bgl_load_up((*sp).s2);
+    _bgl_vector_add();
+
+    _bgl_su3_multiply((*up));
+    _bgl_vector_cmplx_mul(ka0);
+    _bgl_store_up_rs0();
+    _bgl_store_up_rs1();
+
+    _bgl_load((*sp).s1);
+    _bgl_load_up((*sp).s3);
+    _bgl_vector_add();
+      
+    _bgl_su3_multiply((*up));
+    _bgl_vector_cmplx_mul(ka0);
+    _bgl_store_up_rs1();
+    _bgl_store_up_rs3();
+
+
+    /*********************** direction -0 ************************/
+
+    iy=g_iup[ix][1]; 
+    icy=g_lexic2eosub[iy];
+
+    up+=1;
+    _prefetch_su3(up); 
+    sp=k+icy;
+    _prefetch_spinor(sp); 
+
+    _bgl_load((*sm).s0);
+    _bgl_load_up((*sm).s2);
+    _bgl_vector_sub();
+      
+    _bgl_su3_inverse_multiply((*um));
+    _bgl_vector_cmplxcg_mul(ka0);
+
+    _bgl_add_to_rs0();
+    _bgl_sub_from_rs2();
+      
+    _bgl_load((*sm).s1);
+    _bgl_load_up((*sm).s3);
+    _bgl_vector_sub();
+      
+    _bgl_su3_inverse_multiply((*um));
+    _bgl_vector_cmplxcg_mul(ka0);
+
+    _bgl_add_to_rs1();
+    _bgl_sub_from_rs3();
+
+    /*********************** direction +1 ************************/
+
+    iy=g_idn[ix][1]; 
+    icy=g_lexic2eosub[iy];
+
+#ifndef _GAUGE_COPY
+    um=&g_gauge_field[iy][1]; 
+#else
+    um+=1;
+#endif
+    _prefetch_su3(um); 
+    sm=k+icy;
+    _prefetch_spinor(sm); 
+
+    _bgl_load((*sp).s0);
+    _bgl_load_up((*sp).s3);
+/*     _bgl_vector_i_mul(); */
+    _bgl_vector_i_mul_add();
+
+    _bgl_su3_multiply((*up));
+    _bgl_vector_cmplx_mul(ka1);
+
+    _bgl_add_to_rs0();
+/*     _bgl_vector_i_mul(); */
+    _bgl_i_mul_sub_from_rs3();
+
+    _bgl_load((*sp).s1);
+    _bgl_load_up((*sp).s2);
+/*     _bgl_vector_i_mul(); */
+    _bgl_vector_i_mul_add();
+
+    _bgl_su3_multiply((*up));
+    _bgl_vector_cmplx_mul(ka1);
+
+    _bgl_add_to_rs1();
+    
+/*     _bgl_vector_i_mul(); */
+    _bgl_i_mul_sub_from_rs2()
+
+    /*********************** direction -1 ************************/
+
+    iy=g_iup[ix][2]; 
+    icy=g_lexic2eosub[iy];
+
+    up+=1;
+    _prefetch_su3(up); 
+    sp=k+icy;
+    _prefetch_spinor(sp); 
+
+    _bgl_load((*sm).s0);
+    _bgl_load_up((*sm).s3);
+/*     _bgl_vector_i_mul(); */
+    _bgl_vector_i_mul_sub();
+      
+    _bgl_su3_inverse_multiply((*um));
+    _bgl_vector_cmplxcg_mul(ka1);
+      
+    _bgl_add_to_rs0();
+/*     _bgl_vector_i_mul();       */
+    _bgl_i_mul_add_to_rs3();
+
+    _bgl_load((*sm).s1);
+    _bgl_load_up((*sm).s2);
+/*     _bgl_vector_i_mul(); */
+    _bgl_vector_i_mul_sub();
+      
+    _bgl_su3_inverse_multiply((*um));
+    _bgl_vector_cmplxcg_mul(ka1);
+      
+    _bgl_add_to_rs1();
+
+/*     _bgl_vector_i_mul(); */
+    _bgl_i_mul_add_to_rs2();      
+
+    /*********************** direction +2 ************************/
+
+    iy=g_idn[ix][2]; 
+    icy=g_lexic2eosub[iy];
+
+#ifndef _GAUGE_COPY
+    um=&g_gauge_field[iy][2]; 
+#else
+    um+=1;
+#endif
+    _prefetch_su3(um); 
+    sm=k+icy;
+    _prefetch_spinor(sm); 
+
+    _bgl_load((*sp).s0);
+    _bgl_load_up((*sp).s3);
+    _bgl_vector_add();
+
+    _bgl_su3_multiply((*up));
+    _bgl_vector_cmplx_mul(ka2);
+
+    _bgl_add_to_rs0();
+    _bgl_add_to_rs3();
+
+    _bgl_load((*sp).s1);
+    _bgl_load_up((*sp).s2);
+    _bgl_vector_sub();
+
+    _bgl_su3_multiply((*up));
+    _bgl_vector_cmplx_mul(ka2);
+
+    _bgl_add_to_rs1();
+    _bgl_sub_from_rs2();
+
+
+    /*********************** direction -2 ************************/
+
+    iy=g_iup[ix][3]; 
+    icy=g_lexic2eosub[iy];
+
+    up+=1;
+    _prefetch_su3(up); 
+    sp=k+icy;
+    _prefetch_spinor(sp); 
+
+    _bgl_load((*sm).s0);
+    _bgl_load_up((*sm).s3);
+    _bgl_vector_sub();
+      
+    _bgl_su3_inverse_multiply((*um));
+    _bgl_vector_cmplxcg_mul(ka2);
+      
+    _bgl_add_to_rs0();
+    _bgl_sub_from_rs3();
+
+    _bgl_load((*sm).s1);
+    _bgl_load_up((*sm).s2);
+    _bgl_vector_add();
+      
+    _bgl_su3_inverse_multiply((*um));
+    _bgl_vector_cmplxcg_mul(ka2);
+      
+    _bgl_add_to_rs1();
+
+    _bgl_add_to_rs2();
+      
+    /*********************** direction +3 ************************/
+
+    iy=g_idn[ix][3]; 
+    icy=g_lexic2eosub[iy];
+
+#ifndef _GAUGE_COPY
+    um=&g_gauge_field[iy][3]; 
+#else
+    um+=1;
+#endif
+    _prefetch_su3(um); 
+    sm=k+icy;
+    _prefetch_spinor(sm); 
+
+    _bgl_load((*sp).s0);
+    _bgl_load_up((*sp).s2);
+/*     _bgl_vector_i_mul(); */
+    _bgl_vector_i_mul_add();
+
+    _bgl_su3_multiply((*up));
+    _bgl_vector_cmplx_mul(ka3);
+
+    _bgl_add_to_rs0();
+
+/*     _bgl_vector_i_mul();       */
+    _bgl_i_mul_sub_from_rs3();
+
+    _bgl_load((*sp).s1);
+    _bgl_load_up((*sp).s3);
+/*     _bgl_vector_i_mul(); */
+    _bgl_vector_i_mul_sub();
+
+    _bgl_su3_multiply((*up));
+    _bgl_vector_cmplx_mul(ka3);
+
+    _bgl_add_to_rs1();
+
+/*     _bgl_vector_i_mul();       */
+    _bgl_i_mul_add_to_rs3();
+      
+    /*********************** direction -3 ************************/
+
+    icz=icx+1;
+    if(icz==((VOLUME+RAND)/2+ioff)) icz=ioff;
+    iz=g_eo2lexic[icz];
+    iy=g_iup[iz][0]; icy=g_lexic2eosub[iy];
+
+    up=&g_gauge_field[iz][0];
+    _prefetch_su3(up); 
+    sp=k+icy;
+    _prefetch_spinor(sp); 
+
+    _bgl_load((*sm).s0);
+    _bgl_load_up((*sm).s2);
+/*     _bgl_vector_i_mul(); */
+    _bgl_vector_i_mul_sub();
+      
+    _bgl_su3_inverse_multiply((*um));
+    _bgl_vector_cmplxcg_mul(ka3);
+
+    rn=l+(icx-ioff);
+      
+    _bgl_add_to_rs0();
+    _bgl_store_rs0((*rn).s0);
+
+/*     _bgl_vector_i_mul();      */
+    _bgl_i_mul_add_to_rs2();
+    _bgl_store_rs2((*rn).s2);
+
+    _bgl_load((*sm).s1);
+    _bgl_load_up((*sm).s3);
+/*     _bgl_vector_i_mul(); */
+    _bgl_vector_i_mul_add();
+      
+    _bgl_su3_inverse_multiply((*um));
+    _bgl_vector_cmplxcg_mul(ka3);
+
+    _bgl_add_to_rs1();
+    _bgl_store_rs1((*rn).s1);
+
+/*     _bgl_vector_i_mul();       */
+    _bgl_i_mul_sub_from_rs3();
+    _bgl_store_rs3((*rn).s3);
+
+  }
+}
 #elif defined XLC
 
 static su3_vector psi1, psi2, psi, chi, phi1, phi3;
--- bgl.h
+++ bgl.h
@@ -8,93 +8,358 @@
  *
  * In the functions where they are to be used
  * there must be declared
- * doube _Complex reg0, reg1,...,reg7;
+ * double _Complex reg0, reg1,...,reg7;
  *
  ***********************************************/
 
 #define _bgl_load(s) \
-  reg0 = __lfpd((double*)&(s).c0); \
-  reg1 = __lfpd((double*)&(s).c1); \
-  reg2 = __lfpd((double*)&(s).c2); 
+  reg00 = __lfpd((double*)&(s).c0); \
+  reg01 = __lfpd((double*)&(s).c1); \
+  reg02 = __lfpd((double*)&(s).c2); 
 
 #define _bgl_load_up(s) \
-  reg3 = __lfpd((double*)&(s).c0); \
-  reg4 = __lfpd((double*)&(s).c1); \
-  reg5 = __lfpd((double*)&(s).c2); 
+  reg03 = __lfpd((double*)&(s).c0); \
+  reg04 = __lfpd((double*)&(s).c1); \
+  reg05 = __lfpd((double*)&(s).c2); 
 
 #define _bgl_store(s) \
-  __stfpd((double*)&(s).c0, reg0); \
-  __stfpd((double*)&(s).c1, reg1); \
-  __stfpd((double*)&(s).c2, reg2);
+  __stfpd((double*)&(s).c0, reg00); \
+  __stfpd((double*)&(s).c1, reg01); \
+  __stfpd((double*)&(s).c2, reg02);
 
 #define _bgl_store_up(s) \
-  __stfpd((double*)&(s).c0, reg3); \
-  __stfpd((double*)&(s).c1, reg4); \
-  __stfpd((double*)&(s).c2, reg5);
+  __stfpd((double*)&(s).c0, reg03); \
+  __stfpd((double*)&(s).c1, reg04); \
+  __stfpd((double*)&(s).c2, reg05);
+
+#define _bgl_store_rs0(s) \
+  __stfpd((double*)&(s).c0, rs00); \
+  __stfpd((double*)&(s).c1, rs01); \
+  __stfpd((double*)&(s).c2, rs02);
+
+#define _bgl_store_rs1(s) \
+  __stfpd((double*)&(s).c0, rs10); \
+  __stfpd((double*)&(s).c1, rs11); \
+  __stfpd((double*)&(s).c2, rs12);
+
+#define _bgl_store_rs2(s) \
+  __stfpd((double*)&(s).c0, rs20); \
+  __stfpd((double*)&(s).c1, rs21); \
+  __stfpd((double*)&(s).c2, rs22);
+
+#define _bgl_store_rs3(s) \
+  __stfpd((double*)&(s).c0, rs30); \
+  __stfpd((double*)&(s).c1, rs31); \
+  __stfpd((double*)&(s).c2, rs32);
+
+#define _bgl_store_up_rs01() \
+  __stfpd(&rs00, reg03); \
+  __stfpd(&rs01, reg04); \
+  __stfpd(&rs02, reg05); 
+
+#define _bgl_store_up_rs11() \
+  __stfpd(&rs10, reg03); \
+  __stfpd(&rs11, reg04); \
+  __stfpd(&rs12, reg05); 
+
+#define _bgl_store_up_rs21() \
+  __stfpd(&rs20, reg03); \
+  __stfpd(&rs21, reg04); \
+  __stfpd(&rs22, reg05); 
+
+#define _bgl_store_up_rs31() \
+  __stfpd(&rs30, reg03); \
+  __stfpd(&rs31, reg04); \
+  __stfpd(&rs32, reg05); 
+
+
+#define _bgl_store_up_rs1() \
+  rs10 = reg03;		    \
+  rs11 = reg04;		    \
+  rs12 = reg05; 
+
+#define _bgl_store_up_rs3() \
+  rs30 = reg03;		    \
+  rs31 = reg04;		    \
+  rs32 = reg05;
+
+#define _bgl_store_up_rs0() \
+  rs00 = reg03;		    \
+  rs01 = reg04;		    \
+  rs02 = reg05; 
+
+#define _bgl_store_up_rs2() \
+  rs20 = reg03;		    \
+  rs21 = reg04;		    \
+  rs22 = reg05;
+
+#define _bgl_add_to_rs0() \
+  rs00 = __fpadd(reg03, rs00); \
+  rs01 = __fpadd(reg04, rs01); \
+  rs02 = __fpadd(reg05, rs02);
+
+#define _bgl_i_mul_add_to_rs0() \
+  rs00 = __fxcxnpma(rs00, reg03, 1.); \
+  rs01 = __fxcxnpma(rs01, reg04, 1.); \
+  rs02 = __fxcxnpma(rs02, reg05, 1.);
+
+#define _bgl_add_to_rs1() \
+  rs10 = __fpadd(reg03, rs10); \
+  rs11 = __fpadd(reg04, rs11); \
+  rs12 = __fpadd(reg05, rs12);
+
+#define _bgl_i_mul_add_to_rs1() \
+  rs10 = __fxcxnpma(rs10, reg03, 1.); \
+  rs11 = __fxcxnpma(rs11, reg04, 1.); \
+  rs12 = __fxcxnpma(rs12, reg05, 1.);
+
+#define _bgl_add_to_rs2() \
+  rs20 = __fpadd(reg03, rs20); \
+  rs21 = __fpadd(reg04, rs21); \
+  rs22 = __fpadd(reg05, rs22);
+
+#define _bgl_i_mul_add_to_rs2() \
+  rs20 = __fxcxnpma(rs20, reg03, 1.); \
+  rs21 = __fxcxnpma(rs21, reg04, 1.); \
+  rs22 = __fxcxnpma(rs22, reg05, 1.);
+
+#define _bgl_add_to_rs3() \
+  rs30 = __fpadd(reg03, rs30); \
+  rs31 = __fpadd(reg04, rs31); \
+  rs32 = __fpadd(reg05, rs32);
+
+#define _bgl_i_mul_add_to_rs3() \
+  rs30 = __fxcxnpma(rs30, reg03, 1.); \
+  rs31 = __fxcxnpma(rs31, reg04, 1.); \
+  rs32 = __fxcxnpma(rs32, reg05, 1.);
+
+#define _bgl_sub_from_rs0()    \
+  rs00 = __fpsub(rs00, reg03); \
+  rs01 = __fpsub(rs01, reg04); \
+  rs02 = __fpsub(rs02, reg05); \
+
+#define _bgl_i_mul_sub_from_rs0() \
+  rs00 = __fxcxnpma(rs00, reg03, -1.); \
+  rs01 = __fxcxnpma(rs01, reg04, -1.); \
+  rs02 = __fxcxnpma(rs02, reg05, -1.);
+
+#define _bgl_sub_from_rs1()    \
+  rs10 = __fpsub(rs10, reg03); \
+  rs11 = __fpsub(rs11, reg04); \
+  rs12 = __fpsub(rs12, reg05); \
+
+#define _bgl_i_mul_sub_from_rs1() \
+  rs10 = __fxcxnpma(rs10, reg03, -1.); \
+  rs11 = __fxcxnpma(rs11, reg04, -1.); \
+  rs12 = __fxcxnpma(rs12, reg05, -1.);
+
+#define _bgl_sub_from_rs2()    \
+  rs20 = __fpsub(rs20, reg03); \
+  rs21 = __fpsub(rs21, reg04); \
+  rs22 = __fpsub(rs22, reg05); \
+
+#define _bgl_i_mul_sub_from_rs2() \
+  rs20 = __fxcxnpma(rs20, reg03, -1.); \
+  rs21 = __fxcxnpma(rs21, reg04, -1.); \
+  rs22 = __fxcxnpma(rs22, reg05, -1.);
+
+#define _bgl_sub_from_rs3()    \
+  rs30 = __fpsub(rs30, reg03); \
+  rs31 = __fpsub(rs31, reg04); \
+  rs32 = __fpsub(rs32, reg05); \
+
+#define _bgl_i_mul_sub_from_rs3() \
+  rs30 = __fxcxnpma(rs30, reg03, -1.); \
+  rs31 = __fxcxnpma(rs31, reg04, -1.); \
+  rs32 = __fxcxnpma(rs32, reg05, -1.);
 
 #define _bgl_vector_add() \
-  reg0 = __fpadd(reg0, reg3); \
-  reg1 = __fpadd(reg1, reg4); \
-  reg2 = __fpadd(reg2, reg5); 
+  reg00 = __fpadd(reg00, reg03); \
+  reg01 = __fpadd(reg01, reg04); \
+  reg02 = __fpadd(reg02, reg05); 
 
 #define _bgl_vector_sub() \
-  reg0 = __fpsub(reg0, reg3); \
-  reg1 = __fpsub(reg1, reg4); \
-  reg2 = __fpsub(reg2, reg5); 
+  reg00 = __fpsub(reg00, reg03); \
+  reg01 = __fpsub(reg01, reg04); \
+  reg02 = __fpsub(reg02, reg05); 
+
+/*
+ * Multiplies reg3, reg4, reg5 with  
+ * a complex number c 
+ *
+ */
+
+#define _bgl_vector_cmplx_mul(c) \
+  reg00 = __fxpmul(reg03, c.re); \
+  reg03 = __fxcxnpma(reg00, reg03, c.im); \
+  reg01 = __fxpmul(reg04, c.re); \
+  reg04 = __fxcxnpma(reg01, reg04, c.im); \
+  reg02 = __fxpmul(reg05, c.re); \
+  reg05 = __fxcxnpma(reg02, reg05, c.im); 
+
+#define _bgl_vector_cmplxcg_mul(c) \
+  reg00 = __fxpmul(reg03, c.re); \
+  reg03 = __fxcxnsma(reg00, reg03, c.im); \
+  reg01 = __fxpmul(reg04, c.re); \
+  reg04 = __fxcxnsma(reg01, reg04, c.im); \
+  reg02 = __fxpmul(reg05, c.re); \
+  reg05 = __fxcxnsma(reg02, reg05, c.im); 
+
+
+#define _bgl_vector_cmplx_mul1(c) \
+  reg10 = __cmplx(c.re,c.re);	 \
+  reg20 = __cmplx(c.im,-c.im);	 \
+  reg00 = __fpmul(reg03, reg10); \
+  reg03 = __fxmul(reg03, reg20); \
+  reg01 = __fpmul(reg04, reg10); \
+  reg04 = __fxmul(reg04, reg20); \
+  reg02 = __fpmul(reg05, reg10); \
+  reg05 = __fxmul(reg05, reg20); \
+  reg03 = __fpadd(reg00, reg03); \
+  reg03 = __fpadd(reg01, reg04); \
+  reg03 = __fpadd(reg02, reg05);
+
+/*
+ * Multiplies reg3, reg4, reg5 with  
+ * a complex conjugate of c 
+ *
+ */
+
+#define _bgl_vector_cmplxcg_mul1(c) \
+  reg10 = __cmplx(c.re,c.re);	 \
+  reg20 = __cmplx(-c.im,c.im);	 \
+  reg00 = __fpmul(reg03, reg10); \
+  reg03 = __fxmul(reg03, reg20); \
+  reg01 = __fpmul(reg04, reg10); \
+  reg04 = __fxmul(reg04, reg20); \
+  reg02 = __fpmul(reg05, reg10); \
+  reg05 = __fxmul(reg05, reg20); \
+  reg03 = __fpadd(reg00, reg03); \
+  reg03 = __fpadd(reg01, reg04); \
+  reg03 = __fpadd(reg02, reg05);
+
+#define _bgl_vector_i_mul() \
+  reg03 = __cmplx(-__cimag(reg03), __creal(reg03)); \
+  reg04 = __cmplx(-__cimag(reg04), __creal(reg04)); \
+  reg05 = __cmplx(-__cimag(reg05), __creal(reg05)); 
+
+#define _bgl_vector_i_mul_add() \
+  reg00 = __fxcxnpma(reg00, reg03, 1.); \
+  reg01 = __fxcxnpma(reg01, reg04, 1.); \
+  reg02 = __fxcxnpma(reg02, reg05, 1.);
+
+#define _bgl_vector_i_mul_sub() \
+  reg00 = __fxcxnpma(reg00, reg03, -1.); \
+  reg01 = __fxcxnpma(reg01, reg04, -1.); \
+  reg02 = __fxcxnpma(reg02, reg05, -1.);
+
+#define _bgl_vector_i_mul1() \
+  reg10 = __cmplx(1., -1.); \
+  reg03 = __fxmul(reg03, reg10); \
+  reg04 = __fxmul(reg04, reg10); \
+  reg05 = __fxmul(reg05, reg10);
 
 #define _bgl_su3_multiply(u) \
-  reg3 = __fxpmul(reg0, (u).c00.re); \
-  reg6 = __fxpmul(reg1, (u).c01.re); \
-  reg4 = __fxpmul(reg0, (u).c10.re); \
-  reg3 = __fpadd(reg6, reg3); \
-  reg7 = __fxpmul(reg2, (u).c12.re); \
-  reg5 = __fxpmul(reg0, (u).c20.re); \
-  reg4 = __fpadd(reg7, reg4); \
-  reg6 = __fxpmul(reg1, (u).c21.re); \
-  reg7 = __fxpmul(reg2, (u).c02.re); \
-  reg5 = __fpadd(reg6, reg5); \
-  reg3 = __fpadd(reg7, reg3); \
-  reg6 = __fxpmul(reg1, (u).c11.re); \
-  reg7 = __fxpmul(reg2, (u).c22.re); \
-  reg4 = __fpadd(reg6, reg4); \
-  reg5 = __fpadd(reg7, reg5); \
+  reg03 = __fxpmul(reg00, (u).c00.re); \
+  reg06 = __fxpmul(reg01, (u).c01.re); \
+  reg04 = __fxpmul(reg00, (u).c10.re); \
+  reg03 = __fpadd(reg06, reg03); \
+  reg07 = __fxpmul(reg02, (u).c12.re); \
+  reg05 = __fxpmul(reg00, (u).c20.re); \
+  reg04 = __fpadd(reg07, reg04); \
+  reg06 = __fxpmul(reg01, (u).c21.re); \
+  reg07 = __fxpmul(reg02, (u).c02.re); \
+  reg05 = __fpadd(reg06, reg05); \
+  reg03 = __fpadd(reg07, reg03); \
+  reg06 = __fxpmul(reg01, (u).c11.re); \
+  reg07 = __fxpmul(reg02, (u).c22.re); \
+  reg04 = __fpadd(reg06, reg04); \
+  reg05 = __fpadd(reg07, reg05); \
 			      \
-  reg3 = __fxcxnpma(reg3, reg0, (u).c00.im); \
-  reg4 = __fxcxnpma(reg4, reg1, (u).c11.im); \
-  reg5 = __fxcxnpma(reg5, reg2, (u).c22.im); \
-  reg4 = __fxcxnpma(reg4, reg0, (u).c10.im); \
-  reg3 = __fxcxnpma(reg3, reg1, (u).c01.im); \
-  reg5 = __fxcxnpma(reg5, reg0, (u).c20.im); \
-  reg3 = __fxcxnpma(reg3, reg2, (u).c02.im); \
-  reg5 = __fxcxnpma(reg5, reg1, (u).c21.im); \
-  reg4 = __fxcxnpma(reg4, reg2, (u).c12.im);
+  reg03 = __fxcxnpma(reg03, reg00, (u).c00.im); \
+  reg04 = __fxcxnpma(reg04, reg01, (u).c11.im); \
+  reg05 = __fxcxnpma(reg05, reg02, (u).c22.im); \
+  reg04 = __fxcxnpma(reg04, reg00, (u).c10.im); \
+  reg03 = __fxcxnpma(reg03, reg01, (u).c01.im); \
+  reg05 = __fxcxnpma(reg05, reg00, (u).c20.im); \
+  reg03 = __fxcxnpma(reg03, reg02, (u).c02.im); \
+  reg05 = __fxcxnpma(reg05, reg01, (u).c21.im); \
+  reg04 = __fxcxnpma(reg04, reg02, (u).c12.im);
 
 #define _bgl_su3_inverse_multiply(u) \
-  reg3 = __fxpmul(reg0, (u).c00.re); \
-  reg6 = __fxpmul(reg1, (u).c10.re); \
-  reg4 = __fxpmul(reg0, (u).c01.re); \
-  reg3 = __fpadd(reg6, reg3); \
-  reg7 = __fxpmul(reg2, (u).c21.re); \
-  reg5 = __fxpmul(reg0, (u).c02.re); \
-  reg4 = __fpadd(reg7, reg4); \
-  reg6 = __fxpmul(reg1, (u).c12.re); \
-  reg7 = __fxpmul(reg2, (u).c20.re); \
-  reg5 = __fpadd(reg6, reg5); \
-  reg3 = __fpadd(reg7, reg3); \
-  reg6 = __fxpmul(reg1, (u).c11.re); \
-  reg7 = __fxpmul(reg2, (u).c22.re); \
-  reg4 = __fpadd(reg6, reg4); \
-  reg5 = __fpadd(reg7, reg5); \
+  reg03 = __fxpmul(reg00, (u).c00.re); \
+  reg06 = __fxpmul(reg01, (u).c10.re); \
+  reg04 = __fxpmul(reg00, (u).c01.re); \
+  reg03 = __fpadd(reg06, reg03); \
+  reg07 = __fxpmul(reg02, (u).c21.re); \
+  reg05 = __fxpmul(reg00, (u).c02.re); \
+  reg04 = __fpadd(reg07, reg04); \
+  reg06 = __fxpmul(reg01, (u).c12.re); \
+  reg07 = __fxpmul(reg02, (u).c20.re); \
+  reg05 = __fpadd(reg06, reg05); \
+  reg03 = __fpadd(reg07, reg03); \
+  reg06 = __fxpmul(reg01, (u).c11.re); \
+  reg07 = __fxpmul(reg02, (u).c22.re); \
+  reg04 = __fpadd(reg06, reg04); \
+  reg05 = __fpadd(reg07, reg05); \
 			      \
-  reg3 = __fxcxnsma(reg3, reg0, (u).c00.im); \
-  reg4 = __fxcxnsma(reg4, reg1, (u).c11.im); \
-  reg5 = __fxcxnsma(reg5, reg2, (u).c22.im); \
-  reg4 = __fxcxnsma(reg4, reg0, (u).c01.im); \
-  reg3 = __fxcxnsma(reg3, reg1, (u).c10.im); \
-  reg5 = __fxcxnsma(reg5, reg0, (u).c02.im); \
-  reg3 = __fxcxnsma(reg3, reg2, (u).c20.im); \
-  reg5 = __fxcxnsma(reg5, reg1, (u).c12.im); \
-  reg4 = __fxcxnsma(reg4, reg2, (u).c21.im);
+  reg03 = __fxcxnsma(reg03, reg00, (u).c00.im); \
+  reg04 = __fxcxnsma(reg04, reg01, (u).c11.im); \
+  reg05 = __fxcxnsma(reg05, reg02, (u).c22.im); \
+  reg04 = __fxcxnsma(reg04, reg00, (u).c01.im); \
+  reg03 = __fxcxnsma(reg03, reg01, (u).c10.im); \
+  reg05 = __fxcxnsma(reg05, reg00, (u).c02.im); \
+  reg03 = __fxcxnsma(reg03, reg02, (u).c20.im); \
+  reg05 = __fxcxnsma(reg05, reg01, (u).c12.im); \
+  reg04 = __fxcxnsma(reg04, reg02, (u).c21.im);
+
+/* 35 cycles ! */
+#define _prefetch_spinor(addr)			    \
+  __dcbt(((char*)((unsigned long int)(addr))));	    \
+  __dcbt(((char*)((unsigned long int)(addr)))+32);  \
+  __dcbt(((char*)((unsigned long int)(addr)))+64);  \
+  __dcbt(((char*)((unsigned long int)(addr)))+96);  \
+  __dcbt(((char*)((unsigned long int)(addr)))+128); \
+  __dcbt(((char*)((unsigned long int)(addr)))+164); 
+
+#define _prefetch_spinor2(addr)			    \
+  __prefetch_by_load(((char*)((unsigned long int)(addr))));	    \
+  __prefetch_by_load(((char*)((unsigned long int)(addr)))+32);  \
+  __prefetch_by_load(((char*)((unsigned long int)(addr)))+64);  \
+  __prefetch_by_load(((char*)((unsigned long int)(addr)))+96);  \
+  __prefetch_by_load(((char*)((unsigned long int)(addr)))+128); \
+  __prefetch_by_load(((char*)((unsigned long int)(addr)))+164); 
+
+
+#define _prefetch_su3(addr)			    \
+  __dcbt(((char*)((unsigned long int)(addr))));	    \
+  __dcbt(((char*)((unsigned long int)(addr)))+32);  \
+  __dcbt(((char*)((unsigned long int)(addr)))+64);  \
+  __dcbt(((char*)((unsigned long int)(addr)))+96);  \
+  __dcbt(((char*)((unsigned long int)(addr)))+128); 
+
+#define _prefetch_su32(addr)			    \
+  __prefetch_by_load(((char*)((unsigned long int)(addr))));	    \
+  __prefetch_by_load(((char*)((unsigned long int)(addr)))+32);  \
+  __prefetch_by_load(((char*)((unsigned long int)(addr)))+64);  \
+  __prefetch_by_load(((char*)((unsigned long int)(addr)))+96);  \
+  __prefetch_by_load(((char*)((unsigned long int)(addr)))+128); 
+
+#define _prefetch_spinor3(addr) \
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr))));	  \
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr)))+32);	\
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr)))+64);	\
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr)))+96);	\
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr)))+128);	\
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr)))+164); 
+
+
+#define _prefetch_su33(addr) \
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr))));	  \
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr)))+32);	\
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr)))+64);	\
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr)))+96);	\
+  __prefetch_by_stream(1,((char*)((unsigned long int)(addr)))+128); 
 
 
 #endif
