44beacca5ede207cf278c033a8cd2c153ad19b2e
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2006-02-15 17:32:01 +0000

    moved to solver directory already long ago...

--- bicgstabell.h
+++
@@ -1,9 +0,0 @@
-/* $Id$ */
-
-#ifndef _BICGSTABELL_H
-#define _BICGSTABELL_H
-
-int bicgstabell(const int l, const int k, const int max_iter, 
-		double eps_sq, const int _l, const double q_off);
-
-#endif
