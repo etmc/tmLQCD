c5db4fb8d9d56ad448b5c17fca60410a524750f5
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2006-03-26 13:50:28 +0000

    first correct version

--- solver/gmres_dr.c
+++ solver/gmres_dr.c
@@ -43,11 +43,12 @@
 #include"quicksort.h"
 #include"linalg/lapack.h"
 #include"linalg/blas.h"
+#include"solver/gram-schmidt.h"
 #include"gmres_dr.h"
 
 static void init_gmres_dr(const int _M, const int _V);
 complex short_scalar_prod(complex * const x, complex * const y, const int N);
-void short_ModifiedGS(complex v[], int n, int m, complex A[]);
+void short_ModifiedGS(complex v[], int n, int m, complex A[], int lda);
 
 static complex ** work;
 static complex * _work;
@@ -136,7 +137,7 @@ int gmres_dr(spinor * const P,spinor * const Q,
 /*     assign(g_spinor_field[DUM_SOLVER+1], g_spinor_field[DUM_SOLVER], N); */
     for(i = 0; i <= j; i++){
       H[i][j] = scalar_prod(V[i], g_spinor_field[DUM_SOLVER+1], N);
-      /* G and work are in Fortran storage: columns first */
+      /* G, work and work2 are in Fortran storage: columns first */
       G[j][i] = H[i][j];
       work2[j][i] = H[i][j];
       work[i][j].re = H[i][j].re;
@@ -201,7 +202,7 @@ int gmres_dr(spinor * const P,spinor * const Q,
   /* prepare for restart */
   _mult_real(alpha[j], alpha[j], 1./H[j][j].re);
   assign_add_mul(x0, V[j], alpha[j], N);
-  if(g_proc_id == 0) {
+  if(g_proc_id == 0 && g_debug_level > 3) {
     printf("alpha: %e %e\n", alpha[j].re, alpha[j].im);
   }
   for(i = j-1; i >= 0; i--){
@@ -210,59 +211,78 @@ int gmres_dr(spinor * const P,spinor * const Q,
       _diff_complex(alpha[i], tmp1);
     }
     _mult_real(alpha[i], alpha[i], 1./H[i][i].re);
-    if(g_proc_id == 0) {
+    if(g_proc_id == 0 && g_debug_level > 3) {
       printf("alpha: %e %e\n", alpha[i].re, alpha[i].im);
     }
     assign_add_mul(x0, V[i], alpha[i], N);
   }
-  
-  /* r_0=Q-AP  (b=Q, x+0=P) */
-  f(r0, x0);
-  diff(r0, Q, r0, N);
 
-  /* v_0=r_0/||r_0|| */
-  tmp1.re=sqrt(square_norm(r0, N));
-
-  
-  if(g_proc_id == g_stdio_proc && g_debug_level > 0){
-    printf("%d\t%e true residue\n", m, tmp1.re*tmp1.re); 
-    fflush(stdout);
-  }
-  
-  if(tmp1.re < eps_sq){
-    assign(P, x0, N);
-    return(restart*m);
+  /* This produces c=V_m+1*r0 */
+  for(i = 0; i < mp1; i++) {
+    c[i] = scalar_prod(V[i], r0, N); 
+    if(g_proc_id == 0 && g_debug_level > 3) {
+      printf("c: %e %e err = %e\n", c[i].re, c[i].im, err);
+    }
   }
 
+  for(restart = 1; restart < max_restarts; restart++) {  
 
-  /* compute Eigenvalues and vectors */
-  for(i = 0; i < m; i++){
-    c[i].re = 0.;
-    c[i].im = 0.;
-  }
-  c[m-1].re = 1.;
-  _FT(zgesv) (&_m, &one, work[0], &mp1, idx, c, &_m, &info); 
+    /* compute c-\bar H \alpha */
+    _FT(zgemv) ("N", &mp1, &_m, &cmone, G[0], &mp1, alpha, &one, &cpone, c, &one, 1);
+    err = sqrt(short_scalar_prod(c, c, mp1).re);
+    if(g_proc_id == 0 && g_debug_level > 0) {
+      printf("%d\t %e short residue\n", m*restart, err*err);
+    } 
+    
+    /* Compute new residual r0 */
+    /* r_0=Q-AP  (b=Q, x+0=P) */
+    if(g_debug_level > 3) {
+      f(r0, x0);
+      diff(r0, Q, r0, N);
+      tmp1.im=sqrt(square_norm(r0, N));
+      if(g_proc_id == g_stdio_proc){
+	printf("%d\t%e true residue\n", m*restart, tmp1.im*tmp1.im); 
+	fflush(stdout);
+      }
+    }
+    mul(r0, c[0], V[0], N);
+    for(i = 1; i < mp1; i++) {
+      assign_add_mul(r0, V[i], c[i], N);
+    } 
+    if(g_debug_level > 3) {
+      tmp1.im=sqrt(square_norm(r0, N));
+      if(g_proc_id == g_stdio_proc){
+	printf("%d\t%e residue\n", m*restart, tmp1.im*tmp1.im); 
+	fflush(stdout);
+      }
+    }
+    /* Stop if satisfied */
+    if(err < eps){
+      assign(P, x0, N);
+      return(restart*m);
+    }
 
-  /* compute c-\bar H \alpha */
-  work[nr_ev][0].re = err;
-  work[nr_ev][0].im = 0.;
-  err = tmp1.re;
-  for(i=1; i < m+1; i++) {
-    work[nr_ev][i].re = 0.;
-    work[nr_ev][i].im = 0.;
-  }
-  _FT(zgemv) ("N", &mp1, &_m, &cmone, G[0], &mp1, alpha, &one, &cpone, work[nr_ev], &one, 1);
+    /* Prepare to compute harmonic Ritz pairs */
+    for(i = 0; i < m-1; i++){
+      alpha[i].re = 0.;
+      alpha[i].im = 0.;
+    }
+    alpha[m-1].re = 1.;
+    alpha[m-1].im = 0.;
+    _FT(zgesv) (&_m, &one, work[0], &mp1, idx, alpha, &_m, &info); 
+    for(i = 0; i < m; i++) {
+      G[m-1][i].re += (beta2*alpha[idx[i]-1].re);
+      G[m-1][i].im += (beta2*alpha[idx[i]-1].im);
+    }
+    if(g_proc_id == 0 && g_debug_level > 3){
+      printf("zgesv returned info = %d, c[m-1]= %e, %e , idx[m-1]=%d\n", 
+	     info, alpha[idx[m-1]-1].re, alpha[idx[m-1]-1].im, idx[m-1]);
+    }
+    /* c - \bar H * d -> c */
+    /* G contains H + \beta^2 H^-He_n e_n^H */
 
-  G[m-1][m-1].re += (beta2*c[idx[m-1]-1].re);
-  G[m-1][m-1].im += (beta2*c[idx[m-1]-1].im);
-  if(g_proc_id == 0 && g_debug_level > 1){
-    printf("zgesv returned info = %d, c[m-1]= %e, %e , idx[m-1]=%d\n", info, c[idx[m-1]-1].re, c[idx[m-1]-1].im, idx[m-1]);
-  }
-
-
-  for(restart = 1; restart < max_restarts; restart++) {
     /* Compute harmonic Ritz pairs */
-    diagonalise_general_matrix(m, G[0], mp1, c, evalues);
+    diagonalise_general_matrix(m, G[0], mp1, alpha, evalues);
     for(i = 0; i < m; i++) {
       sortarray[i] = _complex_square_norm(evalues[i]);
       idx[i] = i;
@@ -270,59 +290,48 @@ int gmres_dr(spinor * const P,spinor * const Q,
     quicksort(m, sortarray, idx);
     if(g_proc_id == g_stdio_proc && g_debug_level > 1) {
       for(i = 0; i < m; i++) {
-	printf("Evalues %d %e  %e \n", i, evalues[idx[i]].re, evalues[idx[i]].im);
+	printf("# Evalues %d %e  %e \n", i, evalues[idx[i]].re, evalues[idx[i]].im);
       }
       fflush(stdout);
     }
     
     /* Copy the first nr_ev eigenvectors to work */
-    for(i = 0; i < nr_ev; i++) {
+    for(i = 0; i < ne; i++) {
       for(l = 0; l < m; l++) {
 	work[i][l] = G[idx[i]][l];
       }
     }
     /* Orthonormalize them */
-    for(i = 0; i < nr_ev; i++) {
+    for(i = 0; i < ne; i++) {
       work[i][m].re = 0.;
       work[i][m].im = 0.;
-      short_ModifiedGS(work[i], m+1, i, work[0]); 
+      short_ModifiedGS(work[i], m, i, work[0], mp1); 
     }
-    tmp1 = short_scalar_prod(work[nr_ev], work[nr_ev], mp1);
-    if(g_proc_id == 0) {
-      printf("short residue = %e \n", tmp1.re);
-    } 
     /* Orthonormalize c - \bar H d to work */
-    short_ModifiedGS(work[nr_ev], m+1, nr_ev, work[0]); 
-    /* Now compute \bar H = P^T_k+1 \bar H_m P_k */
-/*     for(k = 0; k < ne; k++) { */
-/*       for(l = 0; l < mp1; l++) { */
-/* 	_mult_assign_complex(G[k][l], work2[0][l], work[k][0]); */
-/* 	for(i = 1; i < m; i++) { */
-/* 	  _add_assign_complex(G[k][l], work2[i][l], work[k][i]); */
-/* 	} */
-/*       } */
-/*     } */
-/*     for(k = 0; k < ne; k++) { */
-/*       for(l = 0; l < np1; l++) { */
-/* 	_mult_assign_complex_conj(H[k][l], work[l][0], G[k][0]); */
-/* 	for(i = 1; i < mp1; i++) { */
-/* 	  _add_assign_complex_conj(H[k][l], work[l][i], G[k][i]); */
-/* 	} */
-/*       } */
-/*     } */
+    short_ModifiedGS(c, m+1, ne, work[0], mp1);
     for(i = 0; i < mp1; i++) {
-      for(l = np1; l < mp1; l++) {
+      work[nr_ev][i] = c[i];
+    }
+    /* Now compute \bar H = P^T_k+1 \bar H_m P_k */
+    for(i = 0; i < mp1; i++) {
+      for(l = 0; l < mp1; l++) {
 	H[i][l].re = 0.;
 	H[i][l].im = 0.;
       }
     }    
+
     _FT(zgemm) ("N", "N", &mp1, &ne, &_m, &cpone, work2[0], &mp1, work[0], &mp1, &czero, G[0], &mp1, 1, 1); 
     _FT(zgemm) ("C", "N", &np1, &ne , &mp1, &cpone, work[0], &mp1, G[0], &mp1, &czero, H[0], &mp1, 1, 1);
-    for(i = 0; i < nr_ev+1; i++) {
-      for(l = 0; l < nr_ev+1; l++) {
-	if(g_proc_id == 0 && g_debug_level > 3) {
-	  printf("(g[%d], g[%d]) = %e, %e\n", i, l, short_scalar_prod(work[i], work[l], m+1).re, 
-		 short_scalar_prod(work[i], work[l], m+1).im);
+
+    if(g_debug_level > 3) {
+      for(i = 0; i < ne+1; i++) {
+	for(l = 0; l < ne+1; l++) {
+	  if(g_proc_id == 0) {
+	    printf("(g[%d], g[%d]) = %e, %e\n", i, l, short_scalar_prod(work[i], work[l], m+1).re, 
+		   short_scalar_prod(work[i], work[l], m+1).im);
+	    printf("(g[%d], g[%d]) = %e, %e\n", l, i, short_scalar_prod(work[l], work[i], m+1).re, 
+		   short_scalar_prod(work[l], work[i], m+1).im);
+	  }
 	}
       }
     }
@@ -338,24 +347,19 @@ int gmres_dr(spinor * const P,spinor * const Q,
     for(i = 0; i < np1; i++) {
       assign(V[i], Z[i], N); 
     }
-    f(g_spinor_field[DUM_SOLVER+1], V[0]);
-    mul(g_spinor_field[DUM_SOLVER+3], evalues[idx[0]], V[0], N);
-    diff(g_spinor_field[DUM_SOLVER+3], g_spinor_field[DUM_SOLVER+3], g_spinor_field[DUM_SOLVER+1], N);
-    tmp1.im = square_norm(g_spinor_field[DUM_SOLVER+3], N);
-    if(g_proc_id == 0) {
-      printf("evalue res = %e\n", tmp1.im);
-    }
+    /* Reorthogonalise v_nr_ev */
+    pModifiedGS((complex*)V[nr_ev], _N, nr_ev, (complex*)V[0], V2);  
     if(g_debug_level > 3) {
       for(i = 0; i < np1; i++) {
 	for(l = 0; l < np1; l++) {
-	  alpha[0] = scalar_prod(V[l], V[i], N);
+	  tmp1 = scalar_prod(V[l], V[i], N);
 	  if(g_proc_id == 0) {
-	    printf("(V[%d], V[%d]) = %e %e %d %d %d %d %d %d %e %e\n", l, i, alpha[0].re, alpha[0].im, np1, mp1, ne, _m, _N, V2, H[l][i].re, H[l][i].im);
+	    printf("(V[%d], V[%d]) = %e %e %d %d %d %d %d %d %e %e\n", l, i, tmp1.re, tmp1.im, np1, mp1, ne, _m, _N, V2, H[l][i].re, H[l][i].im);
 	  }
 	}
       }
     }
-
+    /* Copy the content of H to work, work2 and G */
     for(i=0; i < mp1; i++) { 
       for(l = 0; l < mp1; l++) { 
  	G[i][l] = H[i][l];
@@ -365,10 +369,7 @@ int gmres_dr(spinor * const P,spinor * const Q,
       }
     }
 
-
-/*     mul_r(V[0], 1./err, r0, N);  */
-        
-    for(j = nr_ev; j < m; j++) {
+    for(j = ne; j < m; j++) {
       /* g_spinor_field[DUM_SOLVER]=A*v_j */
       f(g_spinor_field[DUM_SOLVER+1], V[j]);
       
@@ -376,15 +377,14 @@ int gmres_dr(spinor * const P,spinor * const Q,
       /* g_spinor_field[DUM_SOLVER+1] <- omega_j */
 /*       assign(g_spinor_field[DUM_SOLVER+1], g_spinor_field[DUM_SOLVER], N); */
       for(i = 0; i <= j; i++){
-	H[j][i] = scalar_prod(V[i], g_spinor_field[DUM_SOLVER+1], N);
-	/* H, G and work are now all in Fortran storage: columns first */
+	H[j][i] = scalar_prod(V[i], g_spinor_field[DUM_SOLVER+1], N);  
+	/* H, G, work and work2 are now all in Fortran storage: columns first */
 	G[j][i] = H[j][i];
 	work2[j][i] = H[j][i];
 	work[i][j].re = H[j][i].re;
 	work[i][j].im = -H[j][i].im;
 	assign_diff_mul(g_spinor_field[DUM_SOLVER+1], V[i], H[j][i], N);
       }
-      
       beta2 = square_norm(g_spinor_field[DUM_SOLVER+1], N);
       _complex_set(H[j][j+1], sqrt(beta2), 0.);
       G[j][j+1] = H[j][j+1];
@@ -393,15 +393,16 @@ int gmres_dr(spinor * const P,spinor * const Q,
       work[j+1][j].im =  -H[j][j+1].im;
       mul_r(V[(j+1)], 1./H[j][j+1].re, g_spinor_field[DUM_SOLVER+1], N);
     }
-    /* This produces c */
-    for(i = 0; i < mp1; i++) {
-      alpha[i] = scalar_prod(V[i], r0, N); 
-      c[i] = alpha[i];
-      if(g_proc_id == 0) {
-	printf("c: %e %e\n", alpha[i].re, alpha[i].im);
+
+    /* Solve the least square problem for alpha*/
+    /* This produces c=V_m+1*r0 */
+    for(i = 0; i < mp1; i++) {      
+      c[i] = scalar_prod(V[i], r0, N);  
+      alpha[i] = c[i];
+      if(g_proc_id == 0 && g_debug_level > 3) {
+	printf("c: %e %e err = %e\n", c[i].re, c[i].im, err);
       }
     }
-    /* Solve the least square problem */
     if(lswork == NULL) {
       lwork = -1;
       _FT(zgels) ("N", &mp1, &_m, &one, H[0], &mp1, alpha, &mp1, &tmp1, &lwork, &info, 1);
@@ -409,59 +410,16 @@ int gmres_dr(spinor * const P,spinor * const Q,
       lswork = (complex*)malloc(lwork*sizeof(complex));
     }
     _FT(zgels) ("N", &mp1, &_m, &one, H[0], &mp1, alpha, &mp1, lswork, &lwork, &info, 1);
-    if(g_proc_id == 0 && g_debug_level > 0) {
+    if(g_proc_id == 0 && g_debug_level > 3) {
       printf("zgels returned info = %d\n", info);
       fflush(stdout);
     }
     /* Compute the new solution vector */
     for(i = 0; i < m; i++){
-      if(g_proc_id == 0) {
+      if(g_proc_id == 0 && g_debug_level > 3) {
 	printf("alpha: %e %e\n", alpha[i].re, alpha[i].im);
       }
       assign_add_mul(x0, V[i], alpha[i], N);
-    }
-    /* compute c-\bar H \alpha */
-    _FT(zgemv) ("N", &mp1, &_m, &cmone, G[0], &mp1, alpha, &one, &cpone, c, &one, 1);
-
-    /* r_0=Q-AP  (b=Q, x+0=P) */
-    f(r0, x0);
-    diff(r0, Q, r0, N);
-    err=sqrt(square_norm(r0, N));
-    if(g_proc_id == 0) {
-      printf("%d\t%e true residue\n", (restart+1)*m, err*err);
-    }
-/*     mul(r0, c[0], V[0], N); */
-/*     for(i = 1; i < mp1; i++) { */
-/*       assign_add_mul(r0, V[i], c[i], N); */
-/*     } */
-/*     _FT(zgemm) ("N", "N", &_N, &one, &mp1, &cpone, (complex*)V[0], &V2, c, &mp1, &czero, (complex*)r0, &V2, 1, 1);   */
-/*     diff(g_spinor_field[DUM_SOLVER+4], g_spinor_field[DUM_SOLVER+4], r0, N); */
-/*     tmp1.im=sqrt(square_norm(r0, N)); */
-/*     err=sqrt(square_norm(r0, N)); */
-    if(g_proc_id == 0) {
-      printf("%d\t%e true residue\n", (restart+1)*m, err*err);
-    }
-    if(err < eps) {
-      assign(P, x0, N);
-      return((restart+1)*m);
-    }
-
-    /* Prepare to compute Eigenvalues and vectors */
-    for(i = 0; i < m; i++){
-      work[nr_ev][i].re = 0.;
-      work[nr_ev][i].im = 0.;
-    }
-    work[nr_ev][m-1].re = 1.;
-
-    _FT(zgesv) (&_m, &one, work[0], &mp1, idx, work[nr_ev], &_m, &info);  
-    G[m-1][m-1].re += (beta2*work[nr_ev][idx[m-1]-1].re);
-    G[m-1][m-1].im += (beta2*work[nr_ev][idx[m-1]-1].im);
-    if(g_proc_id == 0 && g_debug_level > 1){
-      printf("zgesv returned info = %d, c[m-1]= %e, %e , idx[m-1]=%d\n", info, work[nr_ev][idx[m-1]-1].re, work[nr_ev][idx[m-1]-1].im, idx[m-1]);
-    }
-
-    for(i = 0; i < mp1; i++) {
-      work[nr_ev][i] = c[i];
     }
   }
 
@@ -486,15 +444,15 @@ complex short_scalar_prod(complex * const y, complex * const x, const int N) {
 
 }
 
-void short_ModifiedGS(complex v[], int n, int m, complex A[]) {
+void short_ModifiedGS(complex v[], int n, int m, complex A[], int lda) {
 
   int i;
   complex s;
 
   for (i = 0; i < m; i++) {
-    s = short_scalar_prod(A+i*n, v, n);
+    s = short_scalar_prod(A+i*lda, v, n); 
     s.re = -s.re; s.im = -s.im;
-    _FT(zaxpy)(&n, &s, A+i*n, &one, v, &one); 
+    _FT(zaxpy)(&n, &s, A+i*lda, &one, v, &one); 
   }
   s.re = sqrt(short_scalar_prod(v, v, n).re);
   for(i = 0; i < n; i++) {
