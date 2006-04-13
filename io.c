f4555bf52f8a1ea7724c4e77389673951e58b567
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2006-04-13 10:48:51 +0000

    cmi format improved...

--- io.c
+++ io.c
@@ -965,12 +965,12 @@ int write_spinorfield_eo_time_p(spinor * const s, spinor * const r, char * filen
 #ifdef MPI
 	  else{
 	    if(g_cart_id == id){
-#ifndef WORDS_BIGENDIAN
+#  ifndef WORDS_BIGENDIAN
 	      byte_swap_assign(tmp, p + i, sizeof(spinor)/8);
 	      MPI_Send((void*) tmp, sizeof(spinor)/8, MPI_DOUBLE, 0, tag, g_cart_grid);
-#else
+#  else
 	      MPI_Send((void*) (p + i), sizeof(spinor)/8, MPI_DOUBLE, 0, tag, g_cart_grid);
-#endif		  
+#  endif		  
 	    }
 	  }
 #endif
@@ -1224,10 +1224,19 @@ int read_spinorfield_cm_single(spinor * const s, spinor * const r, char * filena
   for(x = 0; x < LX; x++) {
     for(y = 0; y < LY; y++) {
       for(z = 0; z < LZ; z++) {
+#if (defined MPI)
+	fseek(ifs,
+	      (g_proc_coords[0]*T+
+	       (((g_proc_coords[1]*LX+x)*g_nproc_y*LY+g_proc_coords[2]*LY+y)*g_nproc_z*LZ
+		+ g_proc_coords[3]*LZ+z)*T*g_nproc_t)*sizeof(spinor)/2,
+	      SEEK_SET);
+#endif
 	for(t = 0; t < T; t++) {
 
 	  i = g_lexic2eosub[ g_ipt[t][x][y][z] ];
-	  if((t+x+y+z+g_proc_coords[0]*T+g_proc_coords[1]*LX)%2==0) {
+	  if((t+x+y+z+
+	      g_proc_coords[0]*T+g_proc_coords[1]*LX+
+	      g_proc_coords[2]*LY+g_proc_coords[3]*LZ)%2==0) {
 	    p = s;
 	  }
 	  else {
@@ -1258,29 +1267,74 @@ int write_spinorfield_cm_single(spinor * const s, spinor * const r, char * filen
 
   FILE * ofs;
   int t, x, y , z, i = 0;
+  int t0, X, Y, Z, id = 0;
   spinor * p = NULL;
   float tmp[24];
+  int coords[4];
+#ifdef MPI
+  int  tag = 0;
+  MPI_Status status;
+#endif
+  
+  if(g_cart_id == 0) {
+    ofs = fopen(filename, "w");
+  }
 
-  ofs = fopen(filename, "w");
-
-  for(x = 0; x < LX; x++) {
-    for(y = 0; y < LY; y++) {
-      for(z = 0; z < LZ; z++) {
-	for(t = 0; t < T; t++) {
-  	  i = g_lexic2eosub[ g_ipt[t][x][y][z] ];
-	  if((t+x+y+z+g_proc_coords[0]*T+g_proc_coords[1]*LX)%2==0) {
-	    p = s;
+  for(x = 0; x < LX*g_nproc_x; x++) {
+    X = x - LX*g_proc_coords[1];
+    coords[1] = x / LX;
+    for(y = 0; y < LY*g_nproc_y; y++) {
+      Y = y - LY*g_proc_coords[2];
+      coords[2] = y / LY;
+      for(z = 0; z < LZ*g_nproc_z; z++) {
+	Z = z - LZ*g_proc_coords[3];
+	coords[3] = z / LZ;
+	for(t0 = 0; t0 < T*g_nproc_t; t0++) {
+	  t = t0 - T*g_proc_coords[0];
+	  coords[0] = t0 / T;
+#ifdef MPI
+	  MPI_Cart_rank(g_cart_grid, coords, &id);
+#endif
+	  if(g_cart_id == id) {
+	    i = g_lexic2eosub[ g_ipt[t][X][Y][Z] ];
+	    if((t0+x+y+z)%2==0) {
+	      p = s;
+	    }
+	    else {
+	      p = r;
+	    }
 	  }
+	  if(g_cart_id == 0){
+	    if(g_cart_id == id){
+	      double2single_cm(tmp, p + i);
+	    }
+#ifdef MPI
+	    else {
+	      MPI_Recv(tmp, sizeof(spinor)/8, MPI_FLOAT, id, tag, g_cart_grid, &status);
+	    }
+	    fwrite(tmp, sizeof(float), 24, ofs);
+#endif
+	  }
+#ifdef MPI
 	  else {
-	    p = r;
+	    if(g_cart_grid == id) {
+	      double2single_cm(tmp, p + i);
+	      MPI_Send((void*) tmp, sizeof(spinor)/8, MPI_FLOAT, 0, tag, g_cart_grid);
+	    }
 	  }
-	  double2single_cm(tmp, p + i);
-	  fwrite(tmp, sizeof(float), 24, ofs);
+	  tag++;
+#endif
 	}
+#ifdef MPI
+	MPI_Barrier(g_cart_grid); 
+	tag=0;
+#endif
       }
     }
   }
-  fclose(ofs);
+  if(g_cart_id == 0) {
+    fclose(ofs);
+  }
   return(0);
 }
 
