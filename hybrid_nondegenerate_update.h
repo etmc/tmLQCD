b3c95da94468369a8866ebb5de4b7f6157fc084f
Thomas Chiarappa <Thomas.Chiarappa@mib.infn.it>
2006-06-05 12:27:55 +0000

    New hybrid_update file for phmc

--- hybrid_nondegenerate_update.h
+++ hybrid_nondegenerate_update.h
@@ -1,9 +1,16 @@
-#ifndef _HYBRID_NONDEG_UPDATE_H
-#define _HYBRID_NONDEG_UPDATE_H
+#ifndef _HYBRID_NONDEGENERATE_UPDATE_H
+#define _HYBRID_NONDEGENERATE_UPDATE_H
 
+#define first_psf 0
+#define second_psf 1
+#define third_psf 4
 
 /* void update_non_degenerate_fermion_momenta(double step, const int S); */
-void fermion_momenta_nond(double step);
 
+void deri_nondegenerate();
+
+void fermion_momenta_ND(double step);
+
+void leap_frog_ND(double step, int m, int nsmall);
 
 #endif
