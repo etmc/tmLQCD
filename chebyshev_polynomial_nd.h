f5e01279df1f8e14ce6fa2ac72980b319ebf7bfc
Thomas Chiarappa <Thomas.Chiarappa@mib.infn.it>
2006-07-18 13:56:17 +0000

    New header file

--- chebyshev_polynomial_nd.h
+++ chebyshev_polynomial_nd.h
@@ -9,6 +9,8 @@ void chebyshev_coefs(double a, double b, double c[], int n, double exponent);
 
 void QdaggerQ_poly(spinor *R_s, spinor *R_c, double *c, int n, spinor *S_s, spinor *S_c);
 
+double cheb_eval(int M, double *c, double s);
+
 void degree_of_polynomial_nd();
 
 #endif
