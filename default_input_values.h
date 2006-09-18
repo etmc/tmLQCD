d5c48f98992fe49d640cecd09d925cdbb6c5e187
Thomas Chiarappa <Thomas.Chiarappa@mib.infn.it>
2006-09-18 13:12:27 +0000

    default values for Pfirst-, Ptilde- and final H accuracies inserted

--- default_input_values.h
+++ default_input_values.h
@@ -21,6 +21,9 @@
 #define _default_N_PROC_Y 1
 #define _default_N_PROC_Z 1
 #define _default_g_kappa 0.125
+#define _default_g_acc_Pfirst 1.e-02
+#define _default_g_acc_Ptilde 1.e-06
+#define _default_g_acc_Hfin 1.e-04
 #define _default_g_mubar 0.0
 #define _default_g_epsbar 0.0
 #define _default_g_mu 0.0
