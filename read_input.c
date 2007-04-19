632a028deeb81751a1b813218ac6e4c391a60d80
Andreas Nube <andreas.nube@desy.de>
2007-04-19 08:56:38 +0000

    Updated from read_input.l.

--- read_input.c
+++ read_input.c
@@ -9,6 +9,7 @@
 #define YY_FLEX_MINOR_VERSION 5
 
 #include <stdio.h>
+#include <unistd.h>
 
 
 /* cfront 1.2 defines "c_plusplus" instead of "__cplusplus" */
@@ -22,7 +23,6 @@
 #ifdef __cplusplus
 
 #include <stdlib.h>
-#include <unistd.h>
 
 /* Use prototypes in function declarations. */
 #define YY_USE_PROTOS
@@ -2878,7 +2878,7 @@ YY_MALLOC_DECL
 YY_DECL
 	{
 	register yy_state_type yy_current_state;
-	register char *yy_cp, *yy_bp;
+	register char *yy_cp = NULL, *yy_bp = NULL;
 	register int yy_act;
 
 #line 205 "read_input.l"
@@ -4662,7 +4662,7 @@ YY_RULE_SETUP
 #line 890 "read_input.l"
 {
   stilde_min = atof(yytext);
-  if(verbose!=0) printf("Stilde min for PHMC set to %e.\n", stilde_max);
+  if(verbose!=0) printf("Stilde min for PHMC set to %e.\n", stilde_min);
 }
 	YY_BREAK
 case 254:
@@ -5747,6 +5747,7 @@ register char *yy_bp;
 #endif	/* ifndef YY_NO_UNPUT */
 
 
+#ifndef YY_NO_INPUT
 #ifdef __cplusplus
 static int yyinput()
 #else
@@ -5819,7 +5820,7 @@ static int input()
 
 	return c;
 	}
-
+#endif /* YY_NO_INPUT */
 
 #ifdef YY_USE_PROTOS
 void yyrestart( FILE *input_file )
@@ -5930,11 +5931,6 @@ YY_BUFFER_STATE b;
 	}
 
 
-#ifndef YY_ALWAYS_INTERACTIVE
-#ifndef YY_NEVER_INTERACTIVE
-extern int isatty YY_PROTO(( int ));
-#endif
-#endif
 
 #ifdef YY_USE_PROTOS
 void yy_init_buffer( YY_BUFFER_STATE b, FILE *file )
