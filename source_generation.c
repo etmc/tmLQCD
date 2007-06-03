1560d45109d9a235199906646d029f241a36ebaa
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2007-06-03 16:06:10 +0000

    extended pion source

--- source_generation.c
+++ source_generation.c
@@ -15,10 +15,52 @@
 # define M_PI           3.14159265358979323846
 #endif
 
-void extended_pion_only_source(spinor * const P, spinor * const Q,
-			       spinor * const S, spinor * const R,
-			       const int t) {
+void extended_pion_source(spinor * const P, spinor * const Q,
+			  spinor * const S, spinor * const R,
+			  const int t,
+			  const double px, const double py, const double pz) {
+  int lt, lx, ly, lz, i, x, y, z, id=0;
+  int coords[4];
+  spinor * p, * q, r;
+  complex efac;
 
+  zero_spinor_field(P,VOLUME/2);
+  zero_spinor_field(Q,VOLUME/2);
+
+  lt = t - g_proc_coords[0]*T;
+  coords[0] = t / T;
+  for(x = 0; x < LX*g_nproc_x; x++) {
+    lx = x - g_proc_coords[1]*LX;
+    coords[1] = x / LX;
+    for(y = 0; y < LY*g_nproc_y; y++) {
+      ly = y - g_proc_coords[2]*LY;
+      coords[2] = y / LY;
+      for(z = 0; z < LZ*g_nproc_z; z++) {
+	lz = z - g_proc_coords[3]*LZ;
+	coords[3] = z / LZ;
+#ifdef MPI
+	MPI_Cart_rank(g_cart_grid, coords, &id);
+#endif
+	if(g_cart_id == id) {
+	  efac.re= cos(px*x + py*y + pz*z);
+	  efac.im=-sin(px*x + py*y + pz*z);
+
+	  i = g_lexic2eosub[ g_ipt[lt][lx][ly][lz] ];
+	  if((lt+lx+ly+lz+g_proc_coords[3]*LZ+g_proc_coords[2]*LY 
+	      + g_proc_coords[0]*T+g_proc_coords[1]*LX)%2 == 0) {
+	    p = (P + i);
+	    q = (R + i);
+	  }
+	  else {
+	    p = (Q + i);
+	    q = (S + i);
+	  }
+	  _gamma5(r, (*q));
+	  _spinor_mul_complex((*p),efac,r);
+	}
+      }
+    }
+  }
   return;
 }
 
