4a11c380f5d6cd58b16e97573b9334aa3a86290e
Jan Volkholz <Jan.Volkholz@pik-potsdam.de>
2007-07-18 15:15:36 +0000

    *** empty log message ***

--- hybrid_update.c
+++ hybrid_update.c
@@ -10,6 +10,7 @@
 #include "global.h"
 #include "su3.h"
 #include "su3adj.h"
+#include "su3spinor.h"
 #include "expo.h"
 #include "ranlxd.h"
 #include "sse.h"
@@ -27,6 +28,9 @@
 #include "get_staples.h"
 #include "update_backward_gauge.h"
 #include "hybrid_update.h"
+#include "read_input.h"
+#include "stout_smear.h"
+#include "stout_smear_force.h"
 
 extern int ITER_MAX_BCG;
 extern int ITER_MAX_CG;
@@ -38,11 +42,12 @@ extern int ITER_MAX_CG;
  *
  ***********************************/
 
-void gauge_momenta(double step) {
+void gauge_momenta(double step) 
+{
 
-  int i,mu;
+  int i, j, mu;
   static su3 v, w;
-  su3 *z;
+  su3 *z, force;
   static su3adj deriv;
   su3adj *xm;
   static double st, st1;
@@ -55,33 +60,113 @@ void gauge_momenta(double step) {
 
   st  = -step * g_rgi_C0 * g_beta/3.0;
   st1 = -step * g_rgi_C1 * g_beta/3.0;
-  for(i = 0; i < VOLUME; i++){ 
-    for(mu=0;mu<4;mu++){
-      z=&g_gauge_field[i][mu];
-      xm=&moment[i][mu];
-      v=get_staples(i,mu); 
-      _su3_times_su3d(w,*z,v);
-      _trace_lambda(deriv,w);
-      if(g_debug_level > 0) {
-	sum2 = _su3adj_square_norm(deriv);
-	sum+= sum2;
-	if(sum2 > max) max = sum2;
-      }
-      _minus_const_times_mom(*xm,st,deriv);
 
-      if(g_rgi_C1 > 0. || g_rgi_C1 < 0.) {
-	get_rectangle_staples(&v, i, mu);
-	_su3_times_su3d(w, *z, v);
-	_trace_lambda(deriv, w);
-	if(g_debug_level > 0) {
-	  sum2 =_su3adj_square_norm(deriv);
-	  sum1+= sum2;
-	  if(sum2 > max2) max2 = sum2;
-	}
-	_minus_const_times_mom(*xm, st1, deriv);
+  if(use_stout_flag == 1)
+  {
+    /*
+     *  here we smear the gauge field
+     */
+    stout_smear_gauge_field(stout_rho, stout_no_iter);
+    for(i = 0; i < VOLUME; i++)
+    { 
+      for(mu=0;mu<4;mu++)
+      {
+
+        /*
+         *  now we determine \tilde{\Sigma}(x) eqtn(44) hep-lat/0311018 
+         */
+        z=&g_gauge_field[i][mu];
+        xm=&moment[i][mu];
+        v=get_staples(i,mu); 
+        _su3_times_su3d(w,*z,v);
+        _trace_lambda(deriv,w);
+
+        _make_su3(g_stout_force_field[i][mu], deriv);
+
+        /*printf("FORCE x=%d mu=%d:\n", i, mu);
+          printf("DERIV d1=%f d2=%f d3=%f d4=%f d5=%f d6=%f d7=%f d8=%f\n", deriv.d1, deriv.d2, deriv.d3, deriv.d4, deriv.d5, deriv.d6, deriv.d7, deriv.d8);
+          print_su3(&(g_stout_force_field[i][mu]));*/
+      }
+    }
+          stout_smear_force();
+          /*stout_smear_force(i, mu, force);*/
+
+
+
+        for(i = 0; i < VOLUME; i++)
+        { 
+          for(mu=0;mu<4;mu++)
+          {
+
+
+            _trace_lambda(deriv,g_stout_force_field[i][mu]);
+            if(g_debug_level > 0) 
+            {
+              sum2 = _su3adj_square_norm(deriv);
+              sum+= sum2;
+              if(sum2 > max) max = sum2;
+            }
+
+            /*
+             * pi_{x,\mu} = pi_{x,\mu} - \delta S(U) / \delta U
+             */
+            /*_minus_const_times_mom(*xm,st,force);*/
+            if(g_rgi_C1 > 0. || g_rgi_C1 < 0.) {
+              get_rectangle_staples(&v, i, mu);
+              _su3_times_su3d(w, *z, v);
+              _trace_lambda(deriv, w);
+              if(g_debug_level > 0) {
+                sum2 =_su3adj_square_norm(deriv);
+                sum1+= sum2;
+                if(sum2 > max2) max2 = sum2;
+              }
+              _minus_const_times_mom(*xm, st1, deriv);
+            }
+          }
+        }
+  }
+
+  /*
+   *  this is for the case without smearing  
+   */
+  else
+  {
+    for(i = 0; i < VOLUME; i++)
+    { 
+      for(mu=0;mu<4;mu++)
+      {
+        z=&g_gauge_field[i][mu];
+        xm=&moment[i][mu];
+        v=get_staples(i,mu); 
+        _su3_times_su3d(w,*z,v);
+        _trace_lambda(deriv,w);
+        if(g_debug_level > 0) 
+        {
+          sum2 = _su3adj_square_norm(deriv);
+          sum+= sum2;
+          if(sum2 > max) 
+            max = sum2;
+        }
+        _minus_const_times_mom(*xm,st,deriv);
+
+        if(g_rgi_C1 > 0. || g_rgi_C1 < 0.) 
+        {
+          get_rectangle_staples(&v, i, mu);
+          _su3_times_su3d(w, *z, v);
+          _trace_lambda(deriv, w);
+          if(g_debug_level > 0) 
+          {
+            sum2 =_su3adj_square_norm(deriv);
+            sum1+= sum2;
+            if(sum2 > max2) 
+              max2 = sum2;
+          }
+          _minus_const_times_mom(*xm, st1, deriv);
+        }
       }
     }
   }
+
   if(g_debug_level > 0) {
 #ifdef MPI
     MPI_Reduce(&sum, &sum2, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -113,6 +198,7 @@ void gauge_momenta(double step) {
 #endif
 }
 
+/*----------------------------------------------------------------------------*/
 
 /********************************************
  *
@@ -255,12 +341,344 @@ void deri() {
 }
 
 
+/*----------------------------------------------------------------------------*/
+
+/****************************************************************
+ *
+ * Here \delta S_b for the stout smearing iterations is computed
+ *
+ ****************************************************************/
+
+/* input is the pseudo-fermion field */
+void deri_stout_smearing() 
+{
+
+  /*int iter_counter;
+  iter_counter = 0;*/
+
+  printf("Running deri_stout_smearing()\n");
+  printf("g_nr_of_psf = %d\n", g_nr_of_psf);
+  printf("g_rgi_C0 = %f\n", g_rgi_C0);
+  printf("g_rgi_C1 = %f\n", g_rgi_C1);
+  printf("g_beta = %f\n", g_beta);
+
+  /*
+   *  here we smear the gauge field, yielding \tilde{U} 
+   * from eqtn (4) hep-lat/0311018 
+   */
+  stout_smear_gauge_field(stout_rho, stout_no_iter);
+  printf("A\n");
+
+
+  int j, jmax;
+  int i, x, y, mu, matrix_row_index, matrix_column_index;
+  jmax = 1;
+
+  /*
+   *  now we get \tilde{\Sigma}_\mu(x) eqtn (44) hep-lat/0311018 
+   *  we need its explicit form, so we sandwich between spinors with 
+   *  only one explicit entry =1, while all others vanish
+   */
+  for(i=0; i<(VOLUME/2); i++)
+  { 
+    _spinor_null(g_test_spinor_field_left[i]);
+    _spinor_null(g_test_spinor_field_right[i]);
+  } 
+
+  for(y = 0; y < VOLUME/2; y++)
+  {
+    for(matrix_row_index=0; matrix_row_index < 3; matrix_row_index++)
+      for(matrix_column_index=0; matrix_column_index < 3; matrix_column_index++)
+      {
+
+        /*
+         *  here we set the respective test matrix entries to one
+         */
+        if(matrix_column_index == 0)
+        {
+          g_test_spinor_field_left[y].s0.c0.re = 1.0;
+          g_test_spinor_field_left[y].s1.c0.re = 1.0;
+          g_test_spinor_field_left[y].s2.c0.re = 1.0;
+          g_test_spinor_field_left[y].s3.c0.re = 1.0;
+        }
+        if(matrix_row_index == 0)
+        {
+          g_test_spinor_field_right[y].s0.c0.re = 1.0;
+          g_test_spinor_field_right[y].s1.c0.re = 1.0;
+          g_test_spinor_field_right[y].s2.c0.re = 1.0;
+          g_test_spinor_field_right[y].s3.c0.re = 1.0;
+        }
+
+        if(matrix_column_index == 1)
+        {
+          g_test_spinor_field_left[y].s0.c1.re = 1.0;
+          g_test_spinor_field_left[y].s1.c1.re = 1.0;
+          g_test_spinor_field_left[y].s2.c1.re = 1.0;
+          g_test_spinor_field_left[y].s3.c1.re = 1.0;
+        }
+        if(matrix_row_index == 1)
+        {
+          g_test_spinor_field_right[y].s0.c1.re = 1.0;
+          g_test_spinor_field_right[y].s1.c1.re = 1.0;
+          g_test_spinor_field_right[y].s2.c1.re = 1.0;
+          g_test_spinor_field_right[y].s3.c1.re = 1.0;
+        }
+
+        if(matrix_column_index == 2)
+        {
+          g_test_spinor_field_left[y].s0.c2.re = 1.0;
+          g_test_spinor_field_left[y].s1.c2.re = 1.0;              
+          g_test_spinor_field_left[y].s2.c2.re = 1.0;
+          g_test_spinor_field_left[y].s3.c2.re = 1.0;
+        }
+        if(matrix_row_index == 2)
+        {
+          g_test_spinor_field_right[y].s0.c2.re = 1.0;
+          g_test_spinor_field_right[y].s1.c2.re = 1.0;
+          g_test_spinor_field_right[y].s2.c2.re = 1.0;
+          g_test_spinor_field_right[y].s3.c2.re = 1.0;
+        }
+
+#ifdef _KOJAK_INST
+#pragma pomp inst begin(deri)
+#endif
+
+        for(i=0;i<(VOLUME+RAND);i++)
+        { 
+          for(mu=0;mu<4;mu++)
+          { 
+            _zero_su3adj(df0[i][mu]);
+          }
+        }
+
+        jmax=1;
+
+        if(g_nr_of_psf == 2) 
+        {
+          jmax = 3;
+        }
+        if(g_nr_of_psf == 3) 
+        {
+          jmax = 5;
+        }
+
+        for(j=0;j<jmax;j++)
+        { 
+          if(j==0)
+          {
+            g_mu = g_mu1;
+            if(1)
+            {
+              /* 
+               * if CG is used anyhow 
+               */
+              
+              /* 
+               *  first we get 
+               *  Y_o -> DUM_DERI  
+               */
+              gamma5(g_spinor_field[DUM_DERI+1], &(g_test_spinor_field_left[y]), VOLUME/2);
+              count00 += solve_cg(DUM_DERI+1, first_psf, g_eps_sq_force1, g_relative_precision_flag);
+              Qtm_minus_psi(g_spinor_field[DUM_DERI], g_spinor_field[DUM_DERI+1]);
+
+              /* 
+               *  now we get 
+               *  X_o -> DUM_DERI+1  
+               */
+
+              /* 
+               *    
+               *  X_o -> DUM_DERI+1 
+               *  invert Q_{+} Q_{-}  to obtain 
+               */
+              gamma5(g_spinor_field[DUM_DERI+1], &(g_test_spinor_field_right[y]), VOLUME/2);
+              count00 += solve_cg(DUM_DERI+1, first_psf, g_eps_sq_force1, g_relative_precision_flag);
+              
+              
+            }
+            
+            /*
+             * this is never run
+             */
+            else
+            {
+              /*contributions from field 0 -> first_psf*/
+              gamma5(g_spinor_field[DUM_DERI], g_spinor_field[first_psf], VOLUME/2);
+              /* Invert first Q_+ */
+              /* Y_o -> DUM_DERI  */
+              count00 += bicg(DUM_DERI, first_psf, g_eps_sq_force1, g_relative_precision_flag);
+              gamma5(g_spinor_field[DUM_DERI+1], g_spinor_field[DUM_DERI], VOLUME/2);
+              /* Now Q_- */
+              /* X_o -> DUM_DERI+1 */
+              g_mu = -g_mu;
+              count01 += bicg(DUM_DERI+1,DUM_DERI, g_eps_sq_force1, g_relative_precision_flag);
+              g_mu = -g_mu;   
+            }
+          }
+          
+          /*
+           *  these if-cases seem not to be run ever
+           */
+          if(j==1)
+          {
+            /* First term coming from the second field */
+            /* Multiply with W_+ */
+            g_mu = g_mu1;	
+            Qtm_plus_psi(g_spinor_field[DUM_DERI+2], g_spinor_field[second_psf]);
+            g_mu = g_mu2;
+            if(1)
+            {
+              /* If CG is used anyhow */
+              gamma5(g_spinor_field[DUM_DERI+1], g_spinor_field[DUM_DERI+2], VOLUME/2);
+              /* Invert Q_{+} Q_{-} */
+              /* X_W -> DUM_DERI+1 */
+              count10 += solve_cg(DUM_DERI+1, DUM_DERI+2, g_eps_sq_force2, g_relative_precision_flag);
+              /* Y_W -> DUM_DERI  */
+              Qtm_minus_psi(g_spinor_field[DUM_DERI], g_spinor_field[DUM_DERI+1]);
+            }
+            else
+            {
+              gamma5(g_spinor_field[DUM_DERI], g_spinor_field[DUM_DERI+2], VOLUME/2);
+              /* Invert first Q_+ */
+              /* Y_o -> DUM_DERI  */
+              count10 += bicg(DUM_DERI, DUM_DERI+2, g_eps_sq_force2, g_relative_precision_flag);
+              gamma5(g_spinor_field[DUM_DERI+1], g_spinor_field[DUM_DERI], VOLUME/2);
+              /* Now Q_- */
+              /* X_o -> DUM_DERI+1 */
+              g_mu = -g_mu;
+              count11 += bicg(DUM_DERI+1,DUM_DERI, g_eps_sq_force2, g_relative_precision_flag);
+              g_mu = -g_mu;   
+            }
+          }
+          if(j==2)
+          {
+            printf("J=2     %d   %d \n", matrix_row_index, matrix_column_index);
+            /* Second term coming from the second field */
+            /* The sign is opposite!! */
+            mul_r(g_spinor_field[DUM_DERI], -1., g_spinor_field[second_psf], VOLUME/2);
+            g_mu = g_mu1;
+          }
+          if(j == 3) 
+          {
+            printf("J=3     %d   %d \n", matrix_row_index, matrix_column_index);
+            /* First term coming from the second field */
+            /* Multiply with W_+ */
+            g_mu = g_mu2;	
+            Qtm_plus_psi(g_spinor_field[DUM_DERI+2], g_spinor_field[third_psf]);
+            g_mu = g_mu3;
+            if(1)
+            {
+              /* If CG is used anyhow */
+              gamma5(g_spinor_field[DUM_DERI+1], g_spinor_field[DUM_DERI+2], VOLUME/2);
+              /* Invert Q_{+} Q_{-} */
+              /* X_W -> DUM_DERI+1 */
+              count20 += solve_cg(DUM_DERI+1, DUM_DERI+2, g_eps_sq_force3, g_relative_precision_flag);
+              /* Y_W -> DUM_DERI  */
+              Qtm_minus_psi(g_spinor_field[DUM_DERI], g_spinor_field[DUM_DERI+1]);
+            }
+            else
+            {
+              gamma5(g_spinor_field[DUM_DERI], g_spinor_field[DUM_DERI+2], VOLUME/2);
+              /* Invert first Q_+ */
+              /* Y_o -> DUM_DERI  */
+              count20 += bicg(DUM_DERI, DUM_DERI+2, g_eps_sq_force3, g_relative_precision_flag);
+              gamma5(g_spinor_field[DUM_DERI+1], g_spinor_field[DUM_DERI], VOLUME/2);
+              /* Now Q_- */
+              /* X_o -> DUM_DERI+1 */
+              g_mu = -g_mu;
+              count21 += bicg(DUM_DERI+1,DUM_DERI, g_eps_sq_force3, g_relative_precision_flag);
+              g_mu = -g_mu;   
+            }
+          }
+          if(j == 4) 
+          {
+            /* Second term coming from the third field */
+            /* The sign is opposite!! */
+            mul_r( g_spinor_field[DUM_DERI], -1., g_spinor_field[third_psf], VOLUME/2);
+            g_mu = g_mu2;
+          }
+
+          printf("E     %d   %d \n", matrix_row_index, matrix_column_index);
+          /* apply Hopping Matrix M_{eo} */
+          /* to get the even sites of X */
+          H_eo_tm_inv_psi(g_spinor_field[DUM_DERI+2], g_spinor_field[DUM_DERI+1], EO, -1.);
+          /* \delta Q sandwitched by Y_o^\dagger and X_e */
+          deriv_Sb(OE, DUM_DERI, DUM_DERI+2); 
+
+          /* to get the even sites of Y */
+          H_eo_tm_inv_psi(g_spinor_field[DUM_DERI+3], g_spinor_field[DUM_DERI], EO, +1);
+          /* \delta Q sandwitched by Y_e^\dagger and X_o */
+          deriv_Sb(EO, DUM_DERI+3, DUM_DERI+1); 
+          g_mu = g_mu1;
+          printf("Juhu     %d   %d \n", matrix_row_index, matrix_column_index);
+        }
+
+        printf("SU3_CHECK after sandwiching y=%d row_i=%d column_j=%d :\n", y, matrix_row_index, matrix_column_index);
+        /*_su3_assign(&(g_stout_force_field[y][nu]), &(df0[y][nu]));*/
+        /*
+         *  here we reset the test matrix enries to zero
+         */
+        if(matrix_column_index == 0)
+        {
+          g_test_spinor_field_left[y].s0.c0.re = 0.0;
+          g_test_spinor_field_left[y].s1.c0.re = 0.0;
+          g_test_spinor_field_left[y].s2.c0.re = 0.0;
+          g_test_spinor_field_left[y].s3.c0.re = 0.0;
+        }
+        if(matrix_row_index == 0)
+        {
+          g_test_spinor_field_right[y].s0.c0.re = 0.0;
+          g_test_spinor_field_right[y].s1.c0.re = 0.0;
+          g_test_spinor_field_right[y].s2.c0.re = 0.0;
+          g_test_spinor_field_right[y].s3.c0.re = 0.0;
+        }
+        if(matrix_column_index == 1)
+        {
+          g_test_spinor_field_left[y].s0.c1.re = 0.0;
+          g_test_spinor_field_left[y].s1.c1.re = 0.0;
+          g_test_spinor_field_left[y].s2.c1.re = 0.0;
+          g_test_spinor_field_left[y].s3.c1.re = 0.0;
+        }
+        if(matrix_row_index == 1)
+        {
+          g_test_spinor_field_right[y].s0.c1.re = 0.0;
+          g_test_spinor_field_right[y].s1.c1.re = 0.0;
+          g_test_spinor_field_right[y].s2.c1.re = 0.0;
+          g_test_spinor_field_right[y].s3.c1.re = 0.0;
+        }
+
+        if(matrix_column_index == 2)
+        {
+          g_test_spinor_field_left[y].s0.c2.re = 0.0;
+          g_test_spinor_field_left[y].s1.c2.re = 0.0;
+          g_test_spinor_field_left[y].s2.c2.re = 0.0;
+          g_test_spinor_field_left[y].s3.c2.re = 0.0;
+        }
+        if(matrix_row_index == 2)
+        {
+          g_test_spinor_field_right[y].s0.c2.re = 0.0;
+          g_test_spinor_field_right[y].s1.c2.re = 0.0;
+          g_test_spinor_field_right[y].s2.c2.re = 0.0;
+          g_test_spinor_field_right[y].s3.c2.re = 0.0;
+        }
+      }
+  }
+#ifdef _KOJAK_INST
+#pragma pomp inst end(deri)
+#endif
+}
+
+/*------------------------------------------------------------------------*/
+
 void fermion_momenta(double step) {
   int i,mu;
   double tmp;
   su3adj *xm,*deriv;
 
-  deri(); 
+  if(use_stout_flag == 1)
+    deri_stout_smearing(); 
+  else
+    deri(); 
 
 #ifdef MPI
   xchange_deri();
@@ -276,6 +694,8 @@ void fermion_momenta(double step) {
     }
   }
 }
+
+/*----------------------------------------------------------------------------*/
 
 void update_fermion_momenta(double step, const int S) {
   int i,mu;
@@ -324,6 +744,8 @@ void update_fermion_momenta(double step, const int S) {
     }
   }
 }
+
+/*----------------------------------------------------------------------------*/
 
 void update_gauge(double step) {
 
