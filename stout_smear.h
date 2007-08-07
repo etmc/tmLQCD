9a02355b22e7aef8aa29d4b9f05bd9ed1b780152
Jan Volkholz <Jan.Volkholz@pik-potsdam.de>
2007-08-07 10:50:45 +0000

    new version of stout_smear.h

--- stout_smear.h
+++ stout_smear.h
@@ -3,7 +3,9 @@
 #ifndef _STOUT_SMEAR_
 #define _STOUT_SMEAR_
 
-int stout_smear(const double rho , const int no_iters) ; 
+int stout_smear_gauge_field(const double rho , const int no_iters); 
+void  print_su3(su3 *in);
+void  print_spinor(spinor *in); 
 
 #endif
 
