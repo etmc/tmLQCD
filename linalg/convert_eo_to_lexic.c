fd4d95f97cc86321bc8ca24f5570eb8448b05fcb
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2007-08-30 12:00:10 +0000

    conversion from two even odd spinors to a full one

--- linalg/convert_eo_to_lexic.c
+++ linalg/convert_eo_to_lexic.c
@@ -30,7 +30,32 @@ void convert_eo_to_lexic(spinor * const P, spinor * const s, spinor * const r) {
 	  else {
 	    p = r;
 	  }
-	  memcpy((P+ix), p+i, sizeof(spinor));
+	  memcpy((P+ix), (p+i), sizeof(spinor));
+	}
+      }
+    }
+  }
+  return;
+}
+
+void convert_lexic_to_eo(spinor * const s, spinor * const r, spinor * const P) {
+  int x, y, z, t, i, ix;
+  spinor * p = NULL;
+
+  for(x = 0; x < LX; x++) {
+    for(y = 0; y < LY; y++) {
+      for(z = 0; z < LZ; z++) {
+	for(t = 0; t < T; t++) {
+	  ix = g_ipt[t][x][y][z];
+	  i = g_lexic2eosub[ ix ];
+	  if((t+x+y+z+g_proc_coords[3]*LZ+g_proc_coords[2]*LY 
+	      + g_proc_coords[0]*T+g_proc_coords[1]*LX)%2 == 0) {
+	    p = s;
+	  }
+	  else {
+	    p = r;
+	  }
+	  memcpy((p+i), (P+ix), sizeof(spinor));
 	}
       }
     }
