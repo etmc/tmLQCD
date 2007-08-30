020abb5eff265e96b0ae03d949854b21e9e423fa
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2007-08-30 11:58:26 +0000

    conversion from two even odd spinors to a full one

---
+++ linalg/convert_eo_to_lexic.c
@@ -0,0 +1,39 @@
+/* $Id$ */
+
+#ifdef HAVE_CONFIG_H
+# include<config.h>
+#endif
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#ifdef MPI
+# include <mpi.h>
+#endif
+#include "global.h"
+#include "su3.h"
+#include "convert_eo_to_lexic.h"
+
+void convert_eo_to_lexic(spinor * const P, spinor * const s, spinor * const r) {
+  int x, y, z, t, i, ix;
+  spinor * p = NULL;
+
+  for(x = 0; x < LX; x++) {
+    for(y = 0; y < LY; y++) {
+      for(z = 0; z < LZ; z++) {
+	for(t = 0; t < T; t++) {
+	  ix = g_ipt[t][x][y][z];
+	  i = g_lexic2eosub[ ix ];
+	  if((t+x+y+z+g_proc_coords[3]*LZ+g_proc_coords[2]*LY 
+	      + g_proc_coords[0]*T+g_proc_coords[1]*LX)%2 == 0) {
+	    p = s;
+	  }
+	  else {
+	    p = r;
+	  }
+	  memcpy((P+ix), p+i, sizeof(spinor));
+	}
+      }
+    }
+  }
+  return;
+}
---
+++ linalg/convert_eo_to_lexic.h
@@ -0,0 +1,9 @@
+/* $Id$ */
+
+#ifndef _CONVERT_EO_TO_LEXIC_H
+#define _CONVERT_EO_TO_LEXIC_H
+
+void convert_eo_to_lexic(spinor * const P, spinor * const s, spinor * const r);
+void convert_lexic_to_eo(spinor * const s, spinor * const r, spinor * const P);
+
+#endif
