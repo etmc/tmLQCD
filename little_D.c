70999b1853c25b643e584edf46c0fe7223dede9c
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2008-08-19 15:49:10 +0000

    first version of exchange routines for little fields

--- little_D.c
+++ little_D.c
@@ -36,7 +36,7 @@ static int ONE = 1;
 static complex CONE, CZERO, CMONE;
 
 void init_little_field_exchange(complex * w);
-void wait_little_field_exchange(const int i, complex * w);
+void wait_little_field_exchange(const int mu);
 
 
 void little_D(complex * v, complex *w) {
@@ -73,7 +73,7 @@ void little_D(complex * v, complex *w) {
   /* now all non-mpilocal stuff */
   /* start with z direction     */
   for(j = 8; j > -1; j--) {
-    wait_little_field_exchange(j, w);
+    wait_little_field_exchange(j);
     for(i = 0; i < no_blocks; i++) {
       k = g_blocks[i].mpilocal_neighbour[j];
       if(k < 0) {
@@ -83,6 +83,72 @@ void little_D(complex * v, complex *w) {
     }
   }
   
+#endif
+  return;
+}
+
+
+#ifdef MPI
+MPI_Request lrequests[16];
+MPI_Status lstatus[16];
+int waitcount = 0;
+#endif
+
+void init_little_field_exchange(complex * w) {
+  int i = 0, l = 0;
+#ifdef MPI
+#  ifdef PARALLELT
+  int no_dirs = 1;
+#  elif defined PARALLELXT
+  int no_dirs = 2;
+#  elif (defined PARALLELXYT || defined PARALLELXYZT)
+  int no_dirs = 3;
+#  endif
+  if(waitcount != 0) {
+    if(g_proc_id == 0) {
+      fprintf(stderr, "last little_field_exchange not finished! Aborting...\n");
+    }
+    exit(-1);
+  }
+  /* z-dir requires special treatment! */
+  for(i = 0; i < no_dirs; i++) {
+    /* send to the right, receive from the left */
+    MPI_Isend((void*)w, no_blocks*g_N_s, MPI_DOUBLE_COMPLEX, g_nb_list[2*i], 
+	      2*i, g_cart_grid, &lrequests[4*i]);
+    MPI_Irecv((void*)(w + (no_blocks*(i+1)+1)*g_N_s), no_blocks*g_N_s, MPI_DOUBLE_COMPLEX, g_nb_list[2*i+1], 
+	      2*i, g_cart_grid, &lrequests[4*i+1]);
+
+    /* send to the left, receive from the right */
+    MPI_Isend((void*)w, no_blocks*g_N_s, MPI_DOUBLE_COMPLEX, g_nb_list[2*i+1], 
+	      2*i+1, g_cart_grid, &lrequests[4*i+2]);
+    MPI_Irecv((void*)(w + (no_blocks*(i+1))*g_N_s), no_blocks*g_N_s, MPI_DOUBLE_COMPLEX, g_nb_list[2*i], 
+	      2*i+1, g_cart_grid, &lrequests[4*i+3]);
+    waitcount += 4;
+  }
+#  ifdef PARALLELXYZT
+  /* send to the right, receive from the left */
+  i = 3;
+  MPI_Isend((void*)w, g_N_s, MPI_DOUBLE_COMPLEX, g_nb_list[2*i], 
+	    2*i, g_cart_grid, &lrequests[4*i]);
+  MPI_Irecv((void*)(w + (no_blocks*(i+1)+1)*g_N_s), g_N_s, MPI_DOUBLE_COMPLEX, g_nb_list[2*i+1], 
+	    2*i, g_cart_grid, &lrequests[4*i+1]);
+  
+  /* send to the left, receive from the right */
+  MPI_Isend((void*)w, g_N_s, MPI_DOUBLE_COMPLEX, g_nb_list[2*i+1], 
+	    2*i+1, g_cart_grid, &lrequests[4*i+2]);
+  MPI_Irecv((void*)(w + (no_blocks*(i+1))*g_N_s), g_N_s, MPI_DOUBLE_COMPLEX, g_nb_list[2*i], 
+	    2*i+1, g_cart_grid, &lrequests[4*i+3]);
+  waitcount += 4;
+#  endif
+#endif
+  return;
+}
+
+void wait_little_field_exchange(const int mu) {
+
+#ifdef MPI
+  MPI_Waitall(4, &lrequests[4*mu], &lstatus[4*mu]);
+  waitcount -= 4;
 #endif
   return;
 }
