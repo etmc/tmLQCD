d58a28c6e83caf29583c7446d6536314b8dee689
Siebren Reker <siebren.reker@gmail.com>
2008-08-20 16:15:14 +0000

    Added a dummy header file with functions that are 'used' but not actually existing yet.

---
+++ deflation/deflation.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_DEFLATION_H
+#define GUARD_DEFLATION_H
+
+/* NOTE Contains functions that are 'used', but undefined for now.
+   Do with them what is necessary - arguments/return values are undefined as well :) */
+
+void generate_approx_eigenvectors(void *);
+void block_exchange_edges(void *);
+void generate_projectors(void *);
+
+#endif
