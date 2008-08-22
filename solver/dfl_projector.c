b9350a87997e4cf2d7c5cf572754a0b986854941
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2008-08-22 15:47:33 +0000

    some things completed

--- solver/dfl_projector.c
+++ solver/dfl_projector.c
@@ -1,5 +1,23 @@
+/* $Id$ */
+#ifdef HAVE_CONFIG_H
+# include<config.h>
+#endif
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#ifdef MPI
+# include <mpi.h>
+#endif
+#include "global.h"
+#include "complex.h"
+#include "block.h"
+#include "linalg/blas.h"
+#include "D_psi.h"
+#include "little_D.h"
+#include "blocks.h"
 #include "dfl_projector.h"
-#include "../blocks.h"
+
 
 /* Break up full volume spinor to blocks
  * loop over block.basis
@@ -10,19 +28,22 @@
  * create global vector */
 
 void project(spinor * const out, spinor * const in) {
-  int i,j,ctr_t;
+  int i,j,ctr_t, iter;
   spinor **psi;
   int contig_block = LZ / 2;
+  int vol = block_list[0].volume;
   complex *inprod;
   complex *invvec;
 
-  psi = calloc(2, sizeof(spinor*)); /*block local version of global spinor */
+  psi = calloc(4, sizeof(spinor*)); /*block local version of global spinor */
   inprod = calloc(2 * 9 * g_N_s, sizeof(complex)); /*inner product of spinors with bases */
   invvec = calloc(2 * 9 * g_N_s, sizeof(complex)); /*inner product of spinors with bases */
   
   /* no loop below because further down we also don't take this cleanly into account */
-  psi[0] = calloc(VOLUME / 2, sizeof(spinor));
-  psi[1] = calloc(VOLUME / 2, sizeof(spinor));
+  psi[0] = calloc(2*VOLUME, sizeof(spinor));
+  psi[1] = psi[0] + VOLUME/2;
+  psi[2] = psi[0] + VOLUME;
+  psi[3] = psi[0] + 3*VOLUME/2;
 
   /*initialize the local (block) parts of the spinor*/
   for (ctr_t = 0; ctr_t < (VOLUME / LZ); ++ctr_t)
@@ -30,12 +51,33 @@ void project(spinor * const out, spinor * const in) {
     memcpy(psi[0], in + (2 * ctr_t) * contig_block, contig_block * sizeof(spinor));
     memcpy(psi[1], in + (2 * ctr_t + 1) * contig_block, contig_block * sizeof(spinor));
   }
-  for (i = 0; i < 2; ++i) {/* loop over blocks */
+  for (i = 0; i < 2; i++) {/* loop over blocks */
     /* compute inner product */
-    for (j = 0; j < g_N_s; ++j) {/*loop over block.basis */
-      inprod[j + i * 2 * 9 * g_N_s] = block_scalar_prod(block_list[i].basis[j], psi[i], block_list[i].volume);
+    for (j = 0; j < g_N_s; j++) {/*loop over block.basis */
+      inprod[j + i*g_N_s] = block_scalar_prod(block_list[i].basis[j], psi[i], vol);
     }
   }
-  lgcr(invvec, inprod, 10, 1000, 1.e-15, 1, 2 * g_N_s, 2 * 9 * g_N_s, &little_D);
+  iter = lgcr(invvec, inprod, 10, 1000, 1.e-15, 1, 2 * g_N_s, 2 * 9 * g_N_s, &little_D);
+  
+  /* sum up */
+  mul(psi[0], invvec[0], block_list[0].basis[0], vol);
+  mul(psi[1], invvec[g_N_s], block_list[1].basis[0], vol);
+  for(ctr_t = 1; ctr_t < g_N_s; ctr_t++) {
+    assign_add_mul(psi[0], block_list[0].basis[ctr], invvec[ctr_t], vol);
+    assign_add_mul(psi[1], block_list[1].basis[ctr], invvec[ctr_t], vol);
+  }
+  
+  /* reconstruct global field */
+  for (ctr_t = 0; ctr_t < (VOLUME / LZ); ctr_t++) {
+    memcpy(out + (2 * ctr_t) * contig_block, 
+           psi[0] + ctr_t * contig_block,
+	   contig_block * sizeof(spinor));
+    memcpy(out + (2 * ctr_t + 1) * contig_block, 
+           psi[1] + ctr_t * contig_block, 
+	   contig_block * sizeof(spinor));
+  }
 
+  free(psi[0]);
+  free(psi);
+  return;
 }
