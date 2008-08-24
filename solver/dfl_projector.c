c4d89ac6e71a380d18d55f6e489ae0e3cd0cfd51
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2008-08-24 13:52:10 +0000

    projector checking function extended

--- solver/dfl_projector.c
+++ solver/dfl_projector.c
@@ -29,6 +29,7 @@
  * compute sum of basis vectors times complex element
  * create global vector */
 
+/* this is phi_k A^{-1}_{kl} (phi_k, in) */
 void project(spinor * const out, spinor * const in) {
   int i,j,ctr_t, iter;
   spinor **psi;
@@ -79,6 +80,61 @@ void project(spinor * const out, spinor * const in) {
 
   free(psi[0]);
   free(psi);
+  free(invvec);
+  free(inprod);
+  return;
+}
+
+
+void project2(spinor * const out, spinor * const in) {
+  int i,j,ctr_t, iter;
+  spinor **psi;
+  int contig_block = LZ / 2;
+  int vol = block_list[0].volume;
+  complex *inprod;
+  complex *invvec;
+
+  psi = calloc(4, sizeof(spinor*)); /*block local version of global spinor */
+  inprod = calloc(2 * 9 * g_N_s, sizeof(complex)); /*inner product of spinors with bases */
+  
+  /* no loop below because further down we also don't take this cleanly into account */
+  psi[0] = calloc(VOLUME, sizeof(spinor));
+  psi[1] = psi[0] + VOLUME/2;
+
+  /*initialize the local (block) parts of the spinor*/
+  for (ctr_t = 0; ctr_t < (VOLUME / LZ); ctr_t++) {
+    memcpy(psi[0] + ctr_t*contig_block, in + (2 * ctr_t) * contig_block, contig_block * sizeof(spinor));
+    memcpy(psi[1] + ctr_t*contig_block, in + (2 * ctr_t + 1) * contig_block, contig_block * sizeof(spinor));
+  }
+  for (i = 0; i < 2; i++) {/* loop over blocks */
+    /* compute inner product */
+    for (j = 0; j < g_N_s; j++) {/*loop over block.basis */
+      inprod[j + i*g_N_s] = block_scalar_prod(block_list[i].basis[j], psi[i], vol);
+    }
+  }
+  
+  
+  /* sum up */
+  mul(psi[0], inprod[0], block_list[0].basis[0], vol);
+  mul(psi[1], inprod[g_N_s], block_list[1].basis[0], vol);
+  for(ctr_t = 1; ctr_t < g_N_s; ctr_t++) {
+    assign_add_mul(psi[0], block_list[0].basis[ctr_t], inprod[ctr_t], vol);
+    assign_add_mul(psi[1], block_list[1].basis[ctr_t], inprod[g_N_s+ctr_t], vol);
+  }
+  
+  /* reconstruct global field */
+  for (ctr_t = 0; ctr_t < (VOLUME / LZ); ctr_t++) {
+    memcpy(out + (2 * ctr_t) * contig_block, 
+           psi[0] + ctr_t * contig_block,
+	   contig_block * sizeof(spinor));
+    memcpy(out + (2 * ctr_t + 1) * contig_block, 
+           psi[1] + ctr_t * contig_block, 
+	   contig_block * sizeof(spinor));
+  }
+
+  free(psi[0]);
+  free(psi);
+  free(inprod);
   return;
 }
 
@@ -142,5 +198,20 @@ int check_projectors() {
   if(g_proc_id == 0) {
     printf("||P_R^2 psi - P_R psi|| = %1.5e\n", sqrt(nrm));
   }
+
+  project_left(g_spinor_field[DUM_SOLVER+1], g_spinor_field[DUM_SOLVER]);
+  project2(g_spinor_field[DUM_SOLVER+2], g_spinor_field[DUM_SOLVER+1]);
+  nrm = square_norm(g_spinor_field[DUM_SOLVER+2], VOLUME);
+  if(g_proc_id == 0) {
+    printf("||P P_L psi|| = %1.5e\n", sqrt(nrm));
+  }
+
+  project2(g_spinor_field[DUM_SOLVER+1], g_spinor_field[DUM_SOLVER]);
+  project_right(g_spinor_field[DUM_SOLVER+2], g_spinor_field[DUM_SOLVER+1]);
+  nrm = square_norm(g_spinor_field[DUM_SOLVER+2], VOLUME);
+  if(g_proc_id == 0) {
+    printf("||P_R P psi|| = %1.5e\n", sqrt(nrm));
+  }
+
   return(0);
 }
