74d981ef96188f7d2c5d07d05cd7f87b712fd7f2
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2008-09-23 15:39:42 +0000

    Msap preconditioner

--- solver/Msap.c
+++ solver/Msap.c
@@ -17,27 +17,52 @@
 #include "D_psi.h"
 
 
+void dummy_D0(spinor * const P, spinor * const Q) {
+  Block_D_psi(&block_list[0], P, Q);
+  return;
+}
+
+void dummy_D1(spinor * const P, spinor * const Q) {
+  Block_D_psi(&block_list[1], P, Q);
+  return;
+}
+
 void Msap(spinor * const P, spinor * const Q, const int Ncy) {
-  int i, j, blk, ncy, eo, vol;
-  spinor * r, * tmp, * a, * b;
+  int blk, ncy, eo, vol, eolist[2];
+  spinor * r, * a, * b;
 
   r = g_spinor_field[DUM_SOLVER+5];
+  a = g_spinor_field[DUM_SOLVER+6];
+  b = &g_spinor_field[DUM_SOLVER+6][block_list[0].volume + block_list[0].spinpad];
+
+  if(block_list[0].evenodd == 0) {
+    eolist[0] = 0;
+    eolist[1] = 1;
+  }
+  else {
+    eolist[0] = 1;
+    eolist[1] = 0;
+  }
   for(ncy = 0; ncy < Ncy; ncy++) {
     /* even sides first */
     for(eo = 0; eo < 2; eo++) {
+      /* compute the global residue */
       D_psi(r, P);
       diff(r, Q, r, VOLUME);
-      for(blk = 0; blk < 2; blk++) {
-	if(block_list[blk].evenodd == eo) {
-	  vol = block_list[blk].volume;
-	  /* get part of r corresponding to block blk into b */
-	  /* to be done */
-	  /* then invert on b block local*/
-	  mr(a, b, 100, 1.e-31, 1, vol, 0, &Block_D_psi);
-	  /* scale a back to full spinor */
-	  /* to be done */
-	}
+      /* choose the even (odd) block */
+      blk = eolist[eo];
+      vol = block_list[blk].volume;
+      /* get part of r corresponding to block blk into b */
+      copy_global_to_upperlower(b, r, blk);
+      /* then invert on b block local*/
+      if(eolist[eo] == 0) {
+	mr(a, b, 100, 1.e-31, 1, vol, 0, &dummy_D0);
       }
+      else {
+	mr(a, b, 100, 1.e-31, 1, vol, 0, &dummy_D1);
+      }
+      /* scale a back to full spinor P */
+      copy_upperlower_to_global(P, a, blk);
     }
   }
   return;
