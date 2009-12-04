839fb1f8bdc073f5cd6cbbdb75d7a5a88b933075
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2009-12-04 15:59:15 +0000

    ...

---
+++ operator/hopping_sgl.c
@@ -0,0 +1,289 @@
+/**********************************************************************
+ *
+ * $Id$
+ *
+ * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008 Carsten Urbach
+ *
+ * BG and halfspinor versions (C) 2007, 2008 Carsten Urbach
+ *
+ * This file is based on an implementation of the Dirac operator 
+ * written by Martin Luescher, modified by Martin Hasenbusch in 2002 
+ * and modified and extended by Carsten Urbach from 2003-2008
+ *
+ * This file is part of tmLQCD.
+ *
+ * tmLQCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * tmLQCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with tmLQCD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **********************************************************************/
+
+static su3_vector32 psi1, psi2, psi, chi, phi1, phi3;
+
+/* l output , k input*/
+/* for ieo=0, k resides on  odd sites and l on even sites */
+void Hopping_Matrix(int ieo, spinor32 * const l, spinor32 * const k){
+  int ix,iy;
+  int ioff,ioff2,icx,icy;
+  su332 * restrict up, * restrict um;
+  spinor32 * restrict r, * restrict sp, * restrict sm;
+  spinor32 temp;
+
+#ifdef _GAUGE_COPY
+  if(g_update_gauge_copy) {
+    update_backward_gauge();
+  }
+#endif
+
+  /* for parallelization */
+#    if (defined MPI && !(defined _NO_COMM))
+  xchange_field(k, ieo);
+#    endif
+
+  if(k == l){
+    printf("Error in H_psi (simple.c):\n");
+    printf("Arguments k and l must be different\n");
+    printf("Program aborted\n");
+    exit(1);
+  }
+  if(ieo == 0){
+    ioff = 0;
+  } 
+  else{
+    ioff = (VOLUME+RAND)/2;
+  } 
+  ioff2 = (VOLUME+RAND)/2-ioff;
+  /**************** loop over all lattice sites ****************/
+
+  for (icx = ioff; icx < (VOLUME/2 + ioff); icx++){
+    ix=g_eo2lexic[icx];
+
+    r=l+(icx-ioff);
+
+    /*********************** direction +0 ************************/
+    iy=g_iup[ix][0]; icy=g_lexic2eosub[iy];
+
+
+    sp=k+icy;
+#    if ((defined _GAUGE_COPY))
+    up=&g_gauge_field_copy[icx][0];
+#    else
+    up=&g_gauge_field[ix][0];
+#    endif
+      
+    _vector_add(psi,(*sp).s0,(*sp).s2);
+
+    _su3_multiply(chi,(*up),psi);
+    _complex_times_vector(psi,ka0,chi);
+      
+    _vector_assign(temp.s0,psi);
+    _vector_assign(temp.s2,psi);
+
+    _vector_add(psi,(*sp).s1,(*sp).s3);
+
+    _su3_multiply(chi,(*up),psi);
+    _complex_times_vector(psi,ka0,chi);
+            
+    _vector_assign(temp.s1,psi);
+    _vector_assign(temp.s3,psi);
+
+    /*********************** direction -0 ************************/
+
+    iy=g_idn[ix][0]; icy=g_lexic2eosub[iy];
+
+    sm=k+icy;
+#    if ((defined _GAUGE_COPY))
+    um = up+1;
+#    else
+    um=&g_gauge_field[iy][0];
+#    endif
+
+    _vector_sub(psi,(*sm).s0,(*sm).s2);
+
+    _su3_inverse_multiply(chi,(*um),psi);
+    _complexcjg_times_vector(psi,ka0,chi);
+
+    _vector_add_assign(temp.s0,psi);
+    _vector_sub_assign(temp.s2,psi);
+
+    _vector_sub(psi,(*sm).s1,(*sm).s3);
+
+    _su3_inverse_multiply(chi,(*um),psi);
+    _complexcjg_times_vector(psi,ka0,chi);
+      
+    _vector_add_assign(temp.s1,psi);
+    _vector_sub_assign(temp.s3,psi);
+
+    /*********************** direction +1 ************************/
+
+    iy=g_iup[ix][1]; icy=g_lexic2eosub[iy];
+
+    sp=k+icy;
+
+#    if ((defined _GAUGE_COPY))
+    up=um+1;
+#    else
+    up+=1;
+#    endif
+      
+    _vector_i_add(psi,(*sp).s0,(*sp).s3);
+
+    _su3_multiply(chi,(*up),psi);
+    _complex_times_vector(psi,ka1,chi);
+
+    _vector_add_assign(temp.s0,psi);
+    _vector_i_sub_assign(temp.s3,psi);
+
+    _vector_i_add(psi,(*sp).s1,(*sp).s2);
+
+    _su3_multiply(chi,(*up),psi);
+    _complex_times_vector(psi,ka1,chi);
+
+    _vector_add_assign(temp.s1,psi);
+    _vector_i_sub_assign(temp.s2,psi);
+
+    /*********************** direction -1 ************************/
+
+    iy=g_idn[ix][1]; icy=g_lexic2eosub[iy];
+
+    sm=k+icy;
+#    ifndef _GAUGE_COPY
+    um=&g_gauge_field[iy][1];
+#    else
+    um=up+1;
+#    endif
+
+    _vector_i_sub(psi,(*sm).s0,(*sm).s3);
+
+    _su3_inverse_multiply(chi,(*um),psi);
+    _complexcjg_times_vector(psi,ka1,chi);
+
+    _vector_add_assign(temp.s0,psi);
+    _vector_i_add_assign(temp.s3,psi);
+
+    _vector_i_sub(psi,(*sm).s1,(*sm).s2);
+
+    _su3_inverse_multiply(chi,(*um),psi);
+    _complexcjg_times_vector(psi,ka1,chi);
+
+    _vector_add_assign(temp.s1,psi);
+    _vector_i_add_assign(temp.s2,psi);
+
+    /*********************** direction +2 ************************/
+
+    iy=g_iup[ix][2]; icy=g_lexic2eosub[iy];
+
+    sp=k+icy;
+#    if ((defined _GAUGE_COPY))
+    up=um+1;
+#    else
+    up+=1;
+#    endif 
+    _vector_add(psi,(*sp).s0,(*sp).s3);
+
+    _su3_multiply(chi,(*up),psi);
+    _complex_times_vector(psi,ka2,chi);
+
+    _vector_add_assign(temp.s0,psi);
+    _vector_add_assign(temp.s3,psi);
+
+    _vector_sub(psi,(*sp).s1,(*sp).s2);
+
+    _su3_multiply(chi,(*up),psi);
+    _complex_times_vector(psi,ka2,chi);
+      
+    _vector_add_assign(temp.s1,psi);
+    _vector_sub_assign(temp.s2,psi);
+
+
+    /*********************** direction -2 ************************/
+
+    iy=g_idn[ix][2]; icy=g_lexic2eosub[iy];
+
+    sm=k+icy;
+#    ifndef _GAUGE_COPY
+    um = &g_gauge_field[iy][2];
+#    else
+    um = up +1;
+#    endif
+
+    _vector_sub(psi,(*sm).s0,(*sm).s3);
+
+    _su3_inverse_multiply(chi,(*um),psi);
+    _complexcjg_times_vector(psi,ka2,chi);
+
+    _vector_add_assign(temp.s0,psi);
+    _vector_sub_assign(temp.s3,psi);
+
+    _vector_add(psi,(*sm).s1,(*sm).s2);
+
+    _su3_inverse_multiply(chi,(*um),psi);
+    _complexcjg_times_vector(psi,ka2,chi);
+      
+    _vector_add_assign(temp.s1,psi);
+    _vector_add_assign(temp.s2,psi);
+
+    /*********************** direction +3 ************************/
+
+    iy=g_iup[ix][3]; icy=g_lexic2eosub[iy];
+
+    sp=k+icy;
+#    if ((defined _GAUGE_COPY))
+    up=um+1;
+#    else
+    up+=1;
+#    endif 
+    _vector_i_add(psi,(*sp).s0,(*sp).s2);
+      
+    _su3_multiply(chi,(*up),psi);
+    _complex_times_vector(psi,ka3,chi);
+
+    _vector_add_assign(temp.s0,psi);
+    _vector_i_sub_assign(temp.s2,psi);
+
+    _vector_i_sub(psi,(*sp).s1,(*sp).s3);
+
+    _su3_multiply(chi,(*up),psi);
+    _complex_times_vector(psi,ka3,chi);
+
+    _vector_add_assign(temp.s1,psi);
+    _vector_i_add_assign(temp.s3,psi);
+
+    /*********************** direction -3 ************************/
+
+    iy=g_idn[ix][3]; icy=g_lexic2eosub[iy];
+
+    sm=k+icy;
+#    ifndef _GAUGE_COPY
+    um = &g_gauge_field[iy][3];
+#    else
+    um = up+1;
+#    endif
+
+    _vector_i_sub(psi,(*sm).s0,(*sm).s2);
+
+    _su3_inverse_multiply(chi,(*um),psi);
+    _complexcjg_times_vector(psi,ka3,chi);
+      
+    _vector_add((*r).s0, temp.s0, psi);
+    _vector_i_add((*r).s2, temp.s2, psi);
+
+    _vector_i_add(psi,(*sm).s1,(*sm).s3);
+
+    _su3_inverse_multiply(chi,(*um),psi);
+    _complexcjg_times_vector(psi,ka3,chi);
+
+    _vector_add((*r).s1, temp.s1, psi);
+    _vector_i_sub((*r).s3, temp.s3, psi);
+    /************************ end of loop ************************/
+  }
+}
---
+++ operator/hopping_sse_sgl.c
@@ -0,0 +1,441 @@
+/**********************************************************************
+ *
+ * $Id$
+ *
+ * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008 Carsten Urbach
+ *
+ * BG and halfspinor versions (C) 2007, 2008 Carsten Urbach
+ *
+ * This file is based on an implementation of the Dirac operator 
+ * written by Martin Luescher, modified by Martin Hasenbusch in 2002 
+ * and modified and extended by Carsten Urbach from 2003-2008
+ *
+ * This file is part of tmLQCD.
+ *
+ * tmLQCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * tmLQCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with tmLQCD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **********************************************************************/
+
+
+/* input on k; output on l */
+void Hopping_Matrix(const int ieo, spinor * const l, spinor * const k){
+  int icx,icy,icz,ioff,ioff2;
+  int ix,iy,iz;
+  su3 *restrict up;
+  su3 * restrict um;
+  spinor * restrict sp;
+  spinor * restrict sm;
+  spinor * restrict rn;
+  static spinor rs;
+  
+  /* for parallelization */
+#ifdef _GAUGE_COPY
+  if(g_update_gauge_copy) {
+    update_backward_gauge();
+  }
+#endif
+
+#    if (defined MPI && !defined _NO_COMM)
+  xchange_field(k, ieo);
+#    endif
+
+  if(k == l){
+    printf("Error in subroutine D_psi: improper arguments\n");
+    printf("Program aborted\n");
+    exit(1);
+  }
+  if(ieo == 0){
+    ioff = 0;
+  } 
+  else{
+    ioff = (VOLUME+RAND)/2;
+  }
+  ioff2 = (VOLUME+RAND)/2-ioff;
+
+  ix=g_eo2lexic[ioff];
+  iy=g_iup[ix][0]; 
+  icy=g_lexic2eosub[iy];
+
+  sp=k+icy;
+#    if ((defined _GAUGE_COPY))
+  up=&g_gauge_field_copy[ioff][0];
+#    else
+  up=&g_gauge_field[ix][0];
+#    endif
+   
+  /**************** loop over all lattice sites ******************/
+  for(icx = ioff; icx < (VOLUME/2+ioff); icx++){
+    ix=g_eo2lexic[icx];
+    /*********************** direction +0 ************************/
+
+    iy=g_idn[ix][0]; icy=g_lexic2eosub[iy];
+
+    sm=k+icy;
+    _prefetch_spinor(sm);
+
+#    if ((defined _GAUGE_COPY))
+    um=up+1;
+#    else
+    um=&g_gauge_field[iy][0]; 
+#    endif
+    _prefetch_su3(um);
+      
+    _sse_load((*sp).s0);
+    _sse_load_up((*sp).s2);
+    _sse_vector_add();
+
+    _sse_su3_multiply((*up));
+    _sse_vector_cmplx_mul(ka0);
+    _sse_store_up(rs.s0);
+    _sse_store_up(rs.s2);      
+      
+    _sse_load((*sp).s1);
+    _sse_load_up((*sp).s3);
+    _sse_vector_add();
+      
+    _sse_su3_multiply((*up));
+    _sse_vector_cmplx_mul(ka0);
+    _sse_store_up(rs.s1);
+    _sse_store_up(rs.s3); 
+
+    /*********************** direction -0 ************************/
+
+    iy=g_iup[ix][1]; icy=g_lexic2eosub[iy];
+      
+    sp=k+icy;
+    _prefetch_spinor(sp);
+
+#    if ((defined _GAUGE_COPY))
+    up = um + 1;
+#    else
+    up+=1;
+#    endif
+    _prefetch_su3(up);
+
+    _sse_load((*sm).s0);
+    _sse_load_up((*sm).s2);
+    _sse_vector_sub();
+      
+    _sse_su3_inverse_multiply((*um));
+    _sse_vector_cmplxcg_mul(ka0);
+      
+    _sse_load(rs.s0);
+    _sse_vector_add();
+    _sse_store(rs.s0);
+
+    _sse_load(rs.s2);
+    _sse_vector_sub();
+    _sse_store(rs.s2);
+      
+    _sse_load((*sm).s1);
+    _sse_load_up((*sm).s3);
+    _sse_vector_sub();
+      
+    _sse_su3_inverse_multiply((*um));
+    _sse_vector_cmplxcg_mul(ka0);
+      
+    _sse_load(rs.s1);
+    _sse_vector_add();
+    _sse_store(rs.s1);
+
+    _sse_load(rs.s3);
+    _sse_vector_sub();
+    _sse_store(rs.s3);
+      
+    /*********************** direction +1 ************************/
+
+    iy=g_idn[ix][1]; icy=g_lexic2eosub[iy];
+
+    sm=k+icy;
+    _prefetch_spinor(sm);
+
+#    ifndef _GAUGE_COPY
+    um=&g_gauge_field[iy][1]; 
+#    else
+    um=up+1;
+#    endif
+    _prefetch_su3(um);
+
+    _sse_load((*sp).s0);
+    _sse_load_up((*sp).s3);
+    _sse_vector_i_mul();
+    _sse_vector_add();
+
+    _sse_su3_multiply((*up));
+    _sse_vector_cmplx_mul(ka1);
+
+    _sse_load(rs.s0);
+    _sse_vector_add();
+    _sse_store(rs.s0);
+
+    _sse_load(rs.s3);
+    _sse_vector_i_mul();      
+    _sse_vector_sub();
+    _sse_store(rs.s3); 
+      
+    _sse_load((*sp).s1);
+    _sse_load_up((*sp).s2);
+    _sse_vector_i_mul();
+    _sse_vector_add();
+
+    _sse_su3_multiply((*up));
+    _sse_vector_cmplx_mul(ka1);
+
+    _sse_load(rs.s1);
+    _sse_vector_add();
+    _sse_store(rs.s1);
+
+    _sse_load(rs.s2);
+    _sse_vector_i_mul();      
+    _sse_vector_sub();
+    _sse_store(rs.s2);       
+
+    /*********************** direction -1 ************************/
+
+    iy=g_iup[ix][2]; icy=g_lexic2eosub[iy];
+
+    sp=k+icy;
+    _prefetch_spinor(sp);
+
+#    if ((defined _GAUGE_COPY))
+    up = um + 1;
+#    else
+    up+=1;
+#    endif
+    _prefetch_su3(up);
+
+    _sse_load((*sm).s0);
+    _sse_load_up((*sm).s3);
+    _sse_vector_i_mul();
+    _sse_vector_sub();
+      
+    _sse_su3_inverse_multiply((*um));
+    _sse_vector_cmplxcg_mul(ka1);
+      
+    _sse_load(rs.s0);
+    _sse_vector_add();
+    _sse_store(rs.s0);
+
+    _sse_load(rs.s3);
+    _sse_vector_i_mul();      
+    _sse_vector_add();
+    _sse_store(rs.s3);
+
+    _sse_load((*sm).s1);
+    _sse_load_up((*sm).s2);
+    _sse_vector_i_mul();
+    _sse_vector_sub();
+      
+    _sse_su3_inverse_multiply((*um));
+    _sse_vector_cmplxcg_mul(ka1);
+      
+    _sse_load(rs.s1);
+    _sse_vector_add();
+    _sse_store(rs.s1);
+
+    _sse_load(rs.s2);
+    _sse_vector_i_mul();      
+    _sse_vector_add();
+    _sse_store(rs.s2);
+
+    /*********************** direction +2 ************************/
+
+    iy=g_idn[ix][2]; icy=g_lexic2eosub[iy];
+
+    sm=k+icy;
+    _prefetch_spinor(sm);
+
+#    ifndef _GAUGE_COPY
+    um=&g_gauge_field[iy][2]; 
+#    else
+    um=up+1;
+#    endif
+    _prefetch_su3(um);
+
+    _sse_load((*sp).s0);
+    _sse_load_up((*sp).s3);
+    _sse_vector_add();
+
+    _sse_su3_multiply((*up));
+    _sse_vector_cmplx_mul(ka2);
+
+    _sse_load(rs.s0);
+    _sse_vector_add();
+    _sse_store(rs.s0);
+
+    _sse_load(rs.s3);
+    _sse_vector_add();
+    _sse_store(rs.s3);
+      
+    _sse_load((*sp).s1);
+    _sse_load_up((*sp).s2);
+    _sse_vector_sub();
+
+    _sse_su3_multiply((*up));
+    _sse_vector_cmplx_mul(ka2);
+
+    _sse_load(rs.s1);
+    _sse_vector_add();
+    _sse_store(rs.s1);
+
+    _sse_load(rs.s2);
+    _sse_vector_sub();
+    _sse_store(rs.s2);      
+
+    /*********************** direction -2 ************************/
+
+    iy=g_iup[ix][3]; icy=g_lexic2eosub[iy];
+
+    sp=k+icy;
+    _prefetch_spinor(sp);
+
+#    if ((defined _GAUGE_COPY))
+    up = um + 1;
+#    else
+    up+=1;
+#    endif
+    _prefetch_su3(up);
+
+    _sse_load((*sm).s0);
+    _sse_load_up((*sm).s3);
+    _sse_vector_sub();
+      
+    _sse_su3_inverse_multiply((*um));
+    _sse_vector_cmplxcg_mul(ka2);
+      
+    _sse_load(rs.s0);
+    _sse_vector_add();
+    _sse_store(rs.s0);
+
+    _sse_load(rs.s3);
+    _sse_vector_sub();
+    _sse_store(rs.s3);
+      
+    _sse_load((*sm).s1);
+    _sse_load_up((*sm).s2);
+    _sse_vector_add();
+      
+    _sse_su3_inverse_multiply((*um));
+    _sse_vector_cmplxcg_mul(ka2);
+      
+    _sse_load(rs.s1);
+    _sse_vector_add();
+    _sse_store(rs.s1);
+
+    _sse_load(rs.s2);
+    _sse_vector_add();
+    _sse_store(rs.s2);      
+      
+    /*********************** direction +3 ************************/
+
+    iy=g_idn[ix][3]; icy=g_lexic2eosub[iy];
+
+    sm=k+icy;
+    _prefetch_spinor(sm);
+
+#    ifndef _GAUGE_COPY
+    um=&g_gauge_field[iy][3]; 
+#    else
+    um=up+1;
+#    endif
+    _prefetch_su3(um);
+
+    _sse_load((*sp).s0);
+    _sse_load_up((*sp).s2);
+    _sse_vector_i_mul();
+    _sse_vector_add();
+
+    _sse_su3_multiply((*up));
+    _sse_vector_cmplx_mul(ka3);
+
+    _sse_load(rs.s0);
+    _sse_vector_add();
+    _sse_store(rs.s0);
+
+    _sse_load(rs.s2);
+    _sse_vector_i_mul();      
+    _sse_vector_sub();
+    _sse_store(rs.s2);
+      
+    _sse_load((*sp).s1);
+    _sse_load_up((*sp).s3);
+    _sse_vector_i_mul();
+    _sse_vector_sub();
+
+    _sse_su3_multiply((*up));
+    _sse_vector_cmplx_mul(ka3);
+
+    _sse_load(rs.s1);
+    _sse_vector_add();
+    _sse_store(rs.s1);
+
+    _sse_load(rs.s3);
+    _sse_vector_i_mul();      
+    _sse_vector_add();
+    _sse_store(rs.s3);
+      
+    /*********************** direction -3 ************************/
+
+    icz=icx+1;
+    if(icz==((VOLUME+RAND)/2+ioff)) icz=ioff;
+    iz=g_eo2lexic[icz];
+    iy=g_iup[iz][0]; icy=g_lexic2eosub[iy];
+      
+    sp=k+icy;
+    _prefetch_spinor(sp);
+
+#    if ((defined _GAUGE_COPY))
+    up=&g_gauge_field_copy[icz][0];
+#    else
+    up=&g_gauge_field[iz][0];
+#    endif
+    _prefetch_su3(up);
+
+    _sse_load((*sm).s0);
+    _sse_load_up((*sm).s2);
+    _sse_vector_i_mul();
+    _sse_vector_sub();
+      
+    _sse_su3_inverse_multiply((*um));
+    _sse_vector_cmplxcg_mul(ka3);
+
+    rn=l+(icx-ioff);
+      
+    _sse_load(rs.s0);
+    _sse_vector_add();
+    _sse_store_nt((*rn).s0);
+
+    _sse_load(rs.s2);
+    _sse_vector_i_mul();      
+    _sse_vector_add();
+    _sse_store_nt((*rn).s2);
+
+    _sse_load((*sm).s1);
+    _sse_load_up((*sm).s3);
+    _sse_vector_i_mul();
+    _sse_vector_add();
+      
+    _sse_su3_inverse_multiply((*um));
+    _sse_vector_cmplxcg_mul(ka3);
+
+    _sse_load(rs.s1);
+    _sse_vector_add();
+    _sse_store_nt((*rn).s1);
+
+    _sse_load(rs.s3);
+    _sse_vector_i_mul();      
+    _sse_vector_sub();
+    _sse_store_nt((*rn).s3);
+  }
+}
