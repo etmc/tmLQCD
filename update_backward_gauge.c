6a381eebe3cb392ef5de657f855cc37a9b668b9d
Luigi Scorzato <scorzato@ect.it>
2010-02-08 20:07:13 +0000

    update  g_gauge_copys/t instead of g_gauge_copy when _USE_TSPLITPAR.

--- update_backward_gauge.c
+++ update_backward_gauge.c
@@ -56,6 +56,52 @@ void update_backward_gauge() {
   return;
 }
 
+#elif _USE_TSPLITPAR 
+
+void update_backward_gauge() {
+  int ix=0, kb=0, kb2=0;
+
+  for(ix = 0; ix < VOLUME/2;ix++) {
+    kb2=g_eo2lexic[ix];
+    _su3_assign(g_gauge_field_copyt[ix][0],g_gauge_field[kb2][0]);
+    kb=g_idn[g_eo2lexic[ix]][0];
+    _su3_assign(g_gauge_field_copyt[ix][1],g_gauge_field[kb][0]);
+
+    _su3_assign(g_gauge_field_copys[ix][0],g_gauge_field[kb2][1]);
+    kb=g_idn[g_eo2lexic[ix]][1];
+    _su3_assign(g_gauge_field_copys[ix][1],g_gauge_field[kb][1]);
+
+    _su3_assign(g_gauge_field_copys[ix][2],g_gauge_field[kb2][2]);
+    kb=g_idn[g_eo2lexic[ix]][2];
+    _su3_assign(g_gauge_field_copys[ix][3],g_gauge_field[kb][2]);
+
+    _su3_assign(g_gauge_field_copys[ix][4],g_gauge_field[kb2][3]);
+    kb=g_idn[g_eo2lexic[ix]][3];
+    _su3_assign(g_gauge_field_copys[ix][5],g_gauge_field[kb][3]);
+  }
+  for(ix = (VOLUME+RAND)/2; ix < (VOLUME+RAND)/2+VOLUME/2;ix++) {
+    kb2=g_eo2lexic[ix];
+    _su3_assign(g_gauge_field_copyt[ix][0],g_gauge_field[kb2][0]);
+    kb=g_idn[g_eo2lexic[ix]][0];
+    _su3_assign(g_gauge_field_copyt[ix][1],g_gauge_field[kb][0]);
+
+    _su3_assign(g_gauge_field_copys[ix][0],g_gauge_field[kb2][1]);
+    kb=g_idn[g_eo2lexic[ix]][1];
+    _su3_assign(g_gauge_field_copys[ix][1],g_gauge_field[kb][1]);
+
+    _su3_assign(g_gauge_field_copys[ix][2],g_gauge_field[kb2][2]);
+    kb=g_idn[g_eo2lexic[ix]][2];
+    _su3_assign(g_gauge_field_copys[ix][3],g_gauge_field[kb][2]);
+
+    _su3_assign(g_gauge_field_copys[ix][4],g_gauge_field[kb2][3]);
+    kb=g_idn[g_eo2lexic[ix]][3];
+    _su3_assign(g_gauge_field_copys[ix][5],g_gauge_field[kb][3]);
+  }
+  g_update_gauge_copys = 0;
+  g_update_gauge_copyt = 0;
+  return;
+}
+
 #else
 
 void update_backward_gauge() {
