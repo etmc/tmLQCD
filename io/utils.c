9d96c975fc452ab5dc73aacec6957c546400890d
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2010-03-10 10:16:13 +0000

    ...

--- io/utils.c
+++ io/utils.c
@@ -167,16 +167,7 @@ void byte_swap_assign_single2double(void * out_ptr, void * in_ptr, int nmemb){
 #else 
 
 void byte_swap_assign_single2double(void * out_ptr, void * in_ptr, int nmemb){
-  int i;
-  float * float_ptr = (float*) in_ptr;
-  double * double_ptr = (double*) out_ptr;
-
-  for(i = 0; i < nmemb; i++) {
-    (*double_ptr) = (double) (*float_ptr);
-
-    float_ptr++;
-    double_ptr++;
-  }
+  single2double(out_ptr, in_ptr, nmemb);
   return;
 }
 
@@ -212,16 +203,7 @@ void byte_swap_assign_double2single(void * out_ptr, void * in_ptr, int nmemb){
 #else
 
 void byte_swap_assign_double2single(void * out_ptr, void * in_ptr, int nmemb){
-  int i;
-  float * float_ptr = (float*) out_ptr;
-  double * double_ptr = (double*) in_ptr;
-
-  for(i = 0; i < nmemb; i++) {
-    (*float_ptr) = (float) (*double_ptr);
-
-    float_ptr++;
-    double_ptr++;
-  }
+  double2single(out_ptr, in_ptr, nmemb);
   return;
 }
 #endif
