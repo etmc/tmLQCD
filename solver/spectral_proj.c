4c2ac44072dc71c7aebff6b9cfac21fdf445383e
Elena Garcia Ramos <elenagr@ifh.de>
2011-03-15 14:43:12 +0000

    the name has been changed from mode_number.c to spectral_proj.c a new function top_sus() has been added and mode_number() has been simplified

---
+++ solver/spectral_proj.c
@@ -0,0 +1,102 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "global.h"
+#include "start.h"
+#include "su3.h"
+#include "linalg_eo.h"
+#include "chebyshev_polynomial_nd.h"
+#include <io/eospinor.h>
+#include "solver/solver.h"
+#include "solver/jdher.h"
+#include "solver/eigenvalues.h"
+#include "X_psi.h"
+#include "gamma.h"
+#include "P_M_eta.h"
+#include "spectral_proj.h"
+
+double mode_n;
+
+double mode_number(spinor * const S, double const mstarsq){
+
+  spinor **s,*s_;
+
+  s_ = calloc(2*VOLUMEPLUSRAND+1, sizeof(spinor));
+  s  = calloc(2, sizeof(spinor*));
+
+  for(int i = 0; i < 2; i++) {
+#if (defined SSE3 || defined SSE2 || defined SSE)
+    s[i] = (spinor*)(((unsigned long int)(s_)+ALIGN_BASE)&~ALIGN_BASE)+i*VOLUMEPLUSRAND;
+#else
+    s[i] = s_+i*VOLUMEPLUSRAND;
+#endif
+}
+
+  /* Computing P_M = h(X)^2 */
+  h_X_sqr_eta(s[0],s[1],S,mstarsq);
+  
+  /* Computing the mode number  nu = (|eta>,P_M|eta>)=||h(X)^2|eta>||^2 */
+  /* being |eta> the stochastic source */
+
+  mode_n=square_norm(s[1], VOLUME, 1); 
+
+  if(g_proc_id == 0) {
+  printf("The Value of the Mode Number is %f \n", mode_n);
+  }
+
+  return mode_n;
+
+  free(s);
+  free(s_);
+}
+
+
+void top_sus(spinor * const S, double const mstarsq){
+  
+  double mode_num, topo_sus = 0.0;
+  double A = 0.0, B = 0.0, C = 0.0;
+  spinor **s, *s_;
+
+  s_ = calloc(25*VOLUMEPLUSRAND+1, sizeof(spinor));
+  s  = calloc(25, sizeof(spinor*));
+
+  for(int i = 0; i < 25; i++) {
+#if (defined SSE3 || defined SSE2 || defined SSE)
+    s[i] = (spinor*)(((unsigned long int)(s_)+ALIGN_BASE)&~ALIGN_BASE)+i*VOLUMEPLUSRAND;
+#else
+    s[i] = s_+i*VOLUMEPLUSRAND;
+#endif
+}
+
+  /* s[0]=h(X)|eta>  s[2]=h(X)^2|eta>*/
+  h_X_sqr_eta(s[0],s[2],S,mstarsq);
+
+  /* s[2]=[gamma5 h(X)]|eta>*/
+  gamma5(s[1],s[0], VOLUME);
+
+  /* s[3]=[h(X) gamma5 h(X)}|eta> */
+  h_X_eta(s[3], s[1], mstarsq);
+
+
+  /* A = (h(X)^2|eta>,h(X)^2|eta>) */
+
+  A=scalar_prod_r(s[2],s[2], VOLUME, 1); 
+
+  /* B = ([h(X) gamma5 h(X)]|eta>,[h(X) gamma5 h(X)]|eta>)*/
+  B=scalar_prod_r(s[3],s[3], VOLUME, 1); 
+  
+  /* C = ([h(X)]|eta>,[gamma5 h(X)]|eta>) */
+  C=scalar_prod_r(s[0],s[1], VOLUME, 1);
+
+
+  if(g_proc_id == 0) {
+  printf("A = %f \n", A);
+  printf("B = %f \n", B);
+  printf("C = %f \n", C);
+  printf("C^2 = %f \n", C*C);
+  }
+
+  free(s);
+  free(s_);
+}
+
