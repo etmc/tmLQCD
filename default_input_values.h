d7614c20b737a535303bfbc18d70a5e4d2587c56
Carsten Urbach <urbach@hiskp.uni-bonn.de>
2011-11-09 05:17:08 +0000

    default value for csw set to 1

--- default_input_values.h
+++ default_input_values.h
@@ -51,6 +51,7 @@
 #define _default_g_mu1 0.0
 #define _default_g_mu2 0.0
 #define _default_g_mu3 0.0
+#define _default_c_sw 1.0
 #define _default_g_beta 6.0
 #define _default_g_N_s 20
 #define _default_g_dflgcr_flag 0
