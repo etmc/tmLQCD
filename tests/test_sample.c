0c8a3f81715990721b65a599ac76fe7fecaa923a
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2012-01-26 16:06:44 +0100

    Added skeleton unit testing framework based on CU (http://cu.danfis.cz/about)

---
+++ tests/test_sample.c
@@ -0,0 +1,13 @@
+#include "test_sample_ts1.h"
+
+TEST_SUITES {
+  TEST_SUITE_ADD(TS1),
+  TEST_SUITES_CLOSURE
+};
+
+int main(int argc, char *argv[]) {
+  CU_SET_OUT_PREFIX("regressions/");
+  CU_RUN(argc,argv);
+
+  return 0;
+}
---
+++ tests/test_sample_ts1.h
@@ -0,0 +1,14 @@
+#include <cu/cu.h>
+
+/* test declarations, definitions in %.c */
+TEST(test_true);
+TEST(test_false);
+TEST(test_fail);
+
+/* define test suite (enum type thing) */
+TEST_SUITE(TS1) {
+  TEST_ADD(test_true),
+  TEST_ADD(test_false),
+  TEST_ADD(test_fail),
+  TEST_SUITE_CLOSURE
+};
