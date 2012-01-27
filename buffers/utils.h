3ab3b0afe689a8ea539d477f63d61db1182e7570
Albert Deuzeman <deuzeman@itp.unibe.ch>
2012-01-27 16:15:08 +0100

    Have the buffers subdirectory be compiled, as it is actively used now.

--- buffers/utils.h
+++ buffers/utils.h
@@ -2,9 +2,11 @@
 
 #include <buffers/gauge.h>
 
-void copy_gauge_field(gauge_field_t dest, gauge_field_t orig)
+void copy_gauge_field(gauge_field_t dest, gauge_field_t orig);
+
 void generic_exchange(void *field_in, int bytes_per_site);
 void exchange_gauge_field(gauge_field_t target);
+void exchange_gauge_field_array(gauge_field_array_t target);
 
 inline void copy_gauge_field(gauge_field_t dest, gauge_field_t orig)
 {
@@ -14,4 +16,10 @@ inline void copy_gauge_field(gauge_field_t dest, gauge_field_t orig)
 inline void exchange_gauge_field(gauge_field_t target)
 {
   generic_exchange((void*)target.field, sizeof(su3_tuple));
+}
+
+inline void exchange_gauge_field_array(gauge_field_array_t target)
+{
+  for (unsigned int idx = 0; idx < target.length; ++idx)
+    exchange_gauge_field(target.field_array[idx]);
 }
