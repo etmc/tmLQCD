1fb4ad384df2862e984db8e6c9cee7d67060db37
Carsten Urbach <curbach@gmx.de>
2012-01-29 18:43:32 +0100

    the cloverdet monomial

---
+++ cloverdet_monomial.h
@@ -0,0 +1,30 @@
+/***********************************************************************
+ * 
+ * Copyright (C) 2008 Carsten Urbach
+ *
+ * This file is part of tmLQCD.
+ *
+ * tmLQCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * tmLQCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with tmLQCD.  If not, see <http://www.gnu.org/licenses/>.
+ ***********************************************************************/
+
+#ifndef _CLOVERDET_MONOMIAL_H
+#define _CLOVERDET_MONOMIAL_H
+
+#include "hamiltonian_field.h"
+
+void cloverdet_derivative(const int no, hamiltonian_field_t * const hf);
+void cloverdet_heatbath(const int no, hamiltonian_field_t * const hf);
+double cloverdet_acc(const int no, hamiltonian_field_t * const hf);
+
+#endif
