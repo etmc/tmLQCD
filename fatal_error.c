87de58e9107b15b02f9fb8b3e6a884f641a8b869
Albert Deuzeman <deuzeman@itp.unibe.ch>
2012-01-30 13:11:47 +0100

    Added an implementation of a generic exit-with-error system.

---
+++ fatal_error.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <global.h>
+
+void fatal_error(char const *error, char const *function)
+{
+  int rank;
+  if (error != NULL)
+  {
+    fprintf(stderr, "FATAL ERROR\n");
+    if (function != NULL)
+    {
+#ifdef MPI
+      MPI_Get_rank(&rank);
+      fprintf(stderr, "  Within %s (reported by node %d):\n", function, rank);
+#else
+      fprintf(stderr, "  Within %s:\n", function);
+#endif
+    }
+    fprintf(stderr, "    %s\n", error);
+    fflush(stderr);
+  }
+  
+#ifdef MPI
+  MPI_Abort(MPI_COMM_WORLD, 1);
+  MPI_Finalize();
+#endif
+
+  exit(500);
+}
