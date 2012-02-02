861ea922eb3afaf8597f2e0eeb5513f08fdf9ca5
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2012-02-02 13:46:43 +0100

    Reorganized Makefiles for unit tests and added test for exponentiation of su3 matrices. (as inspired by Albert's test case)

--- tests/test_su3_algebra.h
+++ tests/test_su3_algebra.h
@@ -1,9 +1,11 @@
-#include "../cu/cu.h"
+#include <cu/cu.h>
 
 TEST(su3_assign);
+TEST(su3_expo_positivedet);
 
 TEST_SUITE(SU3_ALGEBRA){
   TEST_ADD(su3_assign),
+  TEST_ADD(su3_expo_positivedet),
   TEST_SUITE_CLOSURE
 };
 
