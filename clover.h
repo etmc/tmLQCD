231c1416dc7e5fdf02317dcff4f58455001b2f68
Carsten Urbach <curbach@gmx.de>
2012-02-13 12:13:21 +0100

    added a proper inversion routine for the CLOVER operator plus a sample input file for this case
    again, it works for CSW=0 and mu>0 or CSW>0 and mu = 0, but it doesn't work
    with both together yet. So, I suspect that sw_invert has some problem
    in that situation...

--- clover.h
+++ clover.h
@@ -28,6 +28,11 @@ extern su3 *** sw;
 extern su3 *** sw_inv;
 extern su3 ** swm, ** swp;
 
+void assign_mul_one_sw_pm_imu(const int ieo, spinor * const k, spinor * const l, const double mu);
+void assign_mul_one_sw_pm_imu_inv(const int ieo, spinor * const k, spinor * const l, const double mu);
+void Msw_full(spinor * const Even_new, spinor * const Odd_new, 
+	      spinor * const Even, spinor * const Odd);
+void clover_inv(const int ieo, spinor * const l, const double mu);
 void Qsw_psi(spinor * const l, spinor * const k);
 void Qsw_plus_psi(spinor * const l, spinor * const k);
 void Qsw_minus_psi(spinor * const l, spinor * const k);
