9b25b56fe120560473c354bd99ea91cd587be74d
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2012-03-23 11:39:09 +0100

    get_staples thread-safety

--- get_staples.c
+++ get_staples.c
@@ -39,7 +39,13 @@ su3 get_staples(int x, int mu, su3 ** in_gauge_field) {
   double _Complex w00, w01, w02, w10, w11, w12, w20;
   double _Complex reg00, reg01, reg02, reg10, reg11, reg12, reg20, reg21, reg22;
   int k, iy;
+
+#ifndef OMP
   static su3 v;
+#else
+  su3 v;
+#endif
+  
   su3 *w1 ALIGN;
   su3 *w2 ALIGN;
   su3 *w3 ALIGN;
@@ -85,7 +91,13 @@ su3 get_staples(int x, int mu, su3 ** in_gauge_field) {
 su3 get_staples(int x, int mu, su3 ** in_gauge_field) {
 
   int k,iy;
+
+#ifndef OMP
   static su3 v,st;
+#else
+  su3 v,st;
+#endif 
+    
   su3 *w1,*w2,*w3;
 #ifdef _KOJAK_INST
 #pragma pomp inst begin(staples)
