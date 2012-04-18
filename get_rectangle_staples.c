e0b3c598286764b5c19ee9cc331894dc1b3b9657
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2012-04-18 14:02:44 +0200

    more elegant thread-safety of get_rectangle_staples

--- get_rectangle_staples.c
+++ get_rectangle_staples.c
@@ -27,11 +27,16 @@
 #include "get_rectangle_staples.h"
 
 void get_rectangle_staples(su3 * const v, const int x, const int mu) {
-#ifndef OMP
-  static su3 tmp1, tmp2;
-#else
-  su3 tmp1, tmp2;
+#ifdef OMP
+#define static
 #endif
+
+  static su3 tmp1, tmp2;
+
+#ifdef OMP
+#undef static
+#endif  
+  
   int y, z, nu;
   su3 * a, * b, * c, * d, * e;
 #ifdef _KOJAK_INST
