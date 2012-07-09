975aa433250f8249ae2fe9352bbb894a754456d8
Carsten Urbach <curbach@gmx.de>
2012-07-09 19:18:57 +0200

    more functions towards a BG/Q version with intrinsics

--- bgq.h
+++ bgq.h
@@ -1,9 +1,10 @@
 #ifndef _BGQ_H
 #define _BGQ_H
 
-#define regtype vector4double
+//#define regtype vector4double
 
 inline void vec_load2(vector4double * r, su3_vector * const phi) {
+#pragma disjoint(*r,*phi)
   r[0] = vec_ld2(0, (double*) &phi->c0); 
   r[1] = vec_ld2(0, (double*) &phi->c1);
   r[2] = vec_ld2(0, (double*) &phi->c2);
@@ -11,6 +12,7 @@ inline void vec_load2(vector4double * r, su3_vector * const phi) {
 }
 
 inline void vec_store2(su3_vector * const phi, vector4double * r) {
+#pragma disjoint(*r,*phi)
   vec_st2(r[0], 0, (double*) &phi->c0);
   vec_st2(r[1], 0, (double*) &phi->c1);
   vec_st2(r[2], 0, (double*) &phi->c2);
@@ -26,30 +28,76 @@ inline void vec_add2(vector4double * restrict r, vector4double * restrict s) {
   return;
 }
 
-inline void vec_cmplx_mul_double2(complex double * restrict c, vector4double * restrict r, vector4double * restrict tmp) {
-  __alignx(32, c);
-  tmp[0] = vec_ld2(0, (double*) c);
+inline void vec_sub2(vector4double * restrict r, vector4double * restrict s) {
+#pragma disjoint(*s, *r)
 #pragma unroll(6)
   for(int i = 0; i < 6; i++) {
-    r[i] = vec_xmul(r[6+i], tmp[0]);
-  }
-#pragma unroll(6)
-  for(int i = 0; i < 6; i++) {
-    r[i] = vec_xxnpmadd(tmp[0], r[6+i], r[i]);
+    r[i] = vec_sub(r[i], s[i]);
   }
   return;
 }
+
+inline void vec_i_mul_add(vector4double * restrict r, vector4double * restrict s, 
+			  vector4double * restrict tmp) {
+#pragma disjoint(*s, *r)
+#pragma disjoint(*s, *tmp)
+#pragma disjoint(*r, *tmp)
+  tmp[0] = vec_splats(1.);
+#pragma unroll(6)
+  for(int i = 0; i < 6; i++) {
+    r[i] = vec_xxnpmadd(s[i], tmp[0], r[i]);
+  }
+  return;
+}
+
+inline void vec_cmplx_mul_double2(vector4double * restrict rs, vector4double * restrict r,
+				  vector4double * tmp, complex double * c) {
+#pragma disjoint(*rs, *r)
+#pragma disjoint(*r, *tmp)
+#pragma disjoint(*tmp, rs)
+  __alignx(32, c);
+  tmp[0] = vec_ld2(0, (double*) c);
+#pragma unroll(3)
+  for(int i = 0; i < 6; i++) {
+    rs[i] = vec_xmul(r[i], tmp[0]);
+  }
+#pragma unroll(3)
+  for(int i = 0; i < 6; i++) {
+    rs[i] = vec_xxnpmadd(tmp[0], r[i], rs[i]);
+  }
+  return;
+}
+
+inline void vec_cmplxcg_mul_double2(vector4double * restrict rs, vector4double * restrict r,
+				    vector4double * tmp, complex double * c) {
+#pragma disjoint(*rs, *r)
+#pragma disjoint(*r, *tmp)
+#pragma disjoint(*tmp, rs)
+  __alignx(32, c);
+  tmp[0] = vec_ld2(0, (double*) c);
+#pragma unroll(3)
+  for(int i = 0; i < 6; i++) {
+    rs[i] = vec_xmul(tmp[0], r[i]);
+  }
+#pragma unroll(3)
+  for(int i = 0; i < 6; i++) {
+    rs[i] = vec_xxcpnmadd(r[i], tmp[0], rs[i]);
+  }
+  return;
+}
+
 
 // multiplies one su3 matrix with two su3_vectors
 // the first of which stored in r[0-2]
 // and the second one in r[3-5]
 //
 // the resulting two vectors are stored in
-// r[0-5]
+// r[6-11]
 //
 // this routine uses only half of the 4 doubles in vector4double
 
-inline void vec_su3_multiply_double2(su3 * const restrict u, vector4double * restrict U, vector4double * restrict r) {
+inline void vec_su3_multiply_double2(su3 * const restrict u, vector4double * restrict U, 
+				     vector4double * restrict r) {
 #pragma disjoint(*U, *r)
   __alignx(32, u);
   __alignx(32, U);
@@ -97,7 +145,67 @@ inline void vec_su3_multiply_double2(su3 * const restrict u, vector4double * res
   return;
 }
 
+// multiplies the inverse of one su3 matrix with two su3_vectors
+// the first of which stored in r[0-2]
+// and the second one in r[3-5]
+//
+// the resulting two vectors are stored in
+// r[6-11]
+//
+// this routine uses only half of the 4 doubles in vector4double
 
+inline void vec_su3_inverse_multiply_double2(su3 * const restrict u, vector4double * restrict U, 
+					     vector4double * restrict r) {
+#pragma disjoint(*U, *r)
+  __alignx(32, u);
+  __alignx(32, U);
+  __alignx(32, r);
+
+  U[0] = vec_ld2(0, (double*) &u->c00);
+  U[1] = vec_ld2(0, (double*) &u->c01);
+  U[2] = vec_ld2(0, (double*) &u->c02);
+#pragma unroll(3)
+  for(int i = 0; i < 3; i++) {
+    r[6+i] = vec_xmul(U[i], r[0]);
+    r[9+i] = vec_xmul(U[i], r[3]);
+  }
+#pragma unroll(3)
+  for(int i = 0; i < 3; i++) {
+    r[6+i] = vec_xxcpnmadd(r[0], U[i], r[6+i]);
+    r[9+i] = vec_xxcpnmadd(r[3], U[i], r[9+i]);
+  }
+  U[0] = vec_ld2(0, (double*) &u->c10);
+  U[1] = vec_ld2(0, (double*) &u->c11);
+  U[2] = vec_ld2(0, (double*) &u->c12);
+#pragma unroll(3)
+  for(int i = 0; i < 3; i++) {
+    r[6+i] = vec_xmadd(U[i], r[1], r[6+i]);
+    r[9+i] = vec_xmadd(U[i], r[4], r[9+i]);
+  }
+#pragma unroll(3)
+  for(int i = 0; i < 3; i++) {
+    r[6+i] = vec_xxcpnmadd(r[1], U[i], r[6+i]);
+    r[9+i] = vec_xxcpnmadd(r[4], U[i], r[9+i]);
+  }
+  U[0] = vec_ld2(0, (double*) &u->c20);
+  U[1] = vec_ld2(0, (double*) &u->c21);
+  U[2] = vec_ld2(0, (double*) &u->c22);
+#pragma unroll(3)
+  for(int i = 0; i < 3; i++) {
+    r[6+i] = vec_xmadd(U[i], r[2], r[6+i]);
+    r[9+i] = vec_xmadd(U[i], r[5], r[9+i]);
+  }
+#pragma unroll(3)
+  for(int i = 0; i < 3; i++) {
+    r[6+i] = vec_xxcpnmadd(r[2], U[i], r[6+i]);
+    r[9+i] = vec_xxcpnmadd(r[5], U[i], r[9+i]);
+  }
+  return;
+}
+
+
+// alternative implementation
+//
 // might not be optimal for pipeline as result is 
 // re-used in the next line.
 inline void vec_su3_multiply_double2b(su3 * const u, vector4double * U, vector4double * r) {
---
+++ operator/hopping_bgq_dbl.c
@@ -0,0 +1,161 @@
+/**********************************************************************
+ *
+ *
+ * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008 Carsten Urbach
+ *
+ * BG and halfspinor versions (C) 2007, 2008 Carsten Urbach
+ *
+ * This file is based on an implementation of the Dirac operator 
+ * written by Martin Luescher, modified by Martin Hasenbusch in 2002 
+ * and modified and extended by Carsten Urbach from 2003-2008
+ *
+ * This file is part of tmLQCD.
+ *
+ * tmLQCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * tmLQCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with tmLQCD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **********************************************************************/
+
+
+{
+  int icx,icy,icz,ioff,ioff2;
+  int ix,iy,iz;
+  su3 * restrict up ALIGN;
+  su3 * restrict um ALIGN;
+  spinor * restrict sp ALIGN;
+  spinor * restrict sm ALIGN;
+  spinor * restrict rn ALIGN;
+  /* We have 32 registers available */
+  vector4double r[12];
+  vector4double U[9];
+  /* The following contains the result spinor */
+  vector4double rs[12];
+
+#pragma disjoint(*sp, *sm, *rn, *up, *um, *l, *k)
+
+  __alignx(16,l);
+  __alignx(16,k);
+
+#ifdef _GAUGE_COPY
+  if(g_update_gauge_copy) {
+    update_backward_gauge(g_gauge_field);
+  }
+#endif
+
+#    if (defined MPI && !(defined _NO_COMM))
+  xchange_field(k, ieo);
+#    endif
+
+  if(ieo == 0){
+    ioff = 0;
+  } 
+  else{
+    ioff = (VOLUME+RAND)/2;
+  }
+  ioff2 = (VOLUME+RAND)/2-ioff;
+
+  ix=g_eo2lexic[ioff];
+  iy=g_iup[ix][0]; 
+  icy=g_lexic2eosub[iy];
+
+  sp=k+icy;
+
+#    if ((defined _GAUGE_COPY))
+  up=&g_gauge_field_copy[ioff][0];
+#    else
+  up=&g_gauge_field[ix][0];
+#    endif
+  /**************** loop over all lattice sites ******************/
+  for(icx = ioff; icx < (VOLUME/2+ioff); icx++){
+    rn=l+(icx-ioff);
+    ix=g_eo2lexic[icx];
+    /*********************** direction +t ************************/
+    iy=g_idn[ix][0]; 
+    icy=g_lexic2eosub[iy];
+#    if (!defined _GAUGE_COPY)
+    um=&g_gauge_field[iy][0]; 
+#    else
+    um=up+1;
+#    endif
+    sm=k+icy;
+
+    vec_load2(r, &sp->s0);
+    vec_load2(r+3, &sp->s1);
+    vec_load2(r+6, &sp->s2);
+    vec_load2(r+9, &sp->s3);
+    // s0 + s2 and s1 + s3
+    vec_add2(r, r, &r[6]);
+    // result is now in r[0-5] 
+    vec_su3_multiply_double(up, U, r);
+    // result is now in r[6-11]
+    // mult with ka0 and store in rs
+    vec_cmplx_mul_double2(rs, &r[6], U, &ka0);
+    rs[6] = rs[0]; rs[7] = rs[1]; rs[8] = rs[2];
+    rs[9] = rs[3]; rs[10]= rs[4]; rs[11]= rs[5];
+
+    /*********************** direction -t ************************/
+
+    iy=g_iup[ix][1]; 
+    icy=g_lexic2eosub[iy];
+
+#    if ((defined _GAUGE_COPY))
+    up=um+1;
+#    else
+    up+=1;
+#    endif
+    sp=k+icy;
+    vec_load2(r, &sm->s0);
+    vec_load2(r+3, &sm->s1);
+    vec_load2(r+6, &sm->s2);
+    vec_load2(r+9, &sm->s3);
+    // s0 - s2 and s1 - s3
+    vec_sub2(r, r, &r[6]);
+    // result is now in r[0-5]
+    vec_su3_inverse_multiply_double2(um, U, r);
+    // result is now in r[6-11]
+    // mult with k0
+    vec_cmplxcg_mul_double2(r, &r[6], U, &ka0);
+    // result in r[0-5] now
+    vec_add2(rs, rs, r);
+    vec_sub2(&rs[6], &rs[6], r);
+
+    /*********************** direction +1 ************************/
+
+    iy=g_idn[ix][1]; 
+    icy=g_lexic2eosub[iy];
+
+#    ifndef _GAUGE_COPY
+    um=&g_gauge_field[iy][1]; 
+#    else
+    um = up+1;
+#    endif
+    sm=k+icy;
+
+    vec_load2(r, &sp->s0);
+    vec_load2(r+3, &sp->s1);
+    vec_load2(r+9, &sp->s2);
+    vec_load2(r+6, &sp->s3);
+    vec_i_mul_add(r, &r[6], U);
+    vec_su3_multiply_double2(up, U, r);
+    vec_cmplx_mul_double2(r, &r[6], U, &ka1);
+    vec_add2(rs, rs, r);
+    _bgl_add_to_rs0_reg0();
+    _bgl_i_mul_sub_from_rs3_reg0();
+    _bgl_add_to_rs1_reg1();
+    _bgl_i_mul_sub_from_rs2_reg1();
+
+
+  }
+
+
+}
