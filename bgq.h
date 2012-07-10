e4fbbddfdc62eddc616d4d50582d9daa50e8cb4f
Carsten Urbach <curbach@gmx.de>
2012-07-10 18:07:21 +0200

    loops unrolled by hand...

--- bgq.h
+++ bgq.h
@@ -22,40 +22,44 @@ inline void vec_store2(su3_vector * const phi, vector4double * r) {
 // r = r + s
 inline void vec_add2(vector4double * restrict r, vector4double * restrict s) {
 #pragma disjoint(*s, *r)
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[i] = vec_add(r[i], s[i]);
-  }
+  r[0] = vec_add(r[0], s[0]);
+  r[1] = vec_add(r[1], s[1]);
+  r[2] = vec_add(r[2], s[2]);
   return;
 }
 
 // r = r + s
 inline void vec_add_double2(vector4double * restrict r, vector4double * restrict s) {
 #pragma disjoint(*s, *r)
-#pragma unroll(6)
-  for(int i = 0; i < 6; i++) {
-    r[i] = vec_add(r[i], s[i]);
-  }
+  r[0] = vec_add(r[0], s[0]);
+  r[1] = vec_add(r[1], s[1]);
+  r[2] = vec_add(r[2], s[2]);
+  r[3] = vec_add(r[3], s[3]);
+  r[4] = vec_add(r[4], s[4]);
+  r[5] = vec_add(r[5], s[5]);
+
   return;
 }
 
 // r = r - s
 inline void vec_sub2(vector4double * restrict r, vector4double * restrict s) {
 #pragma disjoint(*s, *r)
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[i] = vec_sub(r[i], s[i]);
-  }
+  r[0] = vec_sub(r[0], s[0]);
+  r[1] = vec_sub(r[1], s[1]);
+  r[2] = vec_sub(r[2], s[2]);
   return;
 }
 
 // r = r - s
 inline void vec_sub_double2(vector4double * restrict r, vector4double * restrict s) {
 #pragma disjoint(*s, *r)
-#pragma unroll(6)
-  for(int i = 0; i < 6; i++) {
-    r[i] = vec_sub(r[i], s[i]);
-  }
+  r[0] = vec_sub(r[0], s[0]);
+  r[1] = vec_sub(r[1], s[1]);
+  r[2] = vec_sub(r[2], s[2]);
+  r[3] = vec_sub(r[3], s[3]);
+  r[4] = vec_sub(r[4], s[4]);
+  r[5] = vec_sub(r[5], s[5]);
+
   return;
 }
 
@@ -67,10 +71,9 @@ inline void vec_i_mul_add2(vector4double * restrict r, vector4double * restrict 
 #pragma disjoint(*s, *tmp)
 #pragma disjoint(*r, *tmp)
   tmp[0] = vec_splats(1.);
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[i] = vec_xxnpmadd(s[i], tmp[0], r[i]);
-  }
+  r[0] = vec_xxnpmadd(s[0], tmp[0], r[0]);
+  r[1] = vec_xxnpmadd(s[1], tmp[0], r[1]);
+  r[2] = vec_xxnpmadd(s[2], tmp[0], r[2]);
   return;
 }
 
@@ -82,10 +85,12 @@ inline void vec_i_mul_add_double2(vector4double * restrict r, vector4double * re
 #pragma disjoint(*s, *tmp)
 #pragma disjoint(*r, *tmp)
   tmp[0] = vec_splats(1.);
-#pragma unroll(6)
-  for(int i = 0; i < 6; i++) {
-    r[i] = vec_xxnpmadd(s[i], tmp[0], r[i]);
-  }
+  r[0] = vec_xxnpmadd(s[0], tmp[0], r[0]);
+  r[1] = vec_xxnpmadd(s[1], tmp[0], r[1]);
+  r[2] = vec_xxnpmadd(s[2], tmp[0], r[2]);
+  r[3] = vec_xxnpmadd(s[3], tmp[0], r[3]);
+  r[4] = vec_xxnpmadd(s[4], tmp[0], r[4]);
+  r[5] = vec_xxnpmadd(s[5], tmp[0], r[5]);
   return;
 }
 
@@ -97,10 +102,9 @@ inline void vec_i_mul_sub2(vector4double * restrict r, vector4double * restrict 
 #pragma disjoint(*s, *tmp)
 #pragma disjoint(*r, *tmp)
   tmp[0] = vec_splats(-1.);
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[i] = vec_xxnpmadd(s[i], tmp[0], r[i]);
-  }
+  r[0] = vec_xxnpmadd(s[0], tmp[0], r[0]);
+  r[1] = vec_xxnpmadd(s[1], tmp[0], r[1]);
+  r[2] = vec_xxnpmadd(s[2], tmp[0], r[2]);
   return;
 }
 
@@ -112,10 +116,12 @@ inline void vec_i_mul_sub_double2(vector4double * restrict r, vector4double * re
 #pragma disjoint(*s, *tmp)
 #pragma disjoint(*r, *tmp)
   tmp[0] = vec_splats(-1.);
-#pragma unroll(6)
-  for(int i = 0; i < 6; i++) {
-    r[i] = vec_xxnpmadd(s[i], tmp[0], r[i]);
-  }
+  r[0] = vec_xxnpmadd(s[0], tmp[0], r[0]);
+  r[1] = vec_xxnpmadd(s[1], tmp[0], r[1]);
+  r[2] = vec_xxnpmadd(s[2], tmp[0], r[2]);
+  r[3] = vec_xxnpmadd(s[3], tmp[0], r[3]);
+  r[4] = vec_xxnpmadd(s[4], tmp[0], r[4]);
+  r[5] = vec_xxnpmadd(s[5], tmp[0], r[5]);
   return;
 }
 
@@ -128,14 +134,18 @@ inline void vec_cmplx_mul_double2(vector4double * restrict rs, vector4double * r
 #pragma disjoint(*tmp, rs)
   __alignx(32, c);
   tmp[0] = vec_ld2(0, (double*) c);
-#pragma unroll(3)
-  for(int i = 0; i < 6; i++) {
-    rs[i] = vec_xmul(r[i], tmp[0]);
-  }
-#pragma unroll(3)
-  for(int i = 0; i < 6; i++) {
-    rs[i] = vec_xxnpmadd(tmp[0], r[i], rs[i]);
-  }
+  rs[0] = vec_xmul(r[0], tmp[0]);
+  rs[1] = vec_xmul(r[1], tmp[0]);
+  rs[2] = vec_xmul(r[2], tmp[0]);
+  rs[3] = vec_xmul(r[3], tmp[0]);
+  rs[4] = vec_xmul(r[4], tmp[0]);
+  rs[5] = vec_xmul(r[5], tmp[0]);
+  rs[0] = vec_xxnpmadd(tmp[0], r[0], rs[0]);
+  rs[1] = vec_xxnpmadd(tmp[0], r[1], rs[1]);
+  rs[2] = vec_xxnpmadd(tmp[0], r[2], rs[2]);
+  rs[3] = vec_xxnpmadd(tmp[0], r[3], rs[3]);
+  rs[4] = vec_xxnpmadd(tmp[0], r[4], rs[4]);
+  rs[5] = vec_xxnpmadd(tmp[0], r[5], rs[5]);
   return;
 }
 
@@ -146,14 +156,18 @@ inline void vec_cmplxcg_mul_double2(vector4double * restrict rs, vector4double *
 #pragma disjoint(*tmp, rs)
   __alignx(32, c);
   tmp[0] = vec_ld2(0, (double*) c);
-#pragma unroll(3)
-  for(int i = 0; i < 6; i++) {
-    rs[i] = vec_xmul(tmp[0], r[i]);
-  }
-#pragma unroll(3)
-  for(int i = 0; i < 6; i++) {
-    rs[i] = vec_xxcpnmadd(r[i], tmp[0], rs[i]);
-  }
+  rs[0] = vec_xmul(tmp[0], r[0]);
+  rs[1] = vec_xmul(tmp[0], r[1]);
+  rs[2] = vec_xmul(tmp[0], r[2]);
+  rs[3] = vec_xmul(tmp[0], r[3]);
+  rs[4] = vec_xmul(tmp[0], r[4]);
+  rs[5] = vec_xmul(tmp[0], r[5]);
+  rs[0] = vec_xxcpnmadd(r[0], tmp[0], rs[0]);
+  rs[1] = vec_xxcpnmadd(r[1], tmp[0], rs[1]);
+  rs[2] = vec_xxcpnmadd(r[2], tmp[0], rs[2]);
+  rs[3] = vec_xxcpnmadd(r[3], tmp[0], rs[3]);
+  rs[4] = vec_xxcpnmadd(r[4], tmp[0], rs[4]);
+  rs[5] = vec_xxcpnmadd(r[5], tmp[0], rs[5]);
   return;
 }
 
@@ -181,38 +195,49 @@ inline void vec_su3_multiply_double2(su3 * const restrict u, vector4double * res
   U[4] = vec_ld2(0, (double*) &u->c11);
   U[7] = vec_ld2(0, (double*) &u->c12);
   U[2] = vec_ld2(0, (double*) &u->c20);
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xmul(r[0], U[i]);
-    r[9+i] = vec_xmul(r[3], U[i]);
-  }
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xxnpmadd(U[i], r[0], r[6+i]);
-    r[9+i] = vec_xxnpmadd(U[i], r[3], r[9+i]);
-  }
+  r[6] = vec_xmul(r[0], U[0]);
+  r[7] = vec_xmul(r[0], U[1]);
+  r[8] = vec_xmul(r[0], U[2]);
+  r[9] = vec_xmul(r[3], U[0]);
+  r[10] = vec_xmul(r[3], U[1]);
+  r[11] = vec_xmul(r[3], U[2]);
+
+  r[6] = vec_xxnpmadd(U[0], r[0], r[6]);
+  r[7] = vec_xxnpmadd(U[1], r[0], r[7]);
+  r[8] = vec_xxnpmadd(U[2], r[0], r[8]);
+  r[9] = vec_xxnpmadd(U[0], r[3], r[9]);
+  r[10] = vec_xxnpmadd(U[1], r[3], r[10]);
+  r[11] = vec_xxnpmadd(U[2], r[3], r[11]);
   U[5] = vec_ld2(0, (double*) &u->c21);
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xmadd(r[1], U[3+i], r[6+i]);
-    r[9+i] = vec_xmadd(r[4], U[3+i], r[9+i]);
-  }
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xxnpmadd(U[3+i], r[1], r[6+i]);
-    r[9+i] = vec_xxnpmadd(U[3+i], r[4], r[9+i]);
-  }
+
+  r[6] = vec_xmadd(r[1], U[3], r[6]);
+  r[7] = vec_xmadd(r[1], U[4], r[7]);
+  r[8] = vec_xmadd(r[1], U[5], r[8]);
+  r[9] = vec_xmadd(r[4], U[3], r[9]);
+  r[10] = vec_xmadd(r[4], U[4], r[10]);
+  r[11] = vec_xmadd(r[4], U[5], r[11]);
+
+  r[6] = vec_xxnpmadd(U[3], r[1], r[6]);
+  r[7] = vec_xxnpmadd(U[4], r[1], r[7]);
+  r[8] = vec_xxnpmadd(U[5], r[1], r[8]);
+  r[9] = vec_xxnpmadd(U[3], r[4], r[9]);
+  r[10] = vec_xxnpmadd(U[4], r[4], r[10]);
+  r[11] = vec_xxnpmadd(U[5], r[4], r[10]);
   U[8] = vec_ld2(0, (double*) &u->c22);
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xmadd(r[2], U[6+i], r[6+i]);
-    r[9+i] = vec_xmadd(r[5], U[6+i], r[9+i]);
-  }
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xxnpmadd(U[6+i], r[2], r[6+i]);
-    r[9+i] = vec_xxnpmadd(U[6+i], r[5], r[9+i]);
-  }
+
+  r[6] = vec_xmadd(r[2], U[6], r[6]);
+  r[7] = vec_xmadd(r[2], U[7], r[7]);
+  r[8] = vec_xmadd(r[2], U[8], r[8]);
+  r[9] = vec_xmadd(r[5], U[6], r[9]);
+  r[10] = vec_xmadd(r[5], U[7], r[10]);
+  r[11] = vec_xmadd(r[5], U[8], r[11]);
+
+  r[6] = vec_xxnpmadd(U[6], r[2], r[6]);
+  r[7] = vec_xxnpmadd(U[7], r[2], r[7]);
+  r[8] = vec_xxnpmadd(U[8], r[2], r[8]);
+  r[9] = vec_xxnpmadd(U[6], r[5], r[9]);
+  r[10] = vec_xxnpmadd(U[7], r[5], r[10]);
+  r[11] = vec_xxnpmadd(U[8], r[5], r[11]);
   return;
 }
 
@@ -235,42 +260,56 @@ inline void vec_su3_inverse_multiply_double2(su3 * const restrict u, vector4doub
   U[0] = vec_ld2(0, (double*) &u->c00);
   U[1] = vec_ld2(0, (double*) &u->c01);
   U[2] = vec_ld2(0, (double*) &u->c02);
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xmul(U[i], r[0]);
-    r[9+i] = vec_xmul(U[i], r[3]);
-  }
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xxcpnmadd(r[0], U[i], r[6+i]);
-    r[9+i] = vec_xxcpnmadd(r[3], U[i], r[9+i]);
-  }
+
+  r[6] = vec_xmul(U[0], r[0]);
+  r[7] = vec_xmul(U[1], r[0]);
+  r[8] = vec_xmul(U[2], r[0]);
+  r[9] = vec_xmul(U[0], r[3]);
+  r[10] = vec_xmul(U[1], r[3]);
+  r[11] = vec_xmul(U[2], r[3]);
+
+  r[6] = vec_xxcpnmadd(r[0], U[0], r[6]);
+  r[7] = vec_xxcpnmadd(r[0], U[1], r[7]);
+  r[8] = vec_xxcpnmadd(r[0], U[2], r[8]);
+  r[9] = vec_xxcpnmadd(r[3], U[0], r[9]);
+  r[10] = vec_xxcpnmadd(r[3], U[1], r[10]);
+  r[11] = vec_xxcpnmadd(r[3], U[2], r[11]);
+
   U[0] = vec_ld2(0, (double*) &u->c10);
   U[1] = vec_ld2(0, (double*) &u->c11);
   U[2] = vec_ld2(0, (double*) &u->c12);
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xmadd(U[i], r[1], r[6+i]);
-    r[9+i] = vec_xmadd(U[i], r[4], r[9+i]);
-  }
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xxcpnmadd(r[1], U[i], r[6+i]);
-    r[9+i] = vec_xxcpnmadd(r[4], U[i], r[9+i]);
-  }
+
+  r[6] = vec_xmadd(U[0], r[1], r[6]);
+  r[7] = vec_xmadd(U[1], r[1], r[7]);
+  r[8] = vec_xmadd(U[2], r[1], r[8]);
+  r[9] = vec_xmadd(U[0], r[4], r[9]);
+  r[10] = vec_xmadd(U[1], r[4], r[10]);
+  r[11] = vec_xmadd(U[2], r[4], r[11]);
+  
+  r[6] = vec_xxcpnmadd(r[1], U[0], r[6]);
+  r[7] = vec_xxcpnmadd(r[1], U[1], r[7]);
+  r[8] = vec_xxcpnmadd(r[1], U[2], r[8]);
+  r[9] = vec_xxcpnmadd(r[4], U[0], r[9]);
+  r[10] = vec_xxcpnmadd(r[4], U[1], r[10]);
+  r[11] = vec_xxcpnmadd(r[4], U[2], r[11]);
+
   U[0] = vec_ld2(0, (double*) &u->c20);
   U[1] = vec_ld2(0, (double*) &u->c21);
   U[2] = vec_ld2(0, (double*) &u->c22);
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xmadd(U[i], r[2], r[6+i]);
-    r[9+i] = vec_xmadd(U[i], r[5], r[9+i]);
-  }
-#pragma unroll(3)
-  for(int i = 0; i < 3; i++) {
-    r[6+i] = vec_xxcpnmadd(r[2], U[i], r[6+i]);
-    r[9+i] = vec_xxcpnmadd(r[5], U[i], r[9+i]);
-  }
+
+  r[6] = vec_xmadd(U[0], r[2], r[6]);
+  r[7] = vec_xmadd(U[1], r[2], r[7]);
+  r[8] = vec_xmadd(U[2], r[2], r[8]);
+  r[9] = vec_xmadd(U[0], r[5], r[9]);
+  r[10] = vec_xmadd(U[1], r[5], r[10]);
+  r[11] = vec_xmadd(U[2], r[5], r[11]);
+
+  r[6] = vec_xxcpnmadd(r[2], U[0], r[6]);
+  r[7] = vec_xxcpnmadd(r[2], U[1], r[7]);
+  r[8] = vec_xxcpnmadd(r[2], U[2], r[8]);
+  r[9] = vec_xxcpnmadd(r[5], U[0], r[9]);
+  r[10] = vec_xxcpnmadd(r[5], U[1], r[10]);
+  r[11] = vec_xxcpnmadd(r[5], U[2], r[11]);
   return;
 }
 
