4875695d8063daa9ade3225b0ef27150db6de7ce
Carsten Urbach <curbach@gmx.de>
2012-09-05 14:02:28 +0200

    fusing c+hop for QPX, untested yet

--- operator/halfspinor_hopping.h
+++ operator/halfspinor_hopping.h
@@ -1119,6 +1119,20 @@
   _vec_store2((res)->s2, r6, r7, r8);					\
   _vec_store2((res)->s3, r9, r10, r11);
 
+#define _g5_cmplx_sub_hop_and_g5store(res)				\
+  _vec_load_halfspinor(r3, r4, r5, pn.s0);				\
+  _vec_cmplx_mul_double2c(r0, r1, r2, r3, r4, r5, cf);			\
+  _vec_unfuse(r0, r1, r2, r3, r4, r5);					\
+  _vec_sub_double2(r0, r1, r2, r3, r4, r5, rs0, rs1, rs2, rs3, rs4, rs5); \
+  _vec_store2((res)->s0, r0, r1, r2);					\
+  _vec_store2((res)->s1, r3, r4, r5);					\
+  _vec_load_halfspinor(r3, r4, r5, pn.s2);				\
+  _vec_cmplxcg_mul_double2c(r0, r1, r2, r3, r4, r5, cf);		\
+  _vec_unfuse(r0, r1, r2, r3, r4, r5);					\
+  _vec_sub_double2(rs6, rs7, rs8, rs9, rs10, rs11, r0, r1, r2, r3, r4, r5); \
+  _vec_store2((res)->s2, rs6, rs7, rs8);				\
+  _vec_store2((res)->s3, rs9, rs10, rs11);
+
 #define _hop_store_post(res)		\
   _vec_store2((res)->s0, rs0, rs1, rs2);	\
   _vec_store2((res)->s1, rs3, rs4, rs5);	\
