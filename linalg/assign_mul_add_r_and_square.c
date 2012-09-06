dbbd0a09f95e3ef6ad8eca2211af2ba95777b557
Carsten Urbach <curbach@gmx.de>
2012-09-06 17:51:04 +0200

    combined operation assign_mul_add_r and square_norm tested now, but doesn't help very much

--- linalg/assign_mul_add_r_and_square.c
+++ linalg/assign_mul_add_r_and_square.c
@@ -20,6 +20,9 @@
 #ifdef HAVE_CONFIG_H
 # include<config.h>
 #endif
+#ifdef MPI
+# include<mpi.h>
+#endif
 #include <stdlib.h>
 #include <complex.h>
 #ifdef OMP
--- solver/cg_her.c
+++ solver/cg_her.c
@@ -92,8 +92,7 @@ int cg_her(spinor * const P, spinor * const Q, const int max_iter,
     alpha_cg = normsq / pro;
     assign_add_mul_r(P, solver_field[2], alpha_cg, N);
 
-    assign_mul_add_r(solver_field[0], -alpha_cg, solver_field[1], N);
-    err=square_norm(solver_field[0], N, 1);
+    err = assign_mul_add_r_and_square(solver_field[0], -alpha_cg, solver_field[1], N, 1);
 
     if(g_proc_id == g_stdio_proc && g_debug_level > 1) {
       printf("CG: iterations: %d res^2 %e\n", iteration, err);
