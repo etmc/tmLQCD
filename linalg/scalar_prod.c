679bd78ea172291ff386ad47ef5498b1eed83433
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2012-09-07 17:08:18 +0200

    OpenMP version of scalar_prod with Kahan summation

--- linalg/scalar_prod.c
+++ linalg/scalar_prod.c
@@ -24,26 +24,36 @@
 #ifdef MPI
 #include <mpi.h>
 #endif
+#ifdef OMP
+# include <omp.h>
+# include <global.h>
+#endif
 #include "su3.h"
 #include "scalar_prod.h"
 
 /*  <S,R>=S^* times R */
 _Complex double scalar_prod(spinor * const S, spinor * const R, const int N, const int parallel) {
-  int ix;
-  _Complex double ALIGN ds,ks,kc,tr,ts,tt;
-  spinor *s,*r;
-  _Complex double c;
-#ifdef MPI
-  _Complex double d;
+  _Complex double ALIGN ks,kc;
+  ks = kc = 0.0;
+#ifdef OMP
+#pragma omp parallel
+  {
+  int thread_num = omp_get_thread_num();
+  g_omp_kc[thread_num] = 0.0;
+  g_omp_ks[thread_num] = 0.0;
 #endif
-  
-  ks=0.0;
-  kc=0.0;
+
+  _Complex double ALIGN ds,tr,ts,tt;
+  spinor *s,*r;
+
 #if (defined BGL && defined XLC)
   __alignx(16, S);
   __alignx(16, R);
-#endif  
-  for (ix = 0; ix < N; ix++)
+#endif
+#ifdef OMP
+#pragma omp for schedule(static)
+#endif
+  for (int ix = 0; ix < N; ix++)
   {
     s=(spinor *) S + ix;
     r=(spinor *) R + ix;
@@ -53,25 +63,44 @@ _Complex double scalar_prod(spinor * const S, spinor * const R, const int N, con
 	 r->s2.c0 * conj(s->s2.c0) + r->s2.c1 * conj(s->s2.c1) + r->s2.c2 * conj(s->s2.c2) + 
          r->s3.c0 * conj(s->s3.c0) + r->s3.c1 * conj(s->s3.c1) + r->s3.c2 * conj(s->s3.c2);
 
-    /* Kahan Summation */    
+    /* Kahan Summation */
+#ifdef OMP
+    tr = ds + g_omp_kc[thread_num];
+    ts = tr + g_omp_ks[thread_num];
+    tt = ts - g_omp_ks[thread_num];
+    g_omp_ks[thread_num] = ts;
+    g_omp_kc[thread_num] = tr - tt;
+#else
     tr=ds+kc;
     ts=tr+ks;
     tt=ts-ks;
     ks=ts;
     kc=tr-tt;
+#endif
   }
-  kc=ks+kc;
 
-  c = kc;
+#ifdef OMP
+  /* thread-local last step of the Kahan summation */
+  g_omp_kc[thread_num] = g_omp_ks[thread_num] + g_omp_kc[thread_num];
+
+  } /* OpenMP closing brace */
+
+  /* having left the parallel section, we can now sum up the Kahan
+     corrected sums from each thread into kc */
+  for(int i = 0; i < omp_num_threads; ++i)
+    kc += g_omp_kc[i];
+#else
+  kc=ks+kc;
+#endif
 
 #ifdef MPI
   if(parallel == 1)
   {
-    d = c;
-    MPI_Allreduce(&d, &c, 1, MPI_DOUBLE_COMPLEX, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(&kc, &ks, 1, MPI_DOUBLE_COMPLEX, MPI_SUM, MPI_COMM_WORLD);
+    return(ks);
   }
 #endif
-  return(c);
+  return(kc);
 }
 
 #ifdef WITHLAPH
