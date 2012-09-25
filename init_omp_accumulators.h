38ba753659ed097bce963bc2da09d4b09a2b4c35
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2012-09-25 13:56:45 +0200

    add some helpful comments about the omp accumulator arrays

--- init_omp_accumulators.h
+++ init_omp_accumulators.h
@@ -16,6 +16,18 @@
  * You should have received a copy of the GNU General Public License
  * along with tmLQCD.  If not, see <http://www.gnu.org/licenses/>.
  ***********************************************************************/
+
+/* The two arrays 
+ *
+ *   g_omp_acc_re
+ *   g_omp_acc_cp
+ *
+ * have as many elements as there are threads (set by ompnumthreads input parameter,
+ * stored in omp_num_threads configuration variable). They are initialiazed
+ * upon program launch and serve to hold thread-local values over the boundaries
+ * of parallel sections, such as for Kahan summations. _re is of type
+ * "double" while _cp is of type "_Complex double". They are declared in global.h */
+
 #ifndef _INIT_OMP_ACCUMULATORS_H
 #define _INIT_OMP_ACCUMULATORS_H
 
