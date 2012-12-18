285c4d8ee278f1f912d142ff3f9d3e7fb51c84ed
Albert Deuzeman (ITP Bern) <deuzeman@itp.unibe.ch>
2012-12-18 17:09:24 +0100

    Add forgotten check for GAUGE_COPY flag.

--- dirty_shameful_business.c
+++ dirty_shameful_business.c
@@ -8,7 +8,10 @@ void ohnohack_remap_g_gauge_field(gauge_field_t gf)
   g_gauge_field[0] = (su3*)gf[0];
   for(unsigned int i = 1; i < VOLUMEPLUSRAND + g_dbw2rand; ++i)
     g_gauge_field[i] = g_gauge_field[i - 1] + 4;
+#ifdef G_UPDATE_GAUGE_COPY
   update_backward_gauge(gf);
+#endif
+
 }
 
 void ohnohack_remap_df0(adjoint_field_t af)
