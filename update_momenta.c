47dab8a5ae2f74734161d5f2d6f53ea0a66d9eee
Albert Deuzeman (ITP Bern) <deuzeman@itp.unibe.ch>
2013-02-05 16:35:21 +0100

    Fixed accumulation error in add_stout_terms_to_forces. Results are far more accurate now, but possible phase factors still an issue.

--- update_momenta.c
+++ update_momenta.c
@@ -84,10 +84,10 @@ void update_momenta(int * mnllist, double step, const int no, hamiltonian_field_
 //   g_gf[0][0].c22 = -0.279641903356002 + 0.173912067032302 * I;
 
   /* FIXME Enter the simplest deviation from identity that will work */
-  g_gf[0][0].c00 = I;
-  g_gf[0][0].c11 = -I;
+   g_gf[0][0].c00 = I;
+   g_gf[0][0].c11 = -I;
   
-  rnd_gauge_trafo(&g_gf, g_gf);
+  //rnd_gauge_trafo(&g_gf, g_gf);
 
   ohnohack_remap_df0(tmp_derivative); /* FIXME Such that we can aggregate results per smearing type. */
   for (int s_ctr = 0; s_ctr < no_relevant_smearings; ++s_ctr)
