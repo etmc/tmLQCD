6abc13ce7a58f798c2dacced82d681f221838c1c
Albert Deuzeman <deuzeman@itp.unibe.ch>
2013-04-06 16:01:54 +0200

    Introduce hex smearing of forces.

--- smearing/hex_construct_hex_control.c
+++ smearing/hex_construct_hex_control.c
@@ -1,11 +1,11 @@
 #include "hex.ih"
 
-hex_control *construct_hex_control(int calculate_force_terms, unsigned int iterations, double const rho_1, double const rho_2, double const rho_3)
+hex_control *construct_hex_control(int calculate_force_terms, unsigned int iterations, double const rho_0, double const rho_1, double const rho_2)
 {
   hex_control *control = (hex_control*)malloc(sizeof(hex_control));
-  control->rho[0] = rho_1;
-  control->rho[1] = rho_2;
-  control->rho[2] = rho_3;
+  control->rho[0] = rho_0;
+  control->rho[1] = rho_1;
+  control->rho[2] = rho_2;
   control->iterations = iterations;
   control->smearing_performed = 0;
   
@@ -14,27 +14,40 @@ hex_control *construct_hex_control(int calculate_force_terms, unsigned int itera
   {
     control->U = (gauge_field_t*)malloc(2 * sizeof(gauge_field_t));
     control->U[1] = get_gauge_field();
+   
+    control->V_stage_one       = (su3_outer **)malloc(sizeof(su3_outer *));
+    control->V_stage_one[0]    = (su3_outer *)aalloc(sizeof(su3_outer) * (VOLUMEPLUSRAND + g_dbw2rand));
+    control->trace_stage_one   = (stout_notes_outer **)NULL;
     
-    control->U_outer = (su3_outer**)malloc(2 * sizeof(su3_outer*));
-    control->U_outer[0] = (su3_outer*)aalloc(VOLUMEPLUSRAND * sizeof(su3_outer));
-    control->U_outer[1] = (su3_outer*)aalloc(VOLUMEPLUSRAND * sizeof(su3_outer));
+    control->V_stage_two       = (su3_outer **)malloc(sizeof(su3_outer *));
+    control->V_stage_two[0]    = (su3_outer *)aalloc(sizeof(su3_outer) * (VOLUMEPLUSRAND + g_dbw2rand));
+    control->trace_stage_two   = (stout_notes_outer **)NULL;
+
+    control->trace_stage_three = (stout_notes_tuple **)NULL;
     
     return control;
   }
 
   control->U = (gauge_field_t*)malloc((iterations + 1) * sizeof(gauge_field_t));
-  control->trace = (hex_notes_tuple**)malloc(iterations * sizeof(hex_notes*)); 
-  control->U_outer = (su3_outer**)malloc(2 * iterations * sizeof(su3_outer*));
-  control->trace_outer = (hex_notes_tuple**)malloc(iterations * sizeof(hex_notes_outer*));  
+  
+  control->V_stage_one = malloc(iterations * sizeof(su3_outer*)); 
+  control->trace_stage_one = (stout_notes_outer **)malloc(iterations * sizeof(stouts_notes_outer*));
+    
+  control->V_stage_two = malloc(iterations * sizeof(su3_outer*)); 
+  control->trace_stage_two = (stout_notes_outer **)malloc(iterations * sizeof(stouts_notes_outer*));
+
+  control->trace_stage_three = (stout_notes_tuple **)malloc(iterations * sizeof(stouts_notes_outer*));
   
   for (unsigned int iter = 0; iter < iterations; ++iter)
-  {
-    control->U[iter + 1] = get_gauge_field();
-    control->trace[iter] = aalloc(VOLUME * sizeof(hex_notes));
-    control->U_outer[2 * iter] = aalloc(VOLUMEPLUSRAND * sizeof(hex_notes_outer));
-    control->U_outer[2 * iter + 1] = aalloc(VOLUMEPLUSRAND * sizeof(hex_notes_outer));
-    control->trace_outer[iter] = aalloc(VOLUME * sizeof(hex_notes_outer));
-    control->trace_outer[iter + 1] = aalloc(VOLUME * sizeof(hex_notes_outer));
+  {   
+    control->V_stage_one[iter]       = (su3_outer*)aalloc(sizeof(su3_outer) * (VOLUMEPLUSRAND + g_dbw2rand));
+    control->trace_stage_one[iter]   = (stout_notes_outer *)aalloc(sizeof(stout_notes_outer) * (VOLUMEPLUSRAND + g_dbw2rand));
+    
+    control->V_stage_two[iter]       = (su3_outer*)aalloc(sizeof(su3_outer) * (VOLUMEPLUSRAND + g_dbw2rand));
+    control->trace_stage_two[iter]   = (stout_notes_outer *)aalloc(sizeof(stout_notes_outer) * (VOLUMEPLUSRAND + g_dbw2rand));
+    
+    control->trace_stage_three[iter] = (stout_notes_tuple *)aalloc(sizeof(stout_notes_tuple) * (VOLUMEPLUSRAND + g_dbw2rand));
+    control->U[iter + 1]             = get_gauge_field();
   }
   
   control->result = control->U[iterations]; /* A shallow copy, just putting the reference in place. */
