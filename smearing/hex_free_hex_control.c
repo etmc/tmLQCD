fd7d52aad3fd04e35462039085ffe2964c769abc
Albert Deuzeman <deuzeman@itp.unibe.ch>
2013-04-06 16:01:54 +0200

    Introduce hex smearing of forces.

--- smearing/hex_free_hex_control.c
+++ smearing/hex_free_hex_control.c
@@ -2,34 +2,41 @@
 
 void free_hex_control(hex_control *control)
 {
+  /* Result and U[0] are always going to be a shallow copies and should not be cleared! */
+  
   if (!control->calculate_force_terms)
   {
     return_gauge_field(&control->U[1]);
     free(control->U);
-    afree(control->U_outer[0]);
-    afree(control->U_outer[1]);
+    
+    afree(control->V_stage_one[0]);
+    afree(control->V_stage_two[0]);
+    
+    free(control->V_stage_one);
+    free(control->V_stage_two);
+    
     free(control);
     return;
   }
 
   for (unsigned int iter = 0; iter < control->iterations; ++iter)
   {
+    afree(control->trace_stage_one[iter]);
+    afree(control->V_stage_one[iter]);
+    afree(control->trace_stage_two[iter]);
+    afree(control->V_stage_two[iter]);
+    afree(control->trace_stage_three[iter]);
     return_gauge_field(&control->U[iter + 1]);
-    afree(control->trace[iter]);
-    afree(control->U_outer[2 * iter]);
-    afree(control->U_outer[2 * iter + 1]);
-    afree(control->trace_outer[2 * iter]);
-    afree(control->trace_outer[2 * iter + 1]);
   }
 
+  free(control->trace_stage_one);
+  free(control->V_stage_one);
+  free(control->trace_stage_two);
+  free(control->V_stage_two);
+  free(control->trace_stage_three);
+
+  free(control->U);  
   return_adjoint_field(&control->force_result);
   
-  free(control->U);
-  free(control->trace);
-  free(control->U_outer);
-  free(control->trace_outer);
   free(control);
-  
-  /* Result and U[0] are always going to be a shallow copies and should not be cleared! */
-  /* The scratch fields should be initialized and cleared by the using functions, not elsewhere. */
 }
