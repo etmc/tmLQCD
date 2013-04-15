6d1fc0aa1e662d86857c1543a2100ade73d7fed6
Albert Deuzeman <deuzeman@itp.unibe.ch>
2013-04-16 00:25:52 +0200

    Draft implementation of full HEX smearing done.

--- smearing/hex_hex_smear_forces.c
+++ smearing/hex_hex_smear_forces.c
@@ -1,11 +1,16 @@
 #include "hex.ih"
 
-#include "hex_construct_intermediates_stage_0.static"
-#include "hex_add_hex_terms_to_forces_stage_0.static"
-#include "hex_construct_intermediates_stage_1.static"
-#include "hex_add_hex_terms_to_forces_stage_1.static"
-#include "stout_construct_intermediates.static" // Identical to the last hex step...
-#include "hex_add_hex_terms_to_forces_stage_2.static"
+#include "hex_construct_Sigma_0.static"
+#include "hex_construct_Sigma_1.static"
+#include "hex_construct_Sigma_2.static"
+#include "hex_construct_Sigma_3.static"
+#include "hex_construct_Z_1.static"
+#include "hex_construct_Z_2.static"
+#include "hex_construct_Z_3.static"
+#include "hex_add_terms_to_forces_stage_0.static"
+#include "hex_add_terms_to_forces_stage_1.static"
+#include "hex_add_terms_to_forces_stage_2.static"
+#include "hex_add_terms_to_forces_stage_3.static"
 
 void hex_smear_forces(hex_control *control, adjoint_field_t in)
 {
@@ -17,6 +22,7 @@ void hex_smear_forces(hex_control *control, adjoint_field_t in)
     fatal_error("Hex smearing not yet performed.", "hex_smear_forces");
   
   gauge_field_t smeared_force = get_gauge_field();
+  gauge_field_t Sigma_0 = get_gauge_field();
   
   /* We'll need the forces in their tangent space representation, so let's first build this up. */
   adjoint_to_gauge(&smeared_force, in);
@@ -25,24 +31,27 @@ void hex_smear_forces(hex_control *control, adjoint_field_t in)
 #pragma omp parallel
   for (unsigned int iter = control->iterations; iter > 0; --iter)
   {
-    construct_Sigma_3();
-    construct_Z_3();
-    construct_Sigma_2();
-    construct_Z_2();
-    construct_Sigma_1();
-    construct_Z_1();
-    construct_Sigma_0();
-   
-//    construct_intermediates(control->trace_stage_2[iter - 1], control->U[iter] /* = V_3 */, control->U[iter - 1] /* = U */, smeared_force);
-//     construct_intermediates_stage_1(control->trace_stage_1[iter - 1], control->V_stage_1[iter - 1] /* = V_2 */, control->U[iter - 1] /* = U */, control->trace_stage_2[iter - 1]);
-//     construct_intermediates_stage_0(control->trace_stage_0[iter - 1], control->V_stage_0[iter - 1] /* = V_1 */, control->U[iter - 1] /* = U */, control->trace_stage_1[iter - 1]);
+    /* Since the U's are fields after full smearing steps, U[iter - 1] == U, U[iter] == V */
+    construct_Sigma_3(control->trace_stage_3[iter - 1], control->U[iter], smeared_force);
+    construct_Z_3(control->trace_stage_3[iter - 1], control->U[iter - 1]);
+    add_terms_to_forces_stage_3(smeared_force, control->trace_stage_3[iter - 1], control->U[iter], control->U[iter - 1]));
     
-    add_hex_terms_to_forces(smeared_force, control->rho, control->trace[iter - 1], control->U[iter] /* = V */, control->U[iter - 1] /* = U */);
+    construct_Sigma_2(control->trace_stage_2[iter - 1], control->alpha[2], control->V_stage_2[iter - 1], control->trace_stage_3[iter - 1]);
+    construct_Z_2(control->trace_stage_2[iter - 1], control->U[iter - 1] );
+    add_terms_to_forces_stage_2(smeared_force, control->trace_stage_2[iter - 1], control->V_stage_2[iter - 1], control->U[iter - 1]));
+    
+    construct_Sigma_1(control->trace_stage_1[iter - 1], control->alpha[1] control->V_stage_1[iter - 1], control->trace_stage_2[iter - 1]);
+    construct_Z_1(control->trace_stage_1[iter - 1], control->U[iter - 1]);
+    add_terms_to_forces_stage_1(smeared_force, control->trace_stage_1[iter - 1], control->V_stage_1[iter - 1], control->U[iter - 1]));
+    
+    construct_Sigma_0(Sigma_0, control->alpha[0], control->U[iter - 1], control->trace_stage_1[iter - 1]);
+    add_terms_to_forces_stage_0(smeared_force, Sigma_0, control->U[iter - 1]));
   }
 
   /* The force terms are still in the tangent space representation, so project them back to the adjoint one */
   gauge_to_adjoint(&control->force_result, smeared_force);
   generic_exchange(&control->force_result, sizeof(su3adj_tuple));
 
+  return_gauge_field(&Sigma_0);
   return_gauge_field(&smeared_force);
 }
