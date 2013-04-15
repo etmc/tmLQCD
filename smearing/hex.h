6e46c7ef59bf65b2f2cbd19b1811bb0a97fdf6e5
Albert Deuzeman (ITP Bern) <deuzeman@itp.unibe.ch>
2013-04-15 14:57:45 +0200

    Reindexing hopefully done.

--- smearing/hex.h
+++ smearing/hex.h
@@ -4,10 +4,9 @@
 #include <smearing/hyp.h>
 
 typedef su3 su3_two_index[12];
-typedef stout_notes_t stout_notes_two_index[12];
 
-typedef su3 su3_two_index[24];
-typedef stout_notes_t stout_notes_two_index[24];
+typedef stout_notes_t stout_notes_two_index[12];
+typedef stout_notes_t stout_notes_three_index[24];
 
 typedef struct
 {
