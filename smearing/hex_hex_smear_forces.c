0ede1725e8dccf06ac85516f155fb8816130332e
Albert Deuzeman (ITP Bern) <deuzeman@itp.unibe.ch>
2013-04-19 13:15:55 +0200

    Refactor code between HEX and Stout.

--- smearing/hex_hex_smear_forces.c
+++ smearing/hex_hex_smear_forces.c
@@ -1,17 +1,8 @@
 #include "hex.ih"
 
-#include "hex_construct_Sigma_0.static"
-#include "hex_construct_Sigma_1.static"
-#include "hex_construct_Sigma_2.static"
-#include "hex_construct_Sigma_3.static"
-#include "hex_inner_Z_loop.static"
-#include "hex_construct_Z_1.static"
-#include "hex_construct_Z_2.static"
-#include "hex_construct_Z_3.static"
-#include "hex_add_terms_to_forces_stage_0.static"
-#include "hex_add_terms_to_forces_stage_1.static"
-#include "hex_add_terms_to_forces_stage_2.static"
-#include "hex_add_terms_to_forces_stage_3.static"
+#include "hex_construct_Sigma.static"
+#include "hex_construct_Z.static"
+#include "hex_add_terms_to_forces.static"
 
 void hex_smear_forces(hex_control *control, adjoint_field_t in)
 {
@@ -19,7 +10,7 @@ void hex_smear_forces(hex_control *control, adjoint_field_t in)
     fatal_error("Smearing control not set up for calculating forces.", "hex_smear_forces");
   
   gauge_field_t smeared_force = get_gauge_field();
-  gauge_field_t Sigma_0 = get_gauge_field();
+  gauge_field_t Sigma_U = get_gauge_field();
   
   /* We'll need the forces in their tangent space representation, so let's first build this up. */
   adjoint_to_gauge(&smeared_force, in);
@@ -29,26 +20,26 @@ void hex_smear_forces(hex_control *control, adjoint_field_t in)
   for (unsigned int iter = control->iterations; iter > 0; --iter)
   {
     /* Since the U's are fields after full smearing steps, U[iter - 1] == U, U[iter] == V */
-    construct_Sigma_3(control->trace_stage_3[iter - 1], control->U[iter], smeared_force);
-    construct_Z_3(control->trace_stage_3[iter - 1], control->U[iter - 1]);
-    add_terms_to_forces_stage_3(smeared_force, control->trace_stage_3[iter - 1], control->alpha[2], control->U[iter], control->U[iter - 1]);
+    construct_Sigma_V(control->trace_stage_3[iter - 1], control->U[iter], smeared_force);
+    construct_Z_V(control->trace_stage_3[iter - 1], control->U[iter - 1]);
+    add_terms_to_forces_V(smeared_force, control->trace_stage_3[iter - 1], control->alpha[2], control->U[iter], control->U[iter - 1]);
     
     construct_Sigma_2(control->trace_stage_2[iter - 1], control->alpha[2], control->V_stage_2[iter - 1], control->trace_stage_3[iter - 1]);
     construct_Z_2(control->trace_stage_2[iter - 1], control->U[iter - 1] );
-    add_terms_to_forces_stage_2(smeared_force, control->trace_stage_2[iter - 1], control->alpha[1], control->V_stage_2[iter - 1], control->U[iter - 1]);
+    add_terms_to_forces_2(smeared_force, control->trace_stage_2[iter - 1], control->alpha[1], control->V_stage_2[iter - 1], control->U[iter - 1]);
     
     construct_Sigma_1(control->trace_stage_1[iter - 1], control->alpha[1], control->V_stage_1[iter - 1], control->trace_stage_2[iter - 1]);
     construct_Z_1(control->trace_stage_1[iter - 1], control->U[iter - 1]);
-    add_terms_to_forces_stage_1(smeared_force, control->trace_stage_1[iter - 1], control->alpha[0], control->V_stage_1[iter - 1], control->U[iter - 1]);
+    add_terms_to_forces_1(smeared_force, control->trace_stage_1[iter - 1], control->alpha[0], control->V_stage_1[iter - 1], control->U[iter - 1]);
     
-    construct_Sigma_0(Sigma_0, control->alpha[0], control->U[iter - 1], control->trace_stage_1[iter - 1]);
-    add_terms_to_forces_stage_0(smeared_force, Sigma_0, control->U[iter - 1]);
+    construct_Sigma_U(Sigma_U, control->alpha[0], control->U[iter - 1], control->trace_stage_1[iter - 1]);
+    add_terms_to_forces_U(smeared_force, Sigma_U, control->U[iter - 1]);
   }
 
   /* The force terms are still in the tangent space representation, so project them back to the adjoint one */
   gauge_to_adjoint(&control->force_result, smeared_force);
   generic_exchange(&control->force_result, sizeof(su3adj_tuple));
 
-  return_gauge_field(&Sigma_0);
+  return_gauge_field(&Sigma_U);
   return_gauge_field(&smeared_force);
 }
--- smearing/stout_stout_smear_forces.c
+++ smearing/stout_stout_smear_forces.c
@@ -1,7 +1,8 @@
 #include "stout.ih"
 
-#include "stout_add_stout_terms_to_forces.static"
-#include "stout_construct_intermediates.static"
+#include "stout_construct_Sigma.static"
+#include "stout_construct_Z.static"
+#include "stout_add_terms_to_forces.static"
 
 void stout_smear_forces(stout_control *control, adjoint_field_t in)
 {
@@ -9,6 +10,7 @@ void stout_smear_forces(stout_control *control, adjoint_field_t in)
     fatal_error("Smearing control not set up for calculating forces.", "stout_smear_forces");
   
   gauge_field_t smeared_force = get_gauge_field();
+  gauge_field_t Sigma_U = get_gauge_field();
   
   /* We'll need the forces in their tangent space representation, so let's first build this up. */
   adjoint_to_gauge(&smeared_force, in);
@@ -17,9 +19,14 @@ void stout_smear_forces(stout_control *control, adjoint_field_t in)
 #pragma omp parallel
   for (unsigned int iter = control->iterations; iter > 0; --iter)
   {
-    construct_intermediates(control->trace[iter - 1], control->U[iter] /* = V */, control->U[iter - 1] /* = U */, smeared_force);
-    add_stout_terms_to_forces(smeared_force, control->rho, control->trace[iter - 1], control->U[iter] /* = V */, control->U[iter - 1] /* = U */);
-    /* Barrier unnecessary from implicit barrier of single section in add_stout_terms_to_forces */
+    /* Since the U's are fields after full smearing steps, U[iter - 1] == U, U[iter] == V */
+    construct_Sigma_V(control->trace[iter - 1], control->U[iter], smeared_force);
+    construct_Z_V(control->trace[iter - 1], control->U[iter - 1]);
+    add_terms_to_forces_V(smeared_force, control->trace[iter - 1], control->rho, control->U[iter], control->U[iter - 1]);
+
+    construct_Sigma_U(control->trace[iter - 1], control->U[iter], smeared_force);
+    add_terms_to_forces_U(smeared_force, Sigma_U, control->U[iter - 1]);
+    /* Barrier unnecessary from implicit barrier of single section in add_terms_to_forces */
   }
 
   /* The force terms are still in the tangent space representation, so project them back to the adjoint one */
@@ -27,4 +34,5 @@ void stout_smear_forces(stout_control *control, adjoint_field_t in)
   generic_exchange(&control->force_result, sizeof(su3adj_tuple));
 
   return_gauge_field(&smeared_force);
+  return_gauge_field(&Sigma_U);
 }
