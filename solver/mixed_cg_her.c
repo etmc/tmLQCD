47e0e459079b393527bcc2e10d962b373fa05158
Florian Burger <burger@physik.hu-berlin.de>
2013-10-29 14:58:57 +0100

    removed some testing stuff again

--- solver/mixed_cg_her.c
+++ solver/mixed_cg_her.c
@@ -57,111 +57,6 @@
 #include "gettime.h"
 
 
-#include "bgq.h"
-#include "bgq2.h"
-#include "operator/halfspinor_hopping32.h"
-void test(){
- 
- su3_32 ALIGN32 Utest[2];
-
- Utest[0].c00 =  0.0f+0.0fI;
- Utest[0].c01 =  0.0f+0.0fI;
- Utest[0].c02 =  0.0f+0.0fI; 
- Utest[0].c10 =  0.0f+0.0fI;
- Utest[0].c11 =  0.0f+0.0fI;
- Utest[0].c12 =  0.0f+0.0fI; 
- Utest[0].c20 =  0.0f+0.0fI;
- Utest[0].c21 =  0.0f+0.0fI;
- Utest[0].c22 =  0.0f+0.0fI; 
-
-
-
- Utest[1].c00 =  2.0f+0.0fI;
- Utest[1].c01 =  0.0f+0.0fI;
- Utest[1].c02 =  0.0f+0.0fI; 
- Utest[1].c10 =  0.0f+0.0fI;
- Utest[1].c11 =  2.0f+0.0fI;
- Utest[1].c12 =  0.0f+0.0fI; 
- Utest[1].c20 =  0.0f+0.0fI;
- Utest[1].c21 =  0.0f+0.0fI;
- Utest[1].c22 =  2.0f+0.0fI; 
-
-
-  
-  spinor32 ALIGN32 a;
-  a.s0.c0 = 1.0f+0.0fI; 
-  a.s0.c1 = 1.0f+0.0fI; 
-  a.s0.c2 = 1.0f+0.0fI; 
-   
-  a.s1.c0 = 2.0f+0.0fI; 
-  a.s1.c1 = 2.0f+0.0fI; 
-  a.s1.c2 = 2.0f+0.0fI; 
-  
-  a.s2.c0 = 3.0f+0.0fI; 
-  a.s2.c1 = 3.0f+0.0fI; 
-  a.s2.c2 = 3.0f+0.0fI; 
-  
-  a.s3.c0 = 4.0f+0.0fI; 
-  a.s3.c1 = 4.0f+0.0fI; 
-  a.s3.c2 = 4.0f+0.0fI; 
-   
-
-  spinor32 ALIGN32 out;
-  out.s0.c0 = 11.0f;
-  out.s0.c1 = 11.0f;
-  out.s0.c2 = 11.0f;
-  out.s1.c0 = 11.0f;
-  out.s1.c1 = 11.0f;
-  out.s1.c2 = 11.0f;
-
-  _declare_hregs();
-  _Complex float ALIGN32 ka0_32 = 1.0f;
-  spinor32 * s ALIGN32;
-  su3_32 * U ALIGN32;
-  halfspinor32 * phi2 ALIGN32;
-  
-  U=&(Utest[1]);
-  s=&a;
-  phi2=(halfspinor32*)&(out);
-
-#if ( defined BGQ && defined XLC)
- #define _prefetch_spinor_32
- #define _prefetch_su3_32 
-  printf("Testing qpx...\n");
-  /*
-  _vec_load2_32(rs0, rs1, rs2, s->s0);				
-  _vec_load2_32(rs3, rs4, rs5, s->s1);				
-  _vec_load2_32(rs6, rs7, rs8, s->s2);				
-  _vec_load2_32(rs9, rs10, rs11, s->s3);			
-  _vec_add_to2(r0, r1, r2, rs0, rs1, rs2, rs6, rs7, rs8);	
-  _vec_add_to2(r3, r4, r5, rs3, rs4, rs5, rs9, rs10, rs11);
-  rtmp = vec_ld2a(0L, (float*) &ka0_32);			
-  _vec_su3_multiply_double2_32(U);		
-  _vec_cmplx_mul_double2(r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, rtmp); 
-  _vec_store2_32(out.s0, r0, r1, r2);
-  _vec_store2_32(out.s1, r3, r4, r5);
-  */
-  _vec_load_32(rs0, rs1, s->s0);		
-  _vec_load16_32(rs2, rs3, s->s1, rtmp);
-  _vec_load_32(rs4, rs5, s->s2);
-  _vec_load16_32(rs6, rs7, s->s3, rtmp);
-  _vec_add(r0, r1, rs0, rs1, rs4, rs5);	
-  _vec_add(r2, r3, rs2, rs3, rs6, rs7);
-  _vec_su3_multiply_double2c_32(U);
-  rtmp = vec_ld2(0, (float*) &ka0_32);
-  _vec_cmplx_mul_double2c(r0, r1, r2, r4, r5, r6, rtmp);
-  _vec_store_halfspinor_32(phi2->s0, r0, r1, r2);
-
-#endif
-
-  printf("IN:  (%f,%f) (%f,%f) (%f,%f)\n", creal(a.s0.c0), cimag(a.s0.c0), creal(a.s0.c1), cimag(a.s0.c1), creal(a.s0.c2), cimag(a.s0.c2));
-  printf("OUT: (%f,%f) (%f,%f) (%f,%f)\n", creal(out.s0.c0), cimag(out.s0.c0), creal(out.s0.c1), cimag(out.s0.c1), creal(out.s0.c2), cimag(out.s0.c2));
-
-//exit(10);
-
-return;
-}
-
 
 
 /* P output = solution , Q input = source */
@@ -182,7 +77,6 @@ int mixed_cg_her(spinor * const P, spinor * const Q, const int max_iter,
   int max_inner_it = 2800;
   int N_outer = max_iter/max_inner_it;
 
-  //test();
 
   if(N == VOLUME) {
     init_solver_field(&solver_field, VOLUMEPLUSRAND, nr_sf);    
@@ -193,21 +87,11 @@ int mixed_cg_her(spinor * const P, spinor * const Q, const int max_iter,
     init_solver_field32(&solver_field32, VOLUMEPLUSRAND/2, nr_sf32);    
   }
 
-/*
-misaligned = ((long) Q) & 0x1F ;
-if(misaligned){
-    printf("x is misaligned!!\n");
-}
-  else{
-    printf("x correctly aligned!!\n");
-}
-*/
+
   squarenorm_d = square_norm(Q, N, 1);
   //printf("sqarenorm_d = %f\n", squarenorm_d);
   sourcesquarenorm = squarenorm_d;
   sqnrm_d = squarenorm_d;
-  
-  //maybe that is wrong
   squarenorm_d = square_norm(P, N, 1);
  
   //printf("sqarenorm_d = %f\n", squarenorm_d);
@@ -339,13 +223,6 @@ if(misaligned){
     //g_sloppy_precision = 0;
     //g_sloppy_precision_flag = 0;
     assign_to_64(xhigh, x, N);    
-    
-//     f(y, xhigh);
-//     diff(P, delta, y, N);
-//     sqnrm = square_norm(P, N, 1);
-//     printf("mixed CG: true residue %d\t%g\t\n",iter, sqnrm);   
-//     exit(1);
-    
     
     add(P, P, xhigh, N);
     f(y, P);
