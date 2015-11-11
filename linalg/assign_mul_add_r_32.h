c9aca78a725443a076c90b8232b1edda938528db
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2015-11-11 12:23:51 +0100

    mixed_cg_merge: 32 bit linalg

---
+++ linalg/assign_mul_add_r_32.h
@@ -0,0 +1,8 @@
+#ifndef ASSIGN_MUL_ADD_R_32_H
+#define ASSIGN_MUL_ADD_R_32_H
+
+#include "su3.h"
+
+void assign_mul_add_r_32(spinor32 * const R, const float c, const spinor32 * const S, const int N);
+
+#endif
