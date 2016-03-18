cb54dd32448b89705d1de3b1d690b9a0387d78bd
Carsten Urbach <curbach@gmx.de>
2016-03-18 20:02:43 +0100

    use threadsafe versions of scalar_prod and square norm

--- solver/mrblk_body.c
+++ solver/mrblk_body.c
@@ -15,12 +15,12 @@ int _PSWITCH(mrblk)(_PTSWITCH(spinor) * const P, _PTSWITCH(spinor) * const Q,
   s[2] = s[1] + N + 1;
 
   r = s[0];
-  norm_r = _PSWITCH(square_norm)(Q, N, parallel);
+  norm_r = _PSWITCH(square_norm_ts)(Q, N, parallel);
 
   _PSWITCH(zero_spinor_field)(P, N);
   f(s[2], P, blk);
   _PSWITCH(diff)(r, Q, s[2], N);
-  norm_r = _PSWITCH(square_norm)(r, N, parallel);
+  norm_r = _PSWITCH(square_norm_ts)(r, N, parallel);
   if(g_proc_id == g_stdio_proc && g_debug_level > 2 && blk == 0) {
     printf("MRblk iteration= %d  |res|^2= %e\n", i, norm_r);
     fflush( stdout );
@@ -29,8 +29,8 @@ int _PSWITCH(mrblk)(_PTSWITCH(spinor) * const P, _PTSWITCH(spinor) * const Q,
   while((norm_r > eps_sq) && (i < max_iter)){
     i++;
     f(s[1], r, blk);
-    alpha = _PSWITCH(scalar_prod)(s[1], r, N, parallel);
-    beta = _PSWITCH(square_norm)(s[1], N, parallel);
+    alpha = _PSWITCH(scalar_prod_ts)(s[1], r, N, parallel);
+    beta = _PSWITCH(square_norm_ts)(s[1], N, parallel);
     alpha /= beta;
     _PSWITCH(assign_add_mul)(P, r, alpha, N);
     if(i%50 == 0) {
@@ -40,7 +40,7 @@ int _PSWITCH(mrblk)(_PTSWITCH(spinor) * const P, _PTSWITCH(spinor) * const Q,
       _PSWITCH(assign_add_mul)(s[2], s[1], alpha, N);
     }
     _PSWITCH(diff)(r, Q, s[2], N);
-    norm_r = _PSWITCH(square_norm)(r, N, parallel);
+    norm_r = _PSWITCH(square_norm_ts)(r, N, parallel);
     if(g_proc_id == g_stdio_proc && g_debug_level > 2 && blk == 0) {
       printf("MRblk iteration= %d  |res|^2= %g\n", i, norm_r);
       fflush(stdout);
