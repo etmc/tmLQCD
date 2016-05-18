f273456c3049c3a473e2bc489e73d6251f820373
Carsten Urbach <curbach@gmx.de>
2016-05-18 15:38:17 +0200

    remove g_ka_csw_8 to fix issue \#274

--- test/test_eigenvalues.c
+++ test/test_eigenvalues.c
@@ -335,7 +335,7 @@ int main(int argc,char *argv[]) {
 
     fprintf(parameterfile, "The lattice size is %d x %d x %d x %d\n", (int)(g_nproc_t*T), (int)(g_nproc_x*LX), (int)(LY), (int)(LZ));
     fprintf(parameterfile, "The local lattice size is %d x %d x %d x %d\n", (int)(T), (int)(LX), (int)(LY), (int)(LZ));
-    fprintf(parameterfile, "g_beta = %f , g_kappa= %f, g_kappa*csw/8= %f \n",g_beta,g_kappa,g_ka_csw_8);
+    fprintf(parameterfile, "g_beta = %f , g_kappa= %f, c_sw = %f \n",g_beta,g_kappa,g_c_sw);
     fprintf(parameterfile, "boundary of fermion fields (t,x,y,z): %f %f %f %f \n",X0,X1,X2,X3);
     fprintf(parameterfile, "EPS_SQ0=%e, EPS_SQ1=%e EPS_SQ2=%e, EPS_SQ3=%e \n"
 	    ,EPS_SQ0,EPS_SQ1,EPS_SQ2,EPS_SQ3);
