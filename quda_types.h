bb47f153fe579f8a5ebbcc713ad5d32ea3a3ceb6
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2018-07-07 10:06:48 +0200

    quda_interface: track more complete state of device gauge and clover fields as well as the mg setup in order to properly deal with parameter changes as they come

--- quda_types.h
+++ quda_types.h
@@ -33,6 +33,10 @@
 #include "quda_solver_translate.h"
 #endif
 
+#include "global.h"
+#include <float.h>
+#include <math.h>
+
 typedef enum tm_quda_ferm_bc_t {
   TM_QUDA_THETABC = 0,
   TM_QUDA_APBC,
@@ -63,5 +67,98 @@ typedef struct tm_QudaParams_t {
   int               mg_run_verify;
   int               mg_enable_size_three_blocks;
 } tm_QudaParams_t;
+
+typedef struct tm_QudaMGSetupState_t {
+  int gauge_id;
+  double c_sw;
+  double kappa;
+  double mu;
+  int initialised;
+} tm_QudaMGSetupState_t;
+
+typedef struct tm_QudaCloverState_t {
+  int gauge_id;
+  double c_sw;
+  double kappa;
+  double mu;
+  int loaded;
+} tm_QudaCloverState_t;
+
+typedef struct tm_QudaGaugeState_t {
+  int gauge_id;
+  int loaded;
+} tm_QudaGaugeState_t;
+
+static inline int check_quda_clover_state(const tm_QudaCloverState_t * const quda_clover_state,
+                                          const tm_QudaGaugeState_t * const quda_gauge_state){
+  return( quda_clover_state->loaded &&
+          (quda_clover_state->gauge_id == quda_gauge_state->gauge_id) &&
+          (fabs(quda_clover_state->c_sw - g_c_sw) < 2*DBL_EPSILON) &&
+          (fabs(quda_clover_state->kappa - g_kappa) < 2*DBL_EPSILON) &&
+          (fabs(quda_clover_state->mu - g_mu) < 2*DBL_EPSILON) );
+}
+
+static inline void set_quda_clover_state(tm_QudaCloverState_t * const quda_clover_state,
+                                         const tm_QudaGaugeState_t * const quda_gauge_state){
+  quda_clover_state->gauge_id = quda_gauge_state->gauge_id;
+  quda_clover_state->c_sw = g_c_sw;
+  quda_clover_state->kappa = g_kappa;
+  quda_clover_state->mu = g_mu;
+  quda_clover_state->loaded = 1;
+}
+
+static inline void reset_quda_clover_state(tm_QudaCloverState_t * const quda_clover_state){
+  quda_clover_state->gauge_id = -1;
+  quda_clover_state->loaded = 0;
+  quda_clover_state->mu = -1.0;
+  quda_clover_state->c_sw = -1.0;
+  quda_clover_state->mu = -1.0;
+}
+
+static inline int check_quda_gauge_state(const tm_QudaGaugeState_t * const quda_gauge_state,
+                                         const int gauge_id){
+  return( quda_gauge_state->loaded &&
+          (quda_gauge_state->gauge_id == gauge_id) );
+}
+
+static inline void set_quda_gauge_state(tm_QudaGaugeState_t * const quda_gauge_state,
+                                        const int gauge_id){
+  quda_gauge_state->gauge_id = gauge_id;
+  quda_gauge_state->loaded = 1;
+}
+
+static inline void reset_quda_gauge_state(tm_QudaGaugeState_t * const quda_gauge_state){
+  quda_gauge_state->gauge_id = -1;
+  quda_gauge_state->loaded = 0;
+}
+
+static inline int check_quda_mg_setup_state(const tm_QudaMGSetupState_t * const quda_mg_setup_state,
+                                            const tm_QudaGaugeState_t * const quda_gauge_state){
+  if( quda_mg_setup_state->initialised != 1 ){
+    return -1;
+  } else {
+    return  ( (quda_mg_setup_state->gauge_id == quda_gauge_state->gauge_id) &&
+              ( fabs(quda_mg_setup_state->c_sw - g_c_sw) < 2*DBL_EPSILON) &&
+              ( fabs(quda_mg_setup_state->kappa - g_kappa) < 2*DBL_EPSILON) &&
+              ( fabs(quda_mg_setup_state->mu - g_mu) < 2*DBL_EPSILON) );
+  }
+}
+
+static inline void set_quda_mg_setup_state(tm_QudaMGSetupState_t * const quda_mg_setup_state,
+                                           const tm_QudaGaugeState_t * const quda_gauge_state){
+  quda_mg_setup_state->gauge_id = quda_gauge_state->gauge_id;
+  quda_mg_setup_state->c_sw = g_c_sw;
+  quda_mg_setup_state->kappa = g_kappa;
+  quda_mg_setup_state->mu = g_mu;
+  quda_mg_setup_state->initialised = 1;
+}
+
+static inline void reset_quda_mg_setup_state(tm_QudaMGSetupState_t * const quda_mg_setup_state){
+  quda_mg_setup_state->gauge_id = -1;
+  quda_mg_setup_state->initialised = 0;
+  quda_mg_setup_state->mu = -1.0;
+  quda_mg_setup_state->c_sw = -1.0;
+  quda_mg_setup_state->mu = -1.0;
+}
 
 #endif // TM_QUDA_TYPES_H
