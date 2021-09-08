7bc1c4907622937ebdb395af814f0cdcaa90aca3
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2021-09-08 16:24:47 +0200

    bunch of forgotten timings in cloverdetratio monomial

--- monomial/cloverdetratio_monomial.c
+++ monomial/cloverdetratio_monomial.c
@@ -98,12 +98,16 @@ void cloverdetratio_derivative_orig(const int no, hamiltonian_field_t * const hf
   
   /* apply Hopping Matrix M_{eo} */
   /* to get the even sites of X */
+  tm_stopwatch_push(&g_timers);
   H_eo_sw_inv_psi(mnl->w_fields[2], mnl->w_fields[1], EO, -1, mnl->mu);
+  tm_stopwatch_pop(&g_timers, 0, 1, "", "H_eo_sw_inv_psi");
   /* \delta Q sandwitched by Y_o^\dagger and X_e */
   deriv_Sb(OE, mnl->w_fields[0], mnl->w_fields[2], hf, mnl->forcefactor); 
   
   /* to get the even sites of Y */
+  tm_stopwatch_push(&g_timers);
   H_eo_sw_inv_psi(mnl->w_fields[3], mnl->w_fields[0], EO, +1, mnl->mu);
+  tm_stopwatch_pop(&g_timers, 0, 1, "", "H_eo_sw_inv_psi");
   /* \delta Q sandwitched by Y_e^\dagger and X_o */
   deriv_Sb(EO, mnl->w_fields[3], mnl->w_fields[1], hf, mnl->forcefactor); 
 
@@ -124,12 +128,16 @@ void cloverdetratio_derivative_orig(const int no, hamiltonian_field_t * const hf
   
   /* apply Hopping Matrix M_{eo} */
   /* to get the even sites of X */
+  tm_stopwatch_push(&g_timers);
   H_eo_sw_inv_psi(mnl->w_fields[2], mnl->w_fields[1], EO, -1, mnl->mu);
+  tm_stopwatch_pop(&g_timers, 0, 1, "", "H_eo_sw_inv_psi");
   /* \delta Q sandwitched by Y_o^\dagger and X_e */
   deriv_Sb(OE, mnl->w_fields[0], mnl->w_fields[2], hf, mnl->forcefactor); 
   
   /* to get the even sites of Y */
+  tm_stopwatch_push(&g_timers);
   H_eo_sw_inv_psi(mnl->w_fields[3], mnl->w_fields[0], EO, +1, mnl->mu);
+  tm_stopwatch_pop(&g_timers, 0, 1, "", "H_eo_sw_inv_psi");
   /* \delta Q sandwitched by Y_e^\dagger and X_o */
   deriv_Sb(EO, mnl->w_fields[3], mnl->w_fields[1], hf, mnl->forcefactor);
 
@@ -213,12 +221,16 @@ void cloverdetratio_derivative(const int no, hamiltonian_field_t * const hf) {
 
   /* apply Hopping Matrix M_{eo} */
   /* to get the even sites of X */
+  tm_stopwatch_push(&g_timers);
   H_eo_sw_inv_psi(mnl->w_fields[2], mnl->w_fields[1], EE, -1, mnl->mu);
+  tm_stopwatch_pop(&g_timers, 0, 1, "", "H_eo_sw_inv_psi");
   /* \delta Q sandwitched by Y_o^\dagger and X_e */
   deriv_Sb(OE, mnl->w_fields[0], mnl->w_fields[2], hf, mnl->forcefactor); 
   
   /* to get the even sites of Y */
+  tm_stopwatch_push(&g_timers);
   H_eo_sw_inv_psi(mnl->w_fields[3], mnl->w_fields[0], EE, +1, mnl->mu);
+  tm_stopwatch_pop(&g_timers, 0, 1, "", "H_eo_sw_inv_psi");
   /* \delta Q sandwitched by Y_e^\dagger and X_o */
   deriv_Sb(EO, mnl->w_fields[3], mnl->w_fields[1], hf, mnl->forcefactor); 
 
