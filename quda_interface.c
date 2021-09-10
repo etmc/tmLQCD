ffabc9dddb54eac21c6069c3e8f05e5986b4993a
Simone Bacchio <s.bacchio@gmail.com>
2021-09-10 15:32:53 +0300

    Fixing lengths

--- quda_interface.c
+++ quda_interface.c
@@ -339,18 +339,14 @@ void _setDefaultQudaParam(void){
 void set_force_gauge_param(CompressionType * compression, QudaGaugeParam * f_gauge_param){
   set_default_gauge_param(f_gauge_param);
 
-  gauge_param->reconstruct = 10;
-  gauge_param->reconstruct_sloppy = 10;
-  gauge_param->reconstruct_precondition = 10;
-  gauge_param->reconstruct_refinement_sloppy = 10;
-  gauge_param->reconstruct_eigensolver = 10;
-  
   pad_size = 0;
   pad_size = 0;
   gauge_param->ga_pad = 0;
 
   f_gauge_param->use_resident_gauge = QUDA_BOOLEAN_NO;
+  f_gauge_param->make_resident_gauge = QUDA_BOOLEAN_NO;
   f_gauge_param->use_resident_mom = QUDA_BOOLEAN_NO;
+  f_gauge_param->make_resident_mom = QUDA_BOOLEAN_NO;
   f_gauge_param->return_result_mom = QUDA_BOOLEAN_YES;
   f_gauge_param->overwrite_mom = QUDA_BOOLEAN_YES;
 }
@@ -2292,15 +2288,15 @@ int invert_eo_quda_twoflavour_mshift(spinor ** const out_up, spinor ** const out
   return(iterations);
 }
 
-void compute_gauge_force_quda(CompressionType compression, int rect) {
-  static int plaq_rect_length[] = {
-    3, 3, 3, 3, 3,
-    5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5 
+void compute_gauge_force_quda(monomial * const mnl, hamiltonian_field_t * const hf) {
+  static int plaq_rect_length[24] = {
+    3, 3, 3, 3, 3, 3,
+    5, 5, 5, 5, 5, 5,
+    5, 5, 5, 5, 5, 5,
+    5, 5, 5, 5, 5, 5,
   };
 
-  static double plaq_rect_coeff[] = {
+  static double plaq_rect_coeff[24] = {
     1.1, 1.2, 1.3, 1.4, 1.5, 1.6,
     2.1, 2.2, 2.3, 2.4, 2.5, 2.6,
     3.1, 3.2, 3.3, 3.4, 3.5, 3.6,
@@ -2332,30 +2328,30 @@ void compute_gauge_force_quda(CompressionType compression, int rect) {
       {2, 2, 7, 5, 5 },
       {4, 4, 7, 3, 3 },
       {3, 3, 7, 4, 4 } },
-    { { 2 ,6 ,5 },
-      { 5 ,6 ,2 },
-      { 3 ,6 ,4 },
-      { 4 ,6 ,3 },
-      { 0 ,6 ,7 },
-      { 7 ,6 ,0 },
-      { 1 ,2 ,6 ,6 ,5 },
-      { 2 ,6 ,6 ,5 ,1 },
-      { 5 ,6 ,6 ,2 ,1 },
-      { 1 ,5 ,6 ,6 ,2 },
-      { 1 ,3 ,6 ,6 ,4 },
-      { 3 ,6 ,6 ,4 ,1 },
-      { 4 ,6 ,6 ,3 ,1 },
-      { 1 ,4 ,6 ,6 ,3 },
-      { 1 ,0 ,6 ,6 ,7 },
-      { 0 ,6 ,6 ,7 ,1 },
-      { 7 ,6 ,6 ,0 ,1 },
-      { 1 ,7 ,6 ,6 ,0 },
-      { 5 ,5 ,6 ,2 ,2 },
-      { 2 ,2 ,6 ,5 ,5 },
-      { 4 ,4 ,6 ,3 ,3 },
-      { 3 ,3 ,6 ,4 ,4 },
-      { 7 ,7 ,6 ,0 ,0 },
-      { 0 ,0 ,6 ,7 ,7 } },
+    { { 2, 6, 5 },
+      { 5, 6, 2 },
+      { 3, 6, 4 },
+      { 4, 6, 3 },
+      { 0, 6, 7 },
+      { 7, 6, 0 },
+      { 1, 2, 6, 6, 5 },
+      { 2, 6, 6, 5, 1 },
+      { 5, 6, 6, 2, 1 },
+      { 1, 5, 6, 6, 2 },
+      { 1, 3, 6, 6, 4 },
+      { 3, 6, 6, 4, 1 },
+      { 4, 6, 6, 3, 1 },
+      { 1, 4, 6, 6, 3 },
+      { 1, 0, 6, 6, 7 },
+      { 0, 6, 6, 7, 1 },
+      { 7, 6, 6, 0, 1 },
+      { 1, 7, 6, 6, 0 },
+      { 5, 5, 6, 2, 2 },
+      { 2, 2, 6, 5, 5 },
+      { 4, 4, 6, 3, 3 },
+      { 3, 3, 6, 4, 4 },
+      { 7, 7, 6, 0, 0 },
+      { 0, 0, 6, 7, 7 } },
     { {3, 5, 4},
       {4, 5, 3},
       {0, 5, 7},
@@ -2380,60 +2376,60 @@ void compute_gauge_force_quda(CompressionType compression, int rect) {
       {0, 0, 5, 7, 7}, 
       {6, 6, 5, 1, 1}, 
       {1, 1, 5, 6, 6} }, 
-    { { 0 ,4 ,7 },
-      { 7 ,4 ,0 },
-      { 1 ,4 ,6 },
-      { 6 ,4 ,1 },
-      { 2 ,4 ,5 },
-      { 5 ,4 ,2 },
-      { 3 ,0 ,4 ,4 ,7 },
-      { 0 ,4 ,4 ,7 ,3 },
-      { 7 ,4 ,4 ,0 ,3 },
-      { 3 ,7 ,4 ,4 ,0 },
-      { 3 ,1 ,4 ,4 ,6 },
-      { 1 ,4 ,4 ,6 ,3 },
-      { 6 ,4 ,4 ,1 ,3 },
-      { 3 ,6 ,4 ,4 ,1 },
-      { 3 ,2 ,4 ,4 ,5 },
-      { 2 ,4 ,4 ,5 ,3 },
-      { 5 ,4 ,4 ,2 ,3 },
-      { 3 ,5 ,4 ,4 ,2 },
-      { 7 ,7 ,4 ,0 ,0 },
-      { 0 ,0 ,4 ,7 ,7 },
-      { 6 ,6 ,4 ,1 ,1 },
-      { 1 ,1 ,4 ,6 ,6 },
-      { 5 ,5 ,4 ,2 ,2 },
-      { 2 ,2 ,4 ,5 ,5 } } 
+    { { 0, 4, 7 },
+      { 7, 4, 0 },
+      { 1, 4, 6 },
+      { 6, 4, 1 },
+      { 2, 4, 5 },
+      { 5, 4, 2 },
+      { 3, 0, 4, 4, 7 },
+      { 0, 4, 4, 7, 3 },
+      { 7, 4, 4, 0, 3 },
+      { 3, 7, 4, 4, 0 },
+      { 3, 1, 4, 4, 6 },
+      { 1, 4, 4, 6, 3 },
+      { 6, 4, 4, 1, 3 },
+      { 3, 6, 4, 4, 1 },
+      { 3, 2, 4, 4, 5 },
+      { 2, 4, 4, 5, 3 },
+      { 5, 4, 4, 2, 3 },
+      { 3, 5, 4, 4, 2 },
+      { 7, 7, 4, 0, 0 },
+      { 0, 0, 4, 7, 7 },
+      { 6, 6, 4, 1, 1 },
+      { 1, 1, 4, 6, 6 },
+      { 5, 5, 4, 2, 2 },
+      { 2, 2, 4, 5, 5 } } 
   };
 
   static int plaq_length[] = {
     3, 3, 3, 3, 3, 3 };
 
   static int plaq_path[4][6][3] = {
-    { {1, 7, 6 },
-      {6, 7, 1 },
-      {2, 7, 5 },
-      {5, 7, 2 },
-      {3, 7, 4 },
-      {4, 7, 3 } },
-    { { 2 ,6 ,5 },
-      { 5 ,6 ,2 },
-      { 3 ,6 ,4 },
-      { 4 ,6 ,3 },
-      { 0 ,6 ,7 },
-      { 7 ,6 ,0 } },
-    { {3, 5, 4},
-      {4, 5, 3},
-      {0, 5, 7},
-      {7, 5, 0},
-      {1, 5, 6},
-      {6, 5, 1} },
-    { { 0 ,4 ,7 },
-      { 7 ,4 ,0 },
-      { 1 ,4 ,6 },
-      { 6 ,4 ,1 },
-      { 2 ,4 ,5 },
-      { 5 ,4 ,2 } } 
+    { { 1, 7, 6 },
+      { 6, 7, 1 },
+      { 2, 7, 5 },
+      { 5, 7, 2 },
+      { 3, 7, 4 },
+      { 4, 7, 3 } },
+    { { 2, 6, 5 },
+      { 5, 6, 2 },
+      { 3, 6, 4 },
+      { 4, 6, 3 },
+      { 0, 6, 7 },
+      { 7, 6, 0 } },
+    { { 3, 5, 4},
+      { 4, 5, 3},
+      { 0, 5, 7},
+      { 7, 5, 0},
+      { 1, 5, 6},
+      { 6, 5, 1} },
+    { { 0, 4, 7 },
+      { 7, 4, 0 },
+      { 1, 4, 6 },
+      { 6, 4, 1 },
+      { 2, 4, 5 },
+      { 5, 4, 2 } } 
   };
 
   static double plaq_coeff[] = {
@@ -2442,20 +2438,13 @@ void compute_gauge_force_quda(CompressionType compression, int rect) {
   
   _initQuda();
   _initMomQuda();
-  QudaGaugeParam f_gauge_param = newQudaGaugeParam();
-  set_force_gauge_param(&compression, &f_gauge_param);
-
-  // we still need to do these for the gauge field
-  set_boundary_conditions(&compression, &gauge_param);
-  set_sloppy_prec(SLOPPY_DOUBLE, &gauge_param, &inv_param);
-  _loadGaugeQuda(compression);
 
   int *** path_buf = rect ? plaq_rect_path : plaq_path;
   int * path_length = rect ? plaq_rect_length : plaq_length;
   double * loop_coeff = rect ? plaq_rect_coeff : plaq_coeff;
 
   int num_paths = rect ? 24 : 6;
-  int max_length = rect ? 6 : 4;
+  int max_length = rect ? 5 : 3;
 
   computeGaugeForceQuda((void*)mom_quda, 
                         (void*)gauge_quda, 
