8f91fa695da774b490e4189609b4dad3d61d7afb
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2021-09-18 13:36:23 +0200

    add some more information to compare_derivative and make program termination dependent on g_strict_residual_check, prefer the usgage of stdout over stderr

--- monomial/gauge_monomial.c
+++ monomial/gauge_monomial.c
@@ -48,25 +48,40 @@
 #include "quda_interface.h"
 #endif
 
-/* this function compares the gauge derivative calculated by Quda and tmLQCD */
-void compare_derivative(monomial *mnl, su3adj **arg1, su3adj **arg2 ){
-  if (g_proc_id==0){
-    for(int ix = 0; ix < VOLUME; ix++){
-      for(int mu=0; mu<4; mu++){
-        double *temp1=&(arg1[ix][mu].d1);
-        double *temp2=&(arg2[ix][mu].d1);
-        for(int j=0; j<8; ++j){
-          double diff=(temp1[j]-temp2[j])/temp1[j];
-          if (sqrt(diff*diff)>1e-10){
-            fprintf(stderr, "The derivative computed by QUDA and tmLQCD does not match for monomial: c0=%e c1=%e g_beta=%e\n",mnl->c0,
-                                                                           mnl->c1,
-                                                                           mnl->beta);
-            fprintf(stderr, "The difference is %e\n", diff);
-            exit(1);
-          }
+/* this function compares the gauge derivative calculated by an external library and tmLQCD */
+void compare_derivative(monomial *mnl, su3adj **ext_lib, su3adj **native ){
+  const double threshold = 1e-7;
+  int n_diff = 0;
+
+  for(int ix = 0; ix < VOLUME; ix++){
+    for(int mu=0; mu<4; mu++){
+      double *ext=&(ext_lib[ix][mu].d1);
+      double *nat=&(native[ix][mu].d1);
+      for(int j=0; j<8; ++j){
+        double diff=(ext[j]-nat[j])/nat[j];
+        if (sqrt(diff*diff) > threshold){
+          n_diff++;
+          printf("gauge derivative relative deviation %e at (t,x,y,z,mu,j) %d,%d,%d,%d,%d,%d on proc_id %d, ext: %e, native: %e\n", 
+                 diff,
+                 g_coord[ix][0], g_coord[ix][1], g_coord[ix][2], g_coord[ix][3], mu, j,
+                 g_proc_id,
+                 ext[j], nat[j]);
         }
       }
     }
+  }
+  if(n_diff > 0){
+    printf("gauge_derivative: the relative deviation between tmLQCD and the external library "
+           "exceeds the threshold %.1e in %d case(s) for parameters: c0=%e c1=%e g_beta=%e on proc_id: %d\n",
+           threshold,
+           n_diff,
+           mnl->c0,
+           mnl->c1,
+           mnl->beta,
+           g_proc_id);
+
+    if(g_strict_residual_check) fatal_error("Difference between external library and tmLQCD-native function!", 
+                                            "gauge_derivative");
   }
 }
 
