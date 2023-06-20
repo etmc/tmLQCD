92cf6cf6bbaca792c33a9d2538e18ab800f24086
simone-romiti <simone.romiti.1994@gmail.com>
2023-06-20 14:56:47 +0200

    some documentation for mul_one_pm_itau2_and_div_by_sqrt2()

--- operator/tm_operators_nd.h
+++ operator/tm_operators_nd.h
@@ -22,6 +22,7 @@
 #ifndef _TM_OPERATTORS_ND_H
 #define _TM_OPERATTORS_ND_H
 
+// \frac{1}{\sqrt(2)} (1 \pm \tau_2)
 void mul_one_pm_itau2_and_div_by_sqrt2(spinor * const p, spinor * const q,
                       spinor * const r, spinor * const s,
                       const double sign, const int N);
