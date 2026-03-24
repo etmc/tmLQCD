a9983fb7386ea946d459e57d59ffef8ae10223ec
Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2026-03-24 10:08:52 +0100

    Remove autotools support from package.py

--- .ci/uenv-recipes/tmlqcd/daint-gh200/repo/packages/lemonio/package.py
+++ .ci/uenv-recipes/tmlqcd/daint-gh200/repo/packages/lemonio/package.py
@@ -8,21 +8,23 @@ from spack_repo.builtin.build_systems.cmake import CMakePackage, generator
 
 from spack.package import *
 
-class Lemonio(AutotoolsPackage, CMakePackage):
+
+class Lemonio(CMakePackage):
     """LEMON: Lightweight Parallel I/O library for Lattice QCD."""
 
     homepage = "https://github.com/etmc/lemon"
-    git      = "https://github.com/etmc/lemon.git"
+    git = "https://github.com/etmc/lemon.git"
     license("GPL-3.0-or-later")
 
-    version('master', branch='master')
+    version("master", branch="master")
 
     depends_on("libtool", type="build", when="@master build_system=cmake")
     depends_on("cmake@4", type="build", when="master build_system=cmake")
 
-    depends_on('mpi')
+    depends_on("mpi")
 
     generator("ninja")
+
 
 class CMakeBuilder(cmake.CMakeBuilder):
     def cmake_args(self):
@@ -31,4 +33,3 @@ class CMakeBuilder(cmake.CMakeBuilder):
             self.define_from_variant("DBUILD_SHARED_LIBS", "shared"),
         ]
         return args
-
