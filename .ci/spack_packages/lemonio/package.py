8fe54981bbb0b61ca7732070955b21b81e4e5218
Roman Gruber <groman@beverin-ln001.cscs.ch>
2026-03-30 16:00:35 +0200

    lemonio is now a CmakePackage in spack

--- .ci/spack_packages/lemonio/package.py
+++ .ci/spack_packages/lemonio/package.py
@@ -2,12 +2,13 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
+from spack_repo.builtin.build_systems import cmake
+from spack_repo.builtin.build_systems.cmake import CMakePackage, generator
 
 
 from spack.package import *
 
-class Lemonio(AutotoolsPackage):
+class Lemonio(CMakePackage):
     """LEMON: Lightweight Parallel I/O library for Lattice QCD."""
 
     homepage = "https://github.com/etmc/lemon"
@@ -16,13 +17,13 @@ class Lemonio(AutotoolsPackage):
 
     version('master', branch='master')
 
-    depends_on("autoconf", type="build", when="@master build_system=autotools")
-    depends_on("automake", type="build", when="@master build_system=autotools")
-    depends_on("libtool", type="build", when="@master build_system=autotools")
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    depends_on("fortran", type="build")
 
     depends_on('mpi')
+    generator("ninja")
 
     def configure_args(self):
         args = []
-        args.append('CC={0}'.format(self.spec['mpi'].mpicc))
         return args
