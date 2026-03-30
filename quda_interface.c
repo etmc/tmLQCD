092e41f8ffd562994ad920d699fbc161181ef8b7
Konstantin Ottnad <ottnad@hiskp.uni-bonn.de>
2026-03-30 12:54:49 +0200

    Changed inv_param.matpc_type from QUDA_MATPC_EVEN_EVEN_ASYMMETRIC to QUDA_MATPC_ODD_ODD_ASYMMETRIC in quda_interface.h. This does not seem to affect the simulation but more correctly reflects the theoretical setup.

--- quda_interface.c
+++ quda_interface.c
@@ -1431,7 +1431,7 @@ void _setOneFlavourSolverParam(const double kappa, const double c_sw, const doub
       inv_param.twist_flavor = QUDA_TWIST_NO;
       inv_param.dslash_type = QUDA_CLOVER_WILSON_DSLASH;
     }
-    inv_param.matpc_type = QUDA_MATPC_EVEN_EVEN_ASYMMETRIC;
+    inv_param.matpc_type = QUDA_MATPC_ODD_ODD_ASYMMETRIC;
     inv_param.solution_type = QUDA_MAT_SOLUTION;
     inv_param.clover_order = QUDA_PACKED_CLOVER_ORDER;
     inv_param.clover_coeff = c_sw * kappa;
